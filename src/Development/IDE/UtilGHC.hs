bc9a1955bd32001be280903eb057ee772ad77f2b
Jost Berthold <jost.berthold@digitalasset.com>
2019-04-10 21:48:08 +1000

    HOTFIX damlc: allow for passing options to the underlying GHC (#346)
    * HOTFIX damlc: allow for passing options to the underlying GHC
    
    As `damlc` is based on GHC, it "understands" all options that GHC understands.
    This PR introduces a way to use GHC options that are not exposed by the `damlc`
    driver, by passing any number of `--ghc-option CUSTOM_OPTION` on the command line.
    
    The code uses the GHC function which parses options inside files, so
    prohibiting a few options that we would not want to expose (package db, output
    file, etc).
    
    All warnings that GHC emits during flag processing are presented to the user.
    
    If an option contradicts a DAML compiler setting, the compilation will be
    aborted with a GHC exception (calls makeDynFlagsConsistent internally).
    
    * HOTFIX damlc: small clean-up

--- src/Development/IDE/UtilGHC.hs
+++ src/Development/IDE/UtilGHC.hs
@@ -14,6 +14,8 @@
 module Development.IDE.UtilGHC where
 
 import           "ghc-lib-parser" Config
+import qualified "ghc-lib-parser" CmdLineParser as Cmd (warnMsg)
+import           "ghc-lib-parser" DynFlags (parseDynamicFilePragma)
 import           "ghc-lib-parser" Fingerprint
 import           "ghc-lib" GHC                         hiding (convertLit)
 import           "ghc-lib-parser" GHC.LanguageExtensions.Type
@@ -21,11 +23,13 @@ import           "ghc-lib-parser" GhcMonad
 import           "ghc-lib" GhcPlugins                  as GHC hiding (PackageState, fst3, (<>))
 import           "ghc-lib" HscMain
 import qualified "ghc-lib-parser" Packages
+import           "ghc-lib-parser" Panic (throwGhcExceptionIO)
 import           "ghc-lib-parser" Platform
 import qualified "ghc-lib-parser" StringBuffer                as SB
 import qualified "ghc-lib-parser" EnumSet
 
 import           Control.DeepSeq
+import           Control.Monad
 import           Data.IORef
 import           Data.List
 import qualified Data.Text as T
@@ -185,9 +189,26 @@ instance NFData PackageState where
 --     * Installs a custom log action;
 --     * Sets up the package databases;
 --     * Sets the import paths to the given list of 'FilePath'.
-setupDamlGHC :: GhcMonad m => [FilePath] -> Maybe String -> PackageState -> m ()
-setupDamlGHC importPaths mbPackageName packageState = do
+--     * if present, parses and applies custom options for GHC
+--       (may fail if the custom options are inconsistent with std DAML ones)
+setupDamlGHC :: GhcMonad m => [FilePath] -> Maybe String -> PackageState -> [String] -> m ()
+setupDamlGHC importPaths mbPackageName packageState [] =
   modifyDynFlags $ adjustDynFlags importPaths packageState mbPackageName
+-- if custom options are given, add them after the standard DAML flag setup
+setupDamlGHC importPaths mbPackageName packageState customOpts = do
+  setupDamlGHC importPaths mbPackageName packageState []
+  damlDFlags <- getSessionDynFlags
+  (dflags', leftover, warns) <- parseDynamicFilePragma damlDFlags $ map noLoc customOpts
+
+  let leftoverError = CmdLineError $
+        (unlines . ("Unable to parse custom flags:":) . map unLoc) leftover
+  unless (null leftover) $ liftIO $ throwGhcExceptionIO leftoverError
+
+  unless (null warns) $
+    liftIO $ putStrLn $ unlines $ "Warnings:" : map (unLoc . Cmd.warnMsg) warns
+
+  modifySession $ \h ->
+    h { hsc_dflags = dflags', hsc_IC = (hsc_IC h) {ic_dflags = dflags' } }
 
 -- | A version of `showSDoc` that uses default flags (to avoid uses of
 -- `showSDocUnsafe`).
