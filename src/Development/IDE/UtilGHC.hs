3f6eabadf3f8fb227d48f71ad6e17fdb30682cb0
Neil Mitchell <35463327+neil-da@users.noreply.github.com>
2019-04-30 21:08:36 +0100

    Add explicit export list (#806)
    * Add explicit export lists in some places
    
    * Add another explicit export

--- src/Development/IDE/UtilGHC.hs
+++ src/Development/IDE/UtilGHC.hs
@@ -11,7 +11,7 @@
 -- * Call runGhc, use runGhcFast instead. It's faster and doesn't require config we don't have.
 --
 -- * Call setSessionDynFlags, use modifyDynFlags instead. It's faster and avoids loading packages.
-module Development.IDE.UtilGHC where
+module Development.IDE.UtilGHC(module Development.IDE.UtilGHC) where
 
 import           "ghc-lib-parser" Config
 import qualified "ghc-lib-parser" CmdLineParser as Cmd (warnMsg)
