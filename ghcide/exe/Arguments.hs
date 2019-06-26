5948666ddee61d40e1cbfdf79a5a802faf86ad45
Neil Mitchell <35463327+neil-da@users.noreply.github.com>
2019-06-26 17:07:08 +0100

    Make hie-core outside an IDE work better (haskell/ghcide#1895)
    * Move the hie-core demo files around (they aren't really a demo anymore)
    
    * Split the command line parsing into a separate module
    
    * Give messages about how long starting something takes
    
    * Make the interactive mode say what it is doing a bit more
    
    * Add a --cwd flag to hie-core
    
    * Take a list of files and directories for hie-core
    
    * Update the readme to say how to test using hie-core
    
    * Fix up the bazel file
    
    * Add HLint exception

---
+++ ghcide/exe/Arguments.hs
@@ -0,0 +1,27 @@
+-- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
+
+module Arguments(Arguments(..), getArguments) where
+
+import Options.Applicative
+
+
+data Arguments = Arguments
+    {argLSP :: Bool
+    ,argsCwd :: Maybe FilePath
+    ,argFiles :: [FilePath]
+    }
+
+getArguments :: IO Arguments
+getArguments = execParser opts
+  where
+    opts = info (arguments <**> helper)
+      ( fullDesc
+     <> progDesc "Used as a test bed to check your IDE will work"
+     <> header "hie-core - the core of a Haskell IDE")
+
+arguments :: Parser Arguments
+arguments = Arguments
+      <$> switch (long "lsp" <> help "Start talking to an LSP server")
+      <*> optional (strOption $ long "cwd" <> metavar "DIR" <> help "Change to this directory")
+      <*> many (argument str (metavar "FILES/DIRS..."))
