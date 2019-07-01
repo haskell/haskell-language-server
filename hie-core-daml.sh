94e252daeffdb9e172aee42ba28fb9fbd94b3963
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2019-07-01 17:30:13 +0200

    Fix running the IDE on damlc (haskell/ghcide#1956)
    * Fix running the IDE on damlc
    
    There were two issues:
    
    1. Missing include paths.
    2. Files where the module name does not match the file name.
    
    I’ve fixed both and added a test that we can load the damlc Main.hs.

---
+++ hie-core-daml.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+cd "$(dirname "$0")"/../..
+export RULES_HASKELL_EXEC_ROOT=$PWD/
+ENV_FILE=$(mktemp)
+ARGS_FILE=$(mktemp)
+bazel build //compiler/hie-core:hie-core-exe >/dev/null 2>&1
+bazel run --define hie_bios_ghci=True //daml-foundations/daml-tools/damlc-app:damlc-app@ghci -- "$ENV_FILE" "$ARGS_FILE" >/dev/null 2>&1
+source "$ENV_FILE"
+export HIE_BIOS_ARGS="$ARGS_FILE"
+./bazel-bin/compiler/hie-core/hie-core-exe $@
+
