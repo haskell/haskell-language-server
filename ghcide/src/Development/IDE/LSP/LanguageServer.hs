b4170dac77b52da2e9832ff544eceb53e6e180ec
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2019-07-01 13:04:37 +0200

    Implement LSP request cancellation (haskell/ghcide#1954)
    * Implement LSP request cancellation

--- ghcide/src/Development/IDE/LSP/LanguageServer.hs
+++ ghcide/src/Development/IDE/LSP/LanguageServer.hs
@@ -17,9 +17,11 @@ import qualified Language.Haskell.LSP.Core as LSP
 import Control.Concurrent.Chan
 import Control.Concurrent.Extra
 import Control.Concurrent.Async
+import Control.Concurrent.STM
 import Control.Exception.Safe
 import Data.Default
 import Data.Maybe
+import qualified Data.Set as Set
 import qualified Data.Text as T
 import           GHC.IO.Handle                    (hDuplicate, hDuplicateTo)
 import System.IO
@@ -65,13 +67,38 @@ runLanguageServer options userHandlers getIdeState = do
     -- dies and can be restarted instead of losing threads silently.
     clientMsgBarrier <- newBarrier
 
-    let withResponse wrap f = Just $ \r -> writeChan clientMsgChan $ Response r wrap f
+    -- The set of requests ids that we have received but not finished processing
+    pendingRequests <- newTVarIO Set.empty
+    -- The set of requests that have been cancelled and are also in pendingRequests
+    cancelledRequests <- newTVarIO Set.empty
+
+    let withResponse wrap f = Just $ \r@RequestMessage{_id} -> do
+            atomically $ modifyTVar pendingRequests (Set.insert _id)
+            writeChan clientMsgChan $ Response r wrap f
     let withNotification old f = Just $ \r -> writeChan clientMsgChan $ Notification r (\lsp ide x -> f lsp ide x >> whenJust old ($ r))
+    let cancelRequest reqId = atomically $ do
+            queued <- readTVar pendingRequests
+            -- We want to avoid that the list of cancelled requests
+            -- keeps growing if we receive cancellations for requests
+            -- that do not exist or have already been processed.
+            when (reqId `elem` queued) $
+                modifyTVar cancelledRequests (Set.insert reqId)
+    let clearReqId reqId = atomically $ do
+            modifyTVar pendingRequests (Set.delete reqId)
+            modifyTVar cancelledRequests (Set.delete reqId)
+        -- We implement request cancellation by racing waitForCancel against
+        -- the actual request handler.
+    let waitForCancel reqId = atomically $ do
+            cancelled <- readTVar cancelledRequests
+            unless (reqId `Set.member` cancelled) retry
     let PartialHandlers parts =
             setHandlersIgnore <> -- least important
             setHandlersDefinition <> setHandlersHover <> setHandlersCodeAction <> -- useful features someone may override
             userHandlers <>
-            setHandlersNotifications -- absolutely critical, join them with user notifications
+            setHandlersNotifications <> -- absolutely critical, join them with user notifications
+            cancelHandler cancelRequest
+            -- Cancel requests are special since they need to be handled
+            -- out of order to be useful. Existing handlers are run afterwards.
     handlers <- parts WithMessage{withResponse, withNotification} def
 
     void $ waitAnyCancel =<< traverse async
@@ -79,7 +106,7 @@ runLanguageServer options userHandlers getIdeState = do
             stdin
             newStdout
             ( const $ Right ()
-            , handleInit (signalBarrier clientMsgBarrier ()) clientMsgChan
+            , handleInit (signalBarrier clientMsgBarrier ()) clearReqId waitForCancel clientMsgChan
             )
             handlers
             (modifyOptions options)
@@ -87,8 +114,8 @@ runLanguageServer options userHandlers getIdeState = do
         , void $ waitBarrier clientMsgBarrier
         ]
     where
-        handleInit :: IO () -> Chan Message -> LSP.LspFuncs () -> IO (Maybe err)
-        handleInit exitClientMsg clientMsgChan lspFuncs@LSP.LspFuncs{..} = do
+        handleInit :: IO () -> (LspId -> IO ()) -> (LspId -> IO ()) -> Chan Message -> LSP.LspFuncs () -> IO (Maybe err)
+        handleInit exitClientMsg clearReqId waitForCancel clientMsgChan lspFuncs@LSP.LspFuncs{..} = do
             ide <- getIdeState sendFunc (makeLSPVFSHandle lspFuncs)
             _ <- flip forkFinally (const exitClientMsg) $ forever $ do
                 msg <- readChan clientMsgChan
@@ -100,9 +127,21 @@ runLanguageServer options userHandlers getIdeState = do
                                 "Message: " ++ show x ++ "\n" ++
                                 "Exception: " ++ show e
                     Response x@RequestMessage{_id, _params} wrap act ->
+                        flip finally (clearReqId _id) $
                         catch (do
-                            res <- act lspFuncs ide _params
-                            sendFunc $ wrap $ ResponseMessage "2.0" (responseId _id) (Just res) Nothing
+                            -- We could optimize this by first checking if the id
+                            -- is in the cancelled set. However, this is unlikely to be a
+                            -- bottleneck and the additional check might hide
+                            -- issues with async exceptions that need to be fixed.
+                            cancelOrRes <- race (waitForCancel _id) $ act lspFuncs ide _params
+                            case cancelOrRes of
+                                Left () -> do
+                                    logDebug (ideLogger ide) $ T.pack $
+                                        "Cancelled request " <> show _id
+                                    sendFunc $ wrap $ ResponseMessage "2.0" (responseId _id) Nothing $
+                                        Just $ ResponseError RequestCancelled "" Nothing
+                                Right res ->
+                                    sendFunc $ wrap $ ResponseMessage "2.0" (responseId _id) (Just res) Nothing
                         ) $ \(e :: SomeException) -> do
                             logError (ideLogger ide) $ T.pack $
                                 "Unexpected exception on request, please report!\n" ++
@@ -117,10 +156,16 @@ runLanguageServer options userHandlers getIdeState = do
 --   Set them to avoid a warning in VS Code output.
 setHandlersIgnore :: PartialHandlers
 setHandlersIgnore = PartialHandlers $ \_ x -> return x
-    {LSP.cancelNotificationHandler = none
-    ,LSP.initializedHandler = none
+    {LSP.initializedHandler = none
     }
     where none = Just $ const $ return ()
+
+cancelHandler :: (LspId -> IO ()) -> PartialHandlers
+cancelHandler cancelRequest = PartialHandlers $ \_ x -> return x
+    {LSP.cancelNotificationHandler = Just $ \msg@NotificationMessage {_params = CancelParams {_id}} -> do
+            cancelRequest _id
+            whenJust (LSP.cancelNotificationHandler x) ($ msg)
+    }
 
 
 -- | A message that we need to deal with - the pieces are split up with existentials to gain additional type safety
