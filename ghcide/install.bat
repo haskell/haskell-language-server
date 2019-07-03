53413ab9847913fdcc39fe4924858ea8d7c33df3
Neil Mitchell <ndmitchell@gmail.com>
2019-07-03 11:51:46 +0100

    Standalone pieces for hie-core (haskell/ghcide#1982)
    * Add a .gitignore just for the hie-core piece
    
    * Add standalone pieces to make hie-core work with a global Cabal install
    
    * Add more things to .gitignore, PR suggestion
    
    * Add copyright header

---
+++ ghcide/install.bat
@@ -0,0 +1,6 @@
+:: Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+:: SPDX-License-Identifier: Apache-2.0
+
+@REM Install hie-core where cabal install would put it on Windows
+@REM but avoid checking configure or installing local libraries (faster)
+ghc Main -o dist\obj\hie-core.exe -XBangPatterns -XDeriveGeneric -XGeneralizedNewtypeDeriving -XLambdaCase -XNamedFieldPuns -XRecordWildCards -XScopedTypeVariables -XStandaloneDeriving -XTupleSections -XTypeApplications -XViewPatterns -package=ghc -DGHC_STABLE -isrc -iexe -outputdir dist\obj && copy dist\obj\hie-core.exe %AppData%\cabal\bin\hie-core.exe
