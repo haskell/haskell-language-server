1b9d07290331abb76a2a94f54da486f9c396b998
Andreas Herrmann <42969706+aherrmann-da@users.noreply.github.com>
2019-07-22 15:42:04 +0200

    hie-core/test: Dedicated test suite for hie-core (haskell/ghcide#2243)
    * hie-core/test: Dedicated test suite for hie-core
    
    * Apply hlint suggestion
    
    Use System.Environment.Blank's setEnv which has a non-overwrite mode
    that implements precisely what we were doing with more code before.
    
    * buildifier fixes
    
    * hie-core-exe works on Windows now - ghc-paths was fixed

---
+++ ghcide/test/exe/Main.hs
@@ -0,0 +1,75 @@
+-- Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+-- SPDX-License-Identifier: Apache-2.0
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Control.Monad (void)
+import qualified Data.Text as T
+import Development.IDE.Test
+import Language.Haskell.LSP.Test
+import Language.Haskell.LSP.Types
+import System.Environment.Blank (setEnv)
+import System.FilePath
+import System.IO.Extra
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import DA.Bazel.Runfiles
+
+
+main :: IO ()
+main = defaultMain $ testGroup "HIE"
+  [ testSession "open close" $ do
+      doc <- openDoc' "Testing.hs" "haskell" ""
+      void (message :: Session ProgressStartNotification)
+      closeDoc doc
+      void (message :: Session ProgressDoneNotification)
+  , testSession "fix syntax error" $ do
+      let content = T.unlines [ "module Testing wher" ]
+      doc <- openDoc' "Testing.hs" "haskell" content
+      expectDiagnostics [("Testing.hs", [(DsError, (0, 15), "parse error")])]
+      let change = TextDocumentContentChangeEvent
+            { _range = Just (Range (Position 0 15) (Position 0 19))
+            , _rangeLength = Nothing
+            , _text = "where"
+            }
+      changeDoc doc [change]
+      expectDiagnostics [("Testing.hs", [])]
+  , testSession "introduce syntax error" $ do
+      let content = T.unlines [ "module Testing where" ]
+      doc <- openDoc' "Testing.hs" "haskell" content
+      void (message :: Session ProgressStartNotification)
+      let change = TextDocumentContentChangeEvent
+            { _range = Just (Range (Position 0 15) (Position 0 18))
+            , _rangeLength = Nothing
+            , _text = "wher"
+            }
+      changeDoc doc [change]
+      expectDiagnostics [("Testing.hs", [(DsError, (0, 15), "parse error")])]
+  ]
+
+
+----------------------------------------------------------------------
+-- Utils
+
+
+testSession :: String -> Session () -> TestTree
+testSession name = testCase name . run
+
+
+run :: Session a -> IO a
+run s = withTempDir $ \dir -> do
+  let hieCoreExePath = mainWorkspace </> exe "compiler/hie-core/hie-core-exe"
+  hieCoreExe <- locateRunfiles hieCoreExePath
+  let cmd = unwords [hieCoreExe, "--lsp", "--cwd", dir]
+  -- HIE calls getXgdDirectory which assumes that HOME is set.
+  -- Only sets HOME if it wasn't already set.
+  setEnv "HOME" "/homeless-shelter" False
+  runSessionWithConfig conf cmd fullCaps dir s
+  where
+    conf = defaultConfig
+      -- If you uncomment this you can see all messages
+      -- which can be quite useful for debugging.
+      -- { logMessages = True, logColor = False, logStdErr = True }
