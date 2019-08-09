7b180accba4b8d43fd086f8f138a3f33ab83f49e
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2019-08-09 12:48:05 +0200

    Add an ide-debug-driver to make it easier to find leaks (haskell/ghcide#2472)

--- ghcide/src/Development/IDE/LSP/Notifications.hs
+++ ghcide/src/Development/IDE/LSP/Notifications.hs
@@ -32,7 +32,6 @@ setHandlersNotifications = PartialHandlers $ \WithMessage{..} x -> return x
     {LSP.didOpenTextDocumentNotificationHandler = withNotification (LSP.didOpenTextDocumentNotificationHandler x) $
         \_ ide (DidOpenTextDocumentParams TextDocumentItem{_uri,_version}) -> do
             updatePositionMapping ide (VersionedTextDocumentIdentifier _uri (Just _version)) (List [])
-            setSomethingModified ide
             whenUriFile _uri $ \file -> do
                 modifyFilesOfInterest ide (S.insert file)
                 logInfo (ideLogger ide) $ "Opened text document: " <> getUri _uri
@@ -50,7 +49,6 @@ setHandlersNotifications = PartialHandlers $ \WithMessage{..} x -> return x
 
     ,LSP.didCloseTextDocumentNotificationHandler = withNotification (LSP.didCloseTextDocumentNotificationHandler x) $
         \_ ide (DidCloseTextDocumentParams TextDocumentIdentifier{_uri}) -> do
-            setSomethingModified ide
             whenUriFile _uri $ \file -> do
                 modifyFilesOfInterest ide (S.delete file)
                 logInfo (ideLogger ide) $ "Closed text document: " <> getUri _uri
