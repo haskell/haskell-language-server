507d4165d06d83669627dad0544307a10e0ca79f
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2019-08-13 17:23:03 +0100

    update copyright notices (haskell/ghcide#2499)

--- ghcide/cbits/getmodtime.c
+++ ghcide/cbits/getmodtime.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// Copyright (c) 2019 The DAML Authors. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 #include <sys/stat.h>
--- ghcide/hie-core-daml.sh
+++ ghcide/hie-core-daml.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (c) 2019 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# Copyright (c) 2019 The DAML Authors. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 set -euo pipefail
