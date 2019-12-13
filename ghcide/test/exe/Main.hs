f33cfd2b2f73389c7bf9930ea8d43ac9ab6cc0e8
Jacek Generowicz <jacg@my-post-office.net>
2019-12-13 14:07:51 +0100

    Tests for issue haskell/ghcide#237 (haskell/ghcide#238)
    * Add tests for issue haskell/ghcide#237
    
    * Tell hlint to ignore test sample code
    
    * Add test showing similar problem in listcomps
    
    * Identify implementation necessary for test to pass

--- ghcide/test/exe/Main.hs
+++ ghcide/test/exe/Main.hs
@@ -786,6 +786,10 @@ findDefinitionAndHoverTests = let
   clL23  = Position 23 11  ;  cls    = [mkR  21  0   22 20]
   clL25  = Position 25  9
   eclL15 = Position 15  8  ;  ecls   = [ExpectHoverText ["Num"], ExpectExternFail]
+  dnbL29 = Position 29 18  ;  dnb    = [ExpectHoverText [":: ()"],   mkR  29 12   29 21]
+  dnbL30 = Position 30 23
+  lcbL33 = Position 33 26  ;  lcb    = [ExpectHoverText [":: Char"], mkR  33 26   33 27]
+  lclL33 = Position 33 22
   in
   mkFindTests
   --     def    hover  look   expect
@@ -806,11 +810,15 @@ findDefinitionAndHoverTests = let
   , test yes    broken clL23  cls    "class in instance declaration"
   , test yes    broken clL25  cls    "class in signature"            -- 147
   , test broken broken eclL15 ecls   "external class in signature"
+  , test yes    broken dnbL29 dnb    "do-notation   bind"            -- 137
+  , test yes    yes    dnbL30 dnb    "do-notation lookup"
+  , test yes    broken lcbL33 lcb    "listcomp   bind"               -- 137
+  , test yes    yes    lclL33 lcb    "listcomp lookup"
   ]
   where yes, broken :: (TestTree -> Maybe TestTree)
         yes    = Just -- test should run and pass
         broken = Just . (`xfail` "known broken")
-      --  no = const Nothing -- don't run this test at all
+        -- no = const Nothing -- don't run this test at all
 
 pluginTests :: TestTree
 pluginTests = testSessionWait "plugins" $ do
