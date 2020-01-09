3b095012045723fc9896ba8fdb592521148bd211
Alejandro Serrano <trupill@gmail.com>
2020-01-09 09:44:32 +0100

    Better docs for completions (haskell/ghcide#288)
    * Remove JSON instances for completions, since we are not implementing "resolve"
    
    * Remove completion resolve data from tests
    
    * Better docs
    
    * Fix tests
    
    * Fix for 8.4
    
    * Turn Haddock markup into Markdown
    
    * Add types to completion items
    
    * Make it work on 8.8 and 8.4
    
    * Revert "Remove completion resolve data from tests"
    
    This reverts commit 625d710f11db2215a886e0a75e35f646190d4b36.
    
    * Revert "Remove JSON instances for completions, since we are not implementing "resolve""
    
    This reverts commit 12ff27dce71d06ba2f74aa8b9695aea95368e1d2.
    
    * Fix tests
    
    * Require higher version of regex-pcre-builtin
    
    * Replace Pandoc with direct conversion from Haddock to Markdown
    
    * Show kinds of type constructors too
    
    * A few fixed to Markdown conversion
    
    * Check optNewColonConvention
    
    * Fix build on 8.4 and 8.8
    
    * More fixes for 8.4 and 8.8
    
    * Check only the common part of the completion text
    
    * Make icons consistent with Outline
    
    * Test docs for completions
    
    * Make constructors return the corresponding CompletionItem + tests for that behavior
    
    * Make test work on 8.4

--- ghcide/src/Development/IDE/Spans/Documentation.hs
+++ ghcide/src/Development/IDE/Spans/Documentation.hs
@@ -1,8 +1,12 @@
 -- Copyright (c) 2019 The DAML Authors. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
+{-# LANGUAGE CPP #-}
+#include "ghc-api-version.h"
+
 module Development.IDE.Spans.Documentation (
     getDocumentation
+  , getDocumentationTryGhc
   ) where
 
 import           Control.Monad
@@ -16,6 +20,28 @@ import           FastString
 import           GHC
 import SrcLoc
 
+#if MIN_GHC_API_VERSION(8,6,0)
+import           Data.Char (isSpace)
+import           Development.IDE.GHC.Util
+import qualified Documentation.Haddock.Parser as H
+import qualified Documentation.Haddock.Types as H
+#endif
+
+getDocumentationTryGhc
+  :: HscEnv
+  -> [TypecheckedModule]
+  -> Name
+  -> IO [T.Text]
+#if MIN_GHC_API_VERSION(8,6,0)
+getDocumentationTryGhc packageState tcs name = do
+  res <- runGhcEnv packageState $ catchSrcErrors "docs" $ getDocs name
+  case res of
+    Right (Right (Just docs, _)) -> return [T.pack $ haddockToMarkdown $ H.toRegular $ H._doc $ H.parseParas Nothing $ unpackHDS docs]
+    _ -> return $ getDocumentation tcs name
+#else
+getDocumentationTryGhc _packageState tcs name = do
+  return $ getDocumentation tcs name
+#endif
 
 getDocumentation
  :: [TypecheckedModule] -- ^ All of the possible modules it could be defined in.
@@ -90,3 +116,81 @@ docHeaders = mapMaybe (\(L _ x) -> wrk x)
                             then Just $ T.pack s
                             else Nothing
     _ -> Nothing
+
+#if MIN_GHC_API_VERSION(8,6,0)
+-- Simple (and a bit hacky) conversion from Haddock markup to Markdown
+haddockToMarkdown
+  :: H.DocH String String -> String
+
+haddockToMarkdown H.DocEmpty
+  = ""
+haddockToMarkdown (H.DocAppend d1 d2)
+  = haddockToMarkdown d1 <> haddockToMarkdown d2
+haddockToMarkdown (H.DocString s)
+  = s
+haddockToMarkdown (H.DocParagraph p)
+  = "\n\n" ++ haddockToMarkdown p
+haddockToMarkdown (H.DocIdentifier i)
+  = "`" ++ i ++ "`"
+haddockToMarkdown (H.DocIdentifierUnchecked i)
+  = "`" ++ i ++ "`"
+haddockToMarkdown (H.DocModule i)
+  = "`" ++ i ++ "`"
+haddockToMarkdown (H.DocWarning w)
+  = haddockToMarkdown w
+haddockToMarkdown (H.DocEmphasis d)
+  = "*" ++ haddockToMarkdown d ++ "*"
+haddockToMarkdown (H.DocBold d)
+  = "**" ++ haddockToMarkdown d ++ "**"
+haddockToMarkdown (H.DocMonospaced d)
+  = "`" ++ escapeBackticks (haddockToMarkdown d) ++ "`"
+  where
+    escapeBackticks "" = ""
+    escapeBackticks ('`':ss) = '\\':'`':escapeBackticks ss
+    escapeBackticks (s  :ss) = s:escapeBackticks ss
+haddockToMarkdown (H.DocCodeBlock d)
+  = "\n```haskell\n" ++ haddockToMarkdown d ++ "\n```\n"
+haddockToMarkdown (H.DocExamples es)
+  = "\n```haskell\n" ++ unlines (map exampleToMarkdown es) ++ "\n```\n"
+  where
+    exampleToMarkdown (H.Example expr result)
+      = ">>> " ++ expr ++ "\n" ++ unlines result
+haddockToMarkdown (H.DocHyperlink (H.Hyperlink url Nothing))
+  = "<" ++ url ++ ">"
+#if MIN_VERSION_haddock_library(1,8,0)
+haddockToMarkdown (H.DocHyperlink (H.Hyperlink url (Just label)))
+  = "[" ++ haddockToMarkdown label ++ "](" ++ url ++ ")"
+#else
+haddockToMarkdown (H.DocHyperlink (H.Hyperlink url (Just label)))
+  = "[" ++ label ++ "](" ++ url ++ ")"
+#endif
+haddockToMarkdown (H.DocPic (H.Picture url Nothing))
+  = "![](" ++ url ++ ")"
+haddockToMarkdown (H.DocPic (H.Picture url (Just label)))
+  = "![" ++ label ++ "](" ++ url ++ ")"
+haddockToMarkdown (H.DocAName aname)
+  = "[" ++ aname ++ "]:"
+haddockToMarkdown (H.DocHeader (H.Header level title))
+  = replicate level '#' ++ " " ++ haddockToMarkdown title
+
+haddockToMarkdown (H.DocUnorderedList things)
+  = '\n' : (unlines $ map (\thing -> "+ " ++ dropWhile isSpace (haddockToMarkdown thing)) things)
+haddockToMarkdown (H.DocOrderedList things)
+  = '\n' : (unlines $ map (\thing -> "1. " ++ dropWhile isSpace (haddockToMarkdown thing)) things)
+haddockToMarkdown (H.DocDefList things)
+  = '\n' : (unlines $ map (\(term, defn) -> "+ **" ++ haddockToMarkdown term ++ "**: " ++ haddockToMarkdown defn) things)
+
+-- we cannot render math by default
+haddockToMarkdown (H.DocMathInline _)
+  = "*cannot render inline math formula*"
+haddockToMarkdown (H.DocMathDisplay _)
+  = "\n\n*cannot render display math formula*\n\n"
+
+-- TODO: render tables
+haddockToMarkdown (H.DocTable _t)
+  = "\n\n*tables are not yet supported*\n\n"
+
+-- things I don't really know how to handle
+haddockToMarkdown (H.DocProperty _)
+  = ""  -- don't really know what to do
+#endif
