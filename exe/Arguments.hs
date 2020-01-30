ad1743307a4d28d99d9c4e0a65eed16262b4ab15
Alan Zimmerman <alan.zimm@gmail.com>
2020-01-30 21:35:36 +0000

    Harmonise CLI flags between ghcide/haskell-ide(-wrapper)
    And add --example to enable the Example plugin in haskell-ide
    
    Closes #24

--- exe/Arguments.hs
+++ exe/Arguments.hs
@@ -1,10 +1,28 @@
 -- Copyright (c) 2019 The DAML Authors. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
+{-# LANGUAGE CPP #-} -- To get precise GHC version
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -Wno-dodgy-imports #-} -- GHC no longer exports def in GHC 8.6 and above
 
-module Arguments(Arguments(..), getArguments) where
+module Arguments
+  ( Arguments(..)
+  , getArguments
+  , ghcideVersion
+  , getLibdir
+  ) where
 
+import Data.Maybe
+import Data.Version
+import Development.GitRev
+import qualified GHC.Paths
 import Options.Applicative
+import Paths_ide
+import System.Environment
 
+-- ---------------------------------------------------------------------
 
 data Arguments = Arguments
     {argLSP :: Bool
@@ -12,21 +30,44 @@ data Arguments = Arguments
     ,argFiles :: [FilePath]
     ,argsVersion :: Bool
     ,argsShakeProfiling :: Maybe FilePath
+    ,argsExamplePlugin :: Bool
     }
 
-getArguments :: IO Arguments
-getArguments = execParser opts
+getArguments :: String -> IO Arguments
+getArguments exeName = execParser opts
   where
-    opts = info (arguments <**> helper)
+    opts = info (arguments exeName <**> helper)
       ( fullDesc
      <> progDesc "Used as a test bed to check your IDE will work"
-     <> header "ghcide - the core of a Haskell IDE")
+     <> header (exeName ++ " - GHC Haskell LSP server"))
 
-arguments :: Parser Arguments
-arguments = Arguments
+arguments :: String -> Parser Arguments
+arguments exeName = Arguments
       <$> switch (long "lsp" <> help "Start talking to an LSP server")
-      <*> optional (strOption $ long "cwd" <> metavar "DIR" <> help "Change to this directory")
+      <*> optional (strOption $ long "cwd" <> metavar "DIR"
+                    <> help "Change to this directory")
       <*> many (argument str (metavar "FILES/DIRS..."))
-      <*> switch (long "version" <> help "Show ghcide and GHC versions")
-      <*> optional (strOption $ long "shake-profiling" <> metavar "DIR" <> help "Dump profiling reports to this directory")
+      <*> switch (long "version"
+                  <> help ("Show " ++ exeName  ++ " and GHC versions"))
+      <*> optional (strOption $ long "shake-profiling" <> metavar "DIR"
+                    <> help "Dump profiling reports to this directory")
+      <*> switch (long "example"
+                  <> help "Include the Example Plugin. For Plugin devs only")
 
+-- ---------------------------------------------------------------------
+-- Set the GHC libdir to the nix libdir if it's present.
+getLibdir :: IO FilePath
+getLibdir = fromMaybe GHC.Paths.libdir <$> lookupEnv "NIX_GHC_LIBDIR"
+
+ghcideVersion :: IO String
+ghcideVersion = do
+  path <- getExecutablePath
+  let gitHashSection = case $(gitHash) of
+        x | x == "UNKNOWN" -> ""
+        x -> " (GIT hash: " <> x <> ")"
+  return $ "ghcide version: " <> showVersion version
+             <> " (GHC: " <> VERSION_ghc
+             <> ") (PATH: " <> path <> ")"
+             <> gitHashSection
+
+-- ---------------------------------------------------------------------
