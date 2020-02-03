5deda6ce0e30c145ee3804c19559a1497897dbee
Alan Zimmerman <alan.zimm@gmail.com>
2020-02-03 12:19:45 +0000

    Allow the withResponse handler to idiomatically return an error (haskell/ghcide#396)
    * Allow the withResponse handler to idiomatically return an error
    
    An LSP response message can have either a result or an error
    field. Expose this in the handler by having a return type
    
        type ResponseBody resp = Either ResponseError resp
    
    Closes haskell/ghcide#395
    
    * Apply hint to use lambdacase
    
    * Simplify, based on @ndmitchell review feedback.
    
    * Remove ResponseBody by inlining it, fmap some results

--- ghcide/src/Development/IDE/LSP/Server.hs
+++ ghcide/src/Development/IDE/LSP/Server.hs
@@ -16,17 +16,16 @@ import qualified Language.Haskell.LSP.Core as LSP
 import qualified Language.Haskell.LSP.Messages as LSP
 import Development.IDE.Core.Service
 
-
 data WithMessage = WithMessage
     {withResponse :: forall m req resp . (Show m, Show req) =>
         (ResponseMessage resp -> LSP.FromServerMessage) -> -- how to wrap a response
-        (LSP.LspFuncs () -> IdeState -> req -> IO resp) -> -- actual work
+        (LSP.LspFuncs () -> IdeState -> req -> IO (Either ResponseError resp)) -> -- actual work
         Maybe (LSP.Handler (RequestMessage m req resp))
     ,withNotification :: forall m req . (Show m, Show req) =>
         Maybe (LSP.Handler (NotificationMessage m req)) -> -- old notification handler
         (LSP.LspFuncs () -> IdeState -> req -> IO ()) -> -- actual work
         Maybe (LSP.Handler (NotificationMessage m req))
-    ,withResponseAndRequest :: forall m rm req resp newReqParams newReqBody. 
+    ,withResponseAndRequest :: forall m rm req resp newReqParams newReqBody.
         (Show m, Show rm, Show req, Show newReqParams, Show newReqBody) =>
         (ResponseMessage resp -> LSP.FromServerMessage) -> -- how to wrap a response
         (RequestMessage rm newReqParams newReqBody -> LSP.FromServerMessage) -> -- how to wrap the additional req
