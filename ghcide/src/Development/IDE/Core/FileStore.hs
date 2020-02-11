811e54b5d416b65c86590c106237caa95b178556
Pepe Iborra <pepeiborra@gmail.com>
2020-02-11 09:43:50 +0000

    Change the representation of filestamps (haskell/ghcide#415)
    * Change the representation of filestamps
    
    This is needed to be able to compare them, which we want to use for loading
    .hie and .hi files
    
    * Update src/Development/IDE/Core/FileStore.hs
    
    Co-Authored-By: Neil Mitchell <ndmitchell@gmail.com>
    
    Co-authored-by: Neil Mitchell <ndmitchell@gmail.com>

--- ghcide/src/Development/IDE/Core/FileStore.hs
+++ ghcide/src/Development/IDE/Core/FileStore.hs
@@ -95,11 +95,12 @@ getModificationTimeRule :: VFSHandle -> Rules ()
 getModificationTimeRule vfs =
     defineEarlyCutoff $ \GetModificationTime file -> do
         let file' = fromNormalizedFilePath file
-        let wrap time = (Just time, ([], Just $ ModificationTime time))
+        let wrap time@(l,s) = (Just $ BS.pack $ show time, ([], Just $ ModificationTime l s))
         alwaysRerun
         mbVirtual <- liftIO $ getVirtualFile vfs $ filePathToUri' file
         case mbVirtual of
-            Just (virtualFileVersion -> ver) -> pure (Just $ BS.pack $ show ver, ([], Just $ VFSVersion ver))
+            Just (virtualFileVersion -> ver) ->
+                pure (Just $ BS.pack $ show ver, ([], Just $ VFSVersion ver))
             Nothing -> liftIO $ fmap wrap (getModTime file')
               `catch` \(e :: IOException) -> do
                 let err | isDoesNotExistError e = "File does not exist: " ++ file'
@@ -115,11 +116,13 @@ getModificationTimeRule vfs =
     -- We might also want to try speeding this up on Windows at some point.
     -- TODO leverage DidChangeWatchedFile lsp notifications on clients that
     -- support them, as done for GetFileExists
-    getModTime :: FilePath -> IO BS.ByteString
+    getModTime :: FilePath -> IO (Int,Int)
     getModTime f =
 #ifdef mingw32_HOST_OS
         do time <- Dir.getModificationTime f
-           pure $! BS.pack $ show (toModifiedJulianDay $ utctDay time, diffTimeToPicoseconds $ utctDayTime time)
+           let !day = toModifiedJulianDay $ utctDay time
+               !dayTime = diffTimeToPicoseconds $ utctDayTime time
+           pure (day, dayTime)
 #else
         withCString f $ \f' ->
         alloca $ \secPtr ->
@@ -127,7 +130,7 @@ getModificationTimeRule vfs =
             Posix.throwErrnoPathIfMinus1Retry_ "getmodtime" f $ c_getModTime f' secPtr nsecPtr
             sec <- peek secPtr
             nsec <- peek nsecPtr
-            pure $! BS.pack $ show sec <> "." <> show nsec
+            pure (fromEnum sec, fromIntegral nsec)
 
 -- Sadly even unix’s getFileStatus + modificationTimeHiRes is still about twice as slow
 -- as doing the FFI call ourselves :(.
