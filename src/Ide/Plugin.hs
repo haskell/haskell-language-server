daf98d759335e8abb3a75b0b5f91b7fde197f5d0
Alan Zimmerman <alan.zimm@gmail.com>
2020-03-18 23:20:59 +0000

    Remove unused import, so tests can run

--- src/Ide/Plugin.hs
+++ src/Ide/Plugin.hs
@@ -40,7 +40,6 @@ import           Ide.Plugin.Config
 import           Ide.Plugin.Formatter
 import           Ide.Types
 import qualified Language.Haskell.LSP.Core as LSP
-import qualified Language.Haskell.LSP.Messages as LSP
 import           Language.Haskell.LSP.Messages
 import           Language.Haskell.LSP.Types
 import qualified Language.Haskell.LSP.Types              as J
