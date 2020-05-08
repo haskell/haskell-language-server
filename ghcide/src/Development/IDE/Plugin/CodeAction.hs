9d408d7a049d37cf419854b66c1430a10d20f394
J. S <document_done@hotmail.com>
2020-05-08 09:48:33 -0400

    let suggest new imports work on symbol operators used infix (haskell/ghcide#540)

--- ghcide/src/Development/IDE/Plugin/CodeAction.hs
+++ ghcide/src/Development/IDE/Plugin/CodeAction.hs
@@ -424,6 +424,8 @@ extractNotInScopeName x
   = Just $ NotInScopeDataConstructor name
   | Just [name] <- matchRegex x "ot in scope: type constructor or class [^‘]*‘([^’]*)’"
   = Just $ NotInScopeTypeConstructorOrClass name
+  | Just [name] <- matchRegex x "ot in scope: \\(([^‘ ]+)\\)"
+  = Just $ NotInScopeThing name
   | Just [name] <- matchRegex x "ot in scope: ([^‘ ]+)"
   = Just $ NotInScopeThing name
   | Just [name] <- matchRegex x "ot in scope:[^‘]*‘([^’]*)’"
