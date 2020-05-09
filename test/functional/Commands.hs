d1ab2d8657f6e51d835b2b0a00dafd15cb07dc34
Jeff Windsor <jeff.windsor@gmail.com>
2020-05-09 13:57:55 -0700

    Commands (de-prefix failing)

--- test/functional/Commands.hs
+++ test/functional/Commands.hs
@@ -15,11 +15,20 @@ import TastyUtils
 
 tests :: TestTree
 tests = testGroup "Commands" [
-    testCase "are prefixed" $ runSession hieCommand fullCaps "test/testdata/" $ do
-        ResponseMessage _ _ (Just res) Nothing <- initializeResponse
-        let List cmds = res ^. LSP.capabilities . executeCommandProvider . _Just . commands
-            f x = (T.length (T.takeWhile isNumber x) >= 1) && (T.count ":" x >= 2)
-        liftIO $ do
-            cmds `shouldSatisfy` all f
-            cmds `shouldNotSatisfy` null
+    testCase "are prefixed" $
+        runSession hieCommand fullCaps "test/testdata/" $ do
+            ResponseMessage _ _ (Just res) Nothing <- initializeResponse
+            let List cmds = res ^. LSP.capabilities . executeCommandProvider . _Just . commands
+                f x = (T.length (T.takeWhile isNumber x) >= 1) && (T.count ":" x >= 2)
+            liftIO $ do
+                cmds `shouldSatisfy` all f
+                cmds `shouldNotSatisfy` null
+    , testCase "get de-prefixed" $
+        runSession hieCommand fullCaps "test/testdata/" $ do
+            ResponseMessage _ _ _ (Just err) <- request
+                WorkspaceExecuteCommand
+                (ExecuteCommandParams "1234:package:add" (Just (List [])) Nothing) :: Session ExecuteCommandResponse
+            let ResponseError _ msg _ = err
+            -- We expect an error message about the dud arguments, but should pickup "add" and "package"
+            liftIO $ msg `shouldSatisfy` T.isInfixOf "while parsing args for add in plugin package"
     ]
