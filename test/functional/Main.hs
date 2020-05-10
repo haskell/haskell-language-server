8cccd93f4807f20c0f149ef817c036b7000d6576
Jeff Windsor <jeff.windsor@gmail.com>
2020-05-10 11:59:33 -0700

    turn on all tests

--- test/functional/Main.hs
+++ test/functional/Main.hs
@@ -1,16 +1,16 @@
 module Main where
 
 import Command
--- import Completion
--- import Deferred
--- import Definition
--- import Diagnostic
--- import Format
+import Completion
+import Deferred
+import Definition
+import Diagnostic
+import Format
 import FunctionalBadProject
--- import FunctionalCodeAction
--- import FunctionalLiquid
--- import HieBios
--- import Highlight
+import FunctionalCodeAction
+import FunctionalLiquid
+import HieBios
+import Highlight
 import Progress
 import Reference
 import Rename
