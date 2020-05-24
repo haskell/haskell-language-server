bec4e9f1418eec3f53b7bddeb02bf292226d1852
Luke Lau <luke_lau@icloud.com>
2020-05-24 01:24:28 +0100

    Remove redundant import
    This doesn't seem to be needed on whatever verison of ghcide is in the
    submodule, and was causing build errors because of haskell-lsp-type's
    List being exported from two different imports

--- src/Ide/Types.hs
+++ src/Ide/Types.hs
@@ -28,7 +28,6 @@ import qualified Data.Set                      as S
 import           Data.String
 import qualified Data.Text                     as T
 import           Development.IDE.Core.Rules
-import           Development.IDE.Types.Diagnostics as D
 import           Development.IDE.Types.Location
 import           Development.Shake
 import           Ide.Plugin.Config
