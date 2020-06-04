3e10ae3c51a69a345694af74b1d61529104edcdb
sir4ur0n <julien.debon@pm.me>
2020-06-04 22:40:47 +0200

    feat(hie-bios): Multi-cradle, ignore directories
    Related to #121

--- hie.yaml.cbl
+++ hie.yaml.cbl
@@ -2,25 +2,40 @@
 # in hie, using cabal as the build system.  To use is, copy it to a
 # file called 'hie.yaml'
 cradle:
-  cabal:
+  multi:
+    - path: "./test/testdata/"
+      config: { cradle: { none:  } }
 
-    - path: "./test/functional/"
-      component: "haskell-language-server:func-test"
+    - path: "./install/"
+      config: { cradle: { none:  } }
 
-    - path: "./test/utils/"
-      component: "haskell-language-server:func-test"
+    - path: "./"
+      config:
+        cradle:
+          cabal:
+            - path: "./test/functional/"
+              component: "haskell-language-server:func-test"
 
-    - path: "./exe/Main.hs"
-      component: "haskell-language-server:exe:haskell-language-server"
+            - path: "./test/utils/"
+              component: "haskell-language-server:func-test"
+        
+            - path: "./exe/Main.hs"
+              component: "haskell-language-server:exe:haskell-language-server"
+        
+            - path: "./exe/Arguments.hs"
+              component: "haskell-language-server:exe:haskell-language-server"
 
-    - path: "./exe/Wrapper.hs"
-      component: "haskell-language-server:exe:haskell-language-server-wrapper"
+            - path: "./exe/Wrapper.hs"
+              component: "haskell-language-server:exe:haskell-language-server-wrapper"
+        
+            - path: "./src"
+              component: "lib:haskell-language-server"
 
-    - path: "./src"
-      component: "lib:haskell-language-server"
-
-    - path: "./ghcide/src"
-      component: "ghcide:lib:ghcide"
-
-    - path: "./ghcide/exe"
-      component: "ghcide:exe:ghcide"
+            - path: "./.stack-work/"
+              component: "lib:haskell-language-server"
+        
+            - path: "./ghcide/src"
+              component: "ghcide:lib:ghcide"
+        
+            - path: "./ghcide/exe"
+              component: "ghcide:exe:ghcide"
