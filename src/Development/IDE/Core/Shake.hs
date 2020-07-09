f32f666d2ef12cbea5ea03f20a1821b502f6c1fd
wz1000 <zubin@cmi.ac.in>
2020-07-09 17:46:50 +0530

    Use a global namecache to read `.hie` files (#677)
    * Use global NameCache for reading HIE files
    
    Co-authored-by: Matthew Pickering <matthewtpickering@gmail.com>
    
    * ignore hlint
    
    * redundant imports
    
    * Use hie files as source of truth for name source spans.
    
    Since we started reusing `.hi` files, this exposes a bug where definitions
    aren't available since a bad source span from the `.hi` file gets put into
    the NameCache. We rectify by ensuring the span in the NameCache always matches
    the one from the `.hie` file.
    
    This has surfaced because an interaction between the commit which uses `.hi`
    instead of retypechecking and the change to use the shared global NameCache
    to read `.hie` files.
    
    * Add test for missing definitions
    
    Co-authored-by: Matthew Pickering <matthewtpickering@gmail.com>

--- src/Development/IDE/Core/Shake.hs
+++ src/Development/IDE/Core/Shake.hs
@@ -56,7 +56,8 @@ module Development.IDE.Core.Shake(
     WithProgressFunc, WithIndefiniteProgressFunc,
     ProgressEvent(..),
     DelayedAction, mkDelayedAction,
-    IdeAction(..), runIdeAction
+    IdeAction(..), runIdeAction,
+    mkUpdater
     ) where
 
 import           Development.Shake hiding (ShakeValue, doesFileExist, Info)
@@ -75,6 +76,7 @@ import qualified Data.Text as T
 import Data.Tuple.Extra
 import Data.Unique
 import Development.IDE.Core.Debouncer
+import Development.IDE.GHC.Compat ( NameCacheUpdater(..), upNameCache )
 import Development.IDE.Core.PositionMapping
 import Development.IDE.Types.Logger hiding (Priority)
 import qualified Development.IDE.Types.Logger as Logger
@@ -105,8 +107,13 @@ import Data.Foldable (traverse_)
 import qualified Control.Monad.STM as STM
 import Control.Monad.IO.Class
 import Control.Monad.Reader
+import Control.Monad.Trans.Maybe
 import Data.Traversable
 
+import Data.IORef
+import NameCache
+import UniqSupply
+import PrelInfo
 
 -- information we stash inside the shakeExtra field
 data ShakeExtras = ShakeExtras
@@ -138,6 +145,7 @@ data ShakeExtras = ShakeExtras
     ,withIndefiniteProgress :: WithIndefiniteProgressFunc
     -- ^ Same as 'withProgress', but for processes that do not report the percentage complete
     ,restartShakeSession :: [DelayedAction ()] -> IO ()
+    , ideNc :: IORef NameCache
     }
 
 type WithProgressFunc = forall a.
@@ -373,6 +381,8 @@ shakeOpen getLspId eventer withProgress withIndefiniteProgress logger debouncer
   shakeProfileDir (IdeReportProgress reportProgress) ideTesting@(IdeTesting testing) opts rules = mdo
 
     inProgress <- newVar HMap.empty
+    us <- mkSplitUniqSupply 'r'
+    ideNc <- newIORef (initNameCache us knownKeyNames)
     (shakeExtras, stopProgressReporting) <- do
         globals <- newVar HMap.empty
         state <- newVar HMap.empty
@@ -707,6 +717,11 @@ runIdeAction _herald s i = runReaderT (runIdeActionT i) s
 
 askShake :: IdeAction ShakeExtras
 askShake = ask
+
+mkUpdater :: MaybeT IdeAction NameCacheUpdater
+mkUpdater = do
+  ref <- lift $ ideNc <$> askShake
+  pure $ NCU (upNameCache ref)
 
 -- | A (maybe) stale result now, and an up to date one later
 data FastResult a = FastResult { stale :: Maybe (a,PositionMapping), uptoDate :: IO (Maybe a)  }
--- src/Development/IDE/GHC/Compat.hs
+++ src/Development/IDE/GHC/Compat.hs
@@ -12,9 +12,9 @@
 module Development.IDE.GHC.Compat(
     getHeaderImports,
     HieFileResult(..),
-    HieFile,
+    HieFile(..),
+    NameCacheUpdater(..),
     hieExportNames,
-    hie_module,
     mkHieFile,
     writeHieFile,
     readHieFile,
@@ -49,7 +49,13 @@ module Development.IDE.GHC.Compat(
     HasSrcSpan,
     getLoc,
 
-    module GHC
+    upNameCache,
+
+    module GHC,
+#if MIN_GHC_API_VERSION(8,8,0)
+    module HieTypes,
+    module HieUtils,
+#endif
     ) where
 
 import StringBuffer
@@ -58,6 +64,9 @@ import FieldLabel
 import Fingerprint (Fingerprint)
 import qualified Module
 import Packages
+import Data.IORef
+import HscTypes
+import NameCache
 
 import qualified GHC
 import GHC hiding (
@@ -84,13 +93,10 @@ import Avail
 import ErrUtils (ErrorMessages)
 import FastString (FastString)
 
-#if MIN_GHC_API_VERSION(8,10,0)
-import HscTypes (mi_mod_hash)
-#endif
-
 #if MIN_GHC_API_VERSION(8,8,0)
 import Development.IDE.GHC.HieAst (mkHieFile)
-import HieBin
+import Development.IDE.GHC.HieBin
+import HieUtils
 import HieTypes
 
 supportsHieFiles :: Bool
@@ -101,10 +107,9 @@ hieExportNames = nameListFromAvails . hie_exports
 
 #else
 
+import IfaceEnv
 #if MIN_GHC_API_VERSION(8,6,0)
 import BinIface
-import Data.IORef
-import IfaceEnv
 #else
 import System.IO.Error
 #endif
@@ -113,7 +118,6 @@ import Binary
 import Control.Exception (catch)
 import Data.ByteString (ByteString)
 import GhcPlugins (Hsc, srcErrorMessages)
-import NameCache
 import TcRnTypes
 import System.IO
 import Foreign.ForeignPtr
@@ -127,6 +131,13 @@ hPutStringBuffer hdl (StringBuffer buf len cur)
 
 #endif
 
+upNameCache :: IORef NameCache -> (NameCache -> (NameCache, c)) -> IO c
+#if !MIN_GHC_API_VERSION(8,8,0)
+upNameCache ref upd_fn
+  = atomicModifyIORef' ref upd_fn
+#else
+upNameCache = updNameCache
+#endif
 #if !MIN_GHC_API_VERSION(8,6,0)
 includePathsGlobal, includePathsQuote :: [String] -> [String]
 includePathsGlobal = id
@@ -288,7 +299,7 @@ instance Binary HieFile where
 data HieFileResult = HieFileResult { hie_file_result :: HieFile }
 
 writeHieFile :: FilePath -> HieFile -> IO ()
-readHieFile :: NameCache -> FilePath -> IO (HieFileResult, ())
+readHieFile :: NameCacheUpdater -> FilePath -> IO HieFileResult
 supportsHieFiles :: Bool
 
 #if MIN_GHC_API_VERSION(8,6,0)
@@ -300,9 +311,8 @@ writeHieFile fp hie = do
 
 readHieFile nc fp = do
   bh <- readBinMem fp
-  nc' <- newIORef nc
-  hie_file <- getWithUserData (NCU (atomicModifyIORef' nc')) bh
-  return (HieFileResult hie_file, ())
+  hie_file <- getWithUserData nc bh
+  return (HieFileResult hie_file)
 
 supportsHieFiles = True
 
