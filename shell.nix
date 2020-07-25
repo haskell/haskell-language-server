ff114030b2cf321deee489610ef7c1157c4196ed
Pepe Iborra <pepeiborra@gmail.com>
2020-07-25 14:15:42 +0100

    slightly better shell.nix

--- shell.nix
+++ shell.nix
@@ -1,4 +1,38 @@
-with (import <nixpkgs> {});
+{ sources ? import nix/sources.nix,
+  nixpkgs ? import sources.nixpkgs {},
+  compiler ? "default"
+ }:
+with nixpkgs;
+
+let haskellPackagesForProject = if compiler == "default"
+    then haskellPackages.ghcWithPackages
+    else haskell.packages.${compiler}.ghcWithPackages;
+
+   compilerWithPackages = haskellPackagesForProject(p:
+        with p;
+        [ aeson
+          async
+          extra
+          gitrev
+          haskell-lsp
+          lens
+          network
+          optparse-simple
+          prettyprinter
+          QuickCheck
+          regex-tdfa
+          rope-utf16-splay
+          safe-exceptions
+          shake
+          tasty
+          tasty-golden
+          tasty-hunit
+          tasty-rerun
+          temporary
+          text
+          unordered-containers
+         ]);
+in
 stdenv.mkDerivation {
   name = "haskell-language-server";
   buildInputs = [
@@ -7,6 +41,10 @@ stdenv.mkDerivation {
     ncurses
 
     haskellPackages.cabal-install
+    haskellPackages.hlint
+
+    compilerWithPackages
+
   ];
   src = null;
   shellHook = ''
