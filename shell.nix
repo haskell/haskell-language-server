b7aa138f15f6b58b9db014d670d7bd73a1c8d473
Pepe Iborra <pepeiborra@gmail.com>
2020-07-26 15:14:40 +0100

    Fix compat. with ghc865, add more packages and rationale

--- shell.nix
+++ shell.nix
@@ -1,3 +1,10 @@
+# This shell.nix file is designed for use with cabal build
+# It aims to leverage the nix cache in as much as possible
+# It does not aim to replace Cabal/Stack with Nix
+
+# Before making changes, ensure that the Nix expression works
+# for all the GHC versions supported by the project (8.6.x - 8.10.x)
+
 { sources ? import nix/sources.nix,
   nixpkgs ? import sources.nixpkgs {},
   compiler ? "default"
@@ -8,10 +15,21 @@ let haskellPackagesForProject = if compiler == "default"
     then haskellPackages.ghcWithPackages
     else haskell.packages.${compiler}.ghcWithPackages;
 
-    # these packages fail to build with ghc8101
-    extraPackages = p: if compiler == "ghc8101"
-      then []
-      else [p.haskell-lsp p.lsp-test];
+    extraPackages = p: with p;
+    if compiler == "ghc8101" then 
+          [conduit-extra
+           conduit-parse
+           hie-bios
+           yaml
+          ]
+        else if compiler == "ghc865" then [] else
+         # compiler = ghc88
+          [ haskell-lsp 
+            lsp-test
+            brittany
+            ormolu
+            stylish-haskell
+           ];
 
    compilerWithPackages = haskellPackagesForProject(p:
         with p;
@@ -21,8 +39,6 @@ let haskellPackagesForProject = if compiler == "default"
           base16-bytestring
           blaze-builder
           blaze-markup
-          conduit-extra
-          conduit-parse
           cryptohash-sha1
           data-default
           data-default-class
@@ -34,14 +50,19 @@ let haskellPackagesForProject = if compiler == "default"
           floskell
           fuzzy
           generic-deriving
+          ghc-check
           gitrev
           Glob
+          haddock-library
           happy
           haskell-src-exts
           hslogger
           hspec
+          HsYAML-aeson
           lens
+          megaparsec
           network
+          opentelemetry
           optparse-simple
           QuickCheck
           parsers
@@ -55,16 +76,19 @@ let haskellPackagesForProject = if compiler == "default"
           safe-exceptions
           shake
           sorted-list
+          strict
           tasty
+          tasty-ant-xml
+          tasty-expected-failure
           tasty-golden
           tasty-hunit
           tasty-rerun
           temporary
           text
           typed-process
+          unix-compat
           unordered-containers
           xml
-          yaml
           zlib
          ] ++ extraPackages p);
 in
