93844f99b35c6befe5077cceedd5cd90c63718bb
Pepe Iborra <pepeiborra@gmail.com>
2020-07-26 11:25:44 +0100

    Fix compat. with ghc8101 and add more packages
    - Removing packages that fail to build with ghc8101 (haskell-lsp due to statistics)
    - Adding more prebuilt packages to further leverage the Nix cache

--- shell.nix
+++ shell.nix
@@ -8,30 +8,65 @@ let haskellPackagesForProject = if compiler == "default"
     then haskellPackages.ghcWithPackages
     else haskell.packages.${compiler}.ghcWithPackages;
 
+    # these packages fail to build with ghc8101
+    extraPackages = p: if compiler == "ghc8101"
+      then []
+      else [p.haskell-lsp p.lsp-test];
+
    compilerWithPackages = haskellPackagesForProject(p:
         with p;
         [ aeson
+          alex
           async
+          base16-bytestring
+          blaze-builder
+          blaze-markup
+          conduit-extra
+          conduit-parse
+          cryptohash-sha1
+          data-default
+          data-default-class
+          data-default-instances-containers
+          data-default-instances-dlist
+          data-default-instances-old-locale
+          Diff
           extra
+          floskell
+          fuzzy
+          generic-deriving
           gitrev
-          haskell-lsp
+          Glob
+          happy
+          haskell-src-exts
+          hslogger
+          hspec
           lens
           network
           optparse-simple
-          prettyprinter
           QuickCheck
+          parsers
+          parser-combinators
+          prettyprinter
+          prettyprinter-ansi-terminal
+          primes
+          psqueues
           regex-tdfa
           rope-utf16-splay
           safe-exceptions
           shake
+          sorted-list
           tasty
           tasty-golden
           tasty-hunit
           tasty-rerun
           temporary
           text
+          typed-process
           unordered-containers
-         ]);
+          xml
+          yaml
+          zlib
+         ] ++ extraPackages p);
 in
 stdenv.mkDerivation {
   name = "haskell-language-server";
