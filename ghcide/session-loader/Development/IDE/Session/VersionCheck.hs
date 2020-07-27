f1b36bac0462020725958d093474ca390ae870e9
Luke Lau <luke_lau@icloud.com>
2020-07-27 08:10:25 +0100

    Move session loading logic into ghcide library (haskell/ghcide#697)
    * Split out the session loading logic into a sublibrary
    
    This way haskell-language-server can also reuse this logic.
    Note that this sublibrary is public so it requires cabal-version: 3.0
    Part of the work towards haskell/ghcide#478
    
    * Move Development.IDE.Session into ghcide itself
    
    Sublibraries do not seem to play well. Hide this behind the ghc-lib flag
    so that the Haskell specific hie-bios stuff can be disabled
    
    Note that we need to put the template-haskell part of this module into a
    separate module because of an access exception when compiling with
    Stack, GHC 8.10.1 and Windows.

---
+++ ghcide/session-loader/Development/IDE/Session/VersionCheck.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+-- | This module exists to circumvent a compile time exception on Windows with
+-- Stack and GHC 8.10.1. It's just been pulled out from Development.IDE.Session.
+-- See https://github.com/digital-asset/ghcide/pull/697
+module Development.IDE.Session.VersionCheck (ghcVersionChecker) where
+
+import Data.Maybe
+import GHC.Check
+-- Only use this for checking against the compile time GHC libDir!
+-- Use getRuntimeGhcLibDir from hie-bios instead for everything else
+-- otherwise binaries will not be distributable since paths will be baked into them
+import qualified GHC.Paths
+import System.Environment
+
+ghcVersionChecker :: GhcVersionChecker
+ghcVersionChecker = $$(makeGhcVersionChecker (fromMaybe GHC.Paths.libdir <$> lookupEnv "NIX_GHC_LIBDIR"))
