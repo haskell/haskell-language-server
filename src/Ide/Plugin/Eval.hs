92ceb7ae8fdd12526c1bf7feeed3437aa3477b5e
Hiromi ISHII <konn.jinro@gmail.com>
2020-08-22 00:45:52 +0900

    Adds `:kind` command that returns kind only without normalisation

--- src/Ide/Plugin/Eval.hs
+++ src/Ide/Plugin/Eval.hs
@@ -87,6 +87,7 @@ import           Control.DeepSeq                ( NFData
                                                 , deepseq
                                                 )
 import Outputable (Outputable(ppr), showSDoc)
+import Control.Applicative ((<|>))
 
 descriptor :: PluginId -> PluginDescriptor
 descriptor plId =
@@ -247,15 +248,17 @@ done, we want to switch back to GhcSessionDeps:
     df <- liftIO $ evalGhcEnv hscEnv' getSessionDynFlags
     let eval (stmt, l)
           | let stmt0 = T.strip $ T.pack stmt -- For stripping and de-prefixing
-          , Just type_ <- T.stripPrefix ":kind! " stmt0
+          , Just (reduce, type_) <-
+                  (True,) <$> T.stripPrefix ":kind! " stmt0
+              <|> (False,) <$> T.stripPrefix ":kind " stmt0
           = do
             let input = T.strip type_
-            (ty, kind) <- typeKind True $ T.unpack input
+            (ty, kind) <- typeKind reduce $ T.unpack input
             pure $ Just
               $ T.unlines 
               $ map ("-- " <>)
-              [input <> " :: " <> T.pack (showSDoc df $ ppr kind)
-              , " = " <> T.pack (showSDoc df $ ppr ty)]
+              $ (input <> " :: " <> T.pack (showSDoc df $ ppr kind))
+              : [ " = " <> T.pack (showSDoc df $ ppr ty) | reduce]
           | isStmt df stmt = do
             -- set up a custom interactive print function
             liftIO $ writeFile temp ""
