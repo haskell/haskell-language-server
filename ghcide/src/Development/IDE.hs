2b251e50dc2b847dfb8894e78ddef0d56be29a8e
Pepe Iborra <pepeiborra@me.com>
2020-09-02 19:28:07 +0100

    module Development.IDE (haskell/ghcide#724)
    A single module to reexport all the commonly used names to simplify the use of
    ghcide as a library

---
+++ ghcide/src/Development/IDE.hs
@@ -0,0 +1,46 @@
+module Development.IDE
+(
+    -- TODO It would be much nicer to enumerate all the exports
+    -- and organize them in sections
+    module X
+
+) where
+
+import Development.IDE.Core.RuleTypes as X
+import Development.IDE.Core.Rules as X
+  (GhcSessionIO(..)
+  ,getAtPoint
+  ,getDefinition
+  ,getParsedModule
+  ,getTypeDefinition
+  )
+import Development.IDE.Core.FileExists as X
+  (getFileExists)
+import Development.IDE.Core.FileStore as X
+  (getFileContents)
+import Development.IDE.Core.IdeConfiguration as X
+  (IdeConfiguration(..)
+  ,isWorkspaceFile)
+import Development.IDE.Core.OfInterest as X (getFilesOfInterest)
+import Development.IDE.Core.Service as X (runAction)
+import Development.IDE.Core.Shake as X
+  ( IdeState,
+    shakeExtras,
+    ShakeExtras,
+    IdeRule,
+    define,
+    GetModificationTime(GetModificationTime),
+    use, useNoFile, uses, useWithStaleFast, useWithStaleFast',
+    FastResult(..),
+    use_, useNoFile_, uses_,
+    ideLogger,
+    actionLogger,
+    IdeAction(..), runIdeAction
+  )
+import Development.IDE.GHC.Error as X
+import Development.IDE.GHC.Util as X
+import Development.IDE.Plugin as X
+import Development.IDE.Types.Diagnostics as X
+import Development.IDE.Types.Location as X
+import Development.IDE.Types.Logger as X
+import Development.Shake as X (Action, action, Rules, RuleResult)
