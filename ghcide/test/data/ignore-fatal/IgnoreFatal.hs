6b1f2c055b5ea6dc7d9866e4b8e44b57c374245e
Ziyang Liu <unsafeFixIO@gmail.com>
2020-09-02 21:34:14 -0700

    Add a test case involving -fno-warn-missing-signatures (haskell/ghcide#720)
    * Only enable non-fatal warnings
    
    * Revert the change since it has been taken care of in haskell/ghcide#738

---
+++ ghcide/test/data/ignore-fatal/IgnoreFatal.hs
@@ -0,0 +1,8 @@
+-- "missing signature" is declared a fatal warning in the cabal file,
+-- but is ignored in this module.
+
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+
+module IgnoreFatal where
+
+a = 'a'
