39ae56baeaa491a0d82bf0c60e7457633edfdfd1
wz1000 <zubin.duggal@gmail.com>
2020-09-03 14:03:14 +0530

    Expose `getCompletionsLSP` to allow completions in hls (#756)
    Co-authored-by: Koray Al <koray.al@soostone.com.tr>

--- src/Development/IDE/Plugin/Completions.hs
+++ src/Development/IDE/Plugin/Completions.hs
@@ -2,7 +2,11 @@
 {-# LANGUAGE TypeFamilies #-}
 #include "ghc-api-version.h"
 
-module Development.IDE.Plugin.Completions(plugin) where
+module Development.IDE.Plugin.Completions
+    (
+      plugin
+    , getCompletionsLSP
+    ) where
 
 import Control.Applicative
 import Language.Haskell.LSP.Messages
