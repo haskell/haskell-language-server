09aa8e5f4b4ba26981fdb34afa550ec4eaeaeb4b
Pepe Iborra <pepeiborra@gmail.com>
2020-09-03 09:32:40 +0100

    Suggestions for missing imports from local modules (#739)
    * Suggestions for missing imports from local modules
    
    * Avoid unnecessary work on InitialLoad when checkProject is off

--- src/Development/IDE/Plugin/CodeAction/Rules.hs
+++ src/Development/IDE/Plugin/CodeAction/Rules.hs
@@ -3,26 +3,17 @@ module Development.IDE.Plugin.CodeAction.Rules
   )
 where
 
-import           Data.HashMap.Strict            ( fromListWith )
-import           Data.Text                      ( Text
-                                                , pack
-                                                )
 import           Data.Traversable               ( forM )
 import           Development.IDE.Core.Rules
 import           Development.IDE.GHC.Util
 import           Development.IDE.Plugin.CodeAction.RuleTypes
+import           Development.IDE.Types.Exports
 import           Development.Shake
 import           GHC                            ( DynFlags(pkgState) )
-import           HscTypes                       ( IfaceExport
-                                                , hsc_dflags
-                                                , mi_exports
-                                                )
+import           HscTypes                       ( hsc_dflags)
 import           LoadIface
 import           Maybes
-import           Module                         ( Module(..)
-                                                , ModuleName
-                                                , moduleNameString
-                                                )
+import           Module                         ( Module(..) )
 import           Packages                       ( explicitPackages
                                                 , exposedModules
                                                 , packageConfigId
@@ -43,19 +34,12 @@ rulePackageExports = defineNoFile $ \(PackageExports session) -> do
         , (mn, _)  <- exposedModules pkg
         ]
 
-  results <- forM targets $ \(pkg, mn) -> do
+  modIfaces <- forM targets $ \(pkg, mn) -> do
     modIface <- liftIO $ initIfaceLoad env $ loadInterface
       ""
       (Module (packageConfigId pkg) mn)
       (ImportByUser False)
-    case modIface of
-      Failed    _err -> return mempty
-      Succeeded mi   -> do
-        let avails = mi_exports mi
-        return $ concatMap (unpackAvail mn) avails
-  return $ fromListWith (++) $ concat results
-
-unpackAvail :: ModuleName -> IfaceExport -> [(Text, [(IdentInfo, Text)])]
-unpackAvail mod =
-  map (\id@IdentInfo {..} -> (name, [(id, pack $ moduleNameString mod)]))
-    . mkIdentInfos
+    return $ case modIface of
+      Failed    _err -> Nothing
+      Succeeded mi   -> Just mi
+  return $ createExportsMap (catMaybes modIfaces)
