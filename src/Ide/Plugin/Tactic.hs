2b7040d99318fcc510974dfb55f0b3515afd4b0a
TOTBWF <reed.mullanix@calabrio.com>
2020-09-06 11:56:13 -0700

    [WIP] Add more to the code action provider

--- src/Ide/Plugin/Tactic.hs
+++ src/Ide/Plugin/Tactic.hs
@@ -10,9 +10,20 @@ module Ide.Plugin.Tactic
 import           Data.Aeson
 import qualified Data.HashMap.Strict             as H
 import qualified GHC.Generics                    as Generics
+
+import           Development.IDE.Core.RuleTypes (GhcSessionDeps (GhcSessionDeps),
+                                                 TcModuleResult (tmrModule),
+                                                 TypeCheck (TypeCheck))
+import           Development.IDE.Core.Shake     (use, IdeState (..))
+import           Development.IDE.Core.Service (runAction)
+import           Development.Shake (Action)
+
 import           Ide.Types
+
 import qualified Language.Haskell.LSP.Types      as J
 import           Language.Haskell.LSP.Types
+
+-- import Refinery.Tactic
 
 descriptor :: PluginId -> PluginDescriptor
 descriptor plId = (defaultPluginDescriptor plId)
@@ -20,11 +31,18 @@ descriptor plId = (defaultPluginDescriptor plId)
     , pluginCodeActionProvider = Just codeActionProvider
     }
 
-codeActionProvider :: CodeActionProvider
-codeActionProvider _conf _ide _plid (TextDocumentIdentifier uri) range _ctx = _hole
+runIde :: IdeState -> Action a -> IO a
+runIde state = runAction "tactic" state
 
--- codeAction :: CodeActionProvider
--- codeAction _lf _state _pid _tid _range 
+codeActionProvider :: CodeActionProvider
+codeActionProvider _conf state _plid (TextDocumentIdentifier uri) range _ctx
+    | Just nfp <- uriToNormalizedFilePath $ toNormalizedUri uri = do
+          tmr <- runIde state $ use TypeCheck nfp
+          hsc <- runIde state $ use GhcSessionDeps nfp
+          -- TODO Check if we have a hole inside of the range.
+          -- Go from range -> SrcSpan, then look inside the module contents
+          _hole
+
 
 data TacticParams = TacticParams
     { file :: J.Uri -- ^ Uri of the file to fill the hole in
@@ -36,7 +54,7 @@ tacticCmd :: CommandFunction TacticParams
 tacticCmd _lf _ide (TacticParams uri range) = do
     let
       textEdits = J.List
-        [J.TextEdit range _solution
+        [J.TextEdit range ("Howdy Partner!")
         ]
       res = J.WorkspaceEdit
         (Just $ H.singleton uri textEdits)
