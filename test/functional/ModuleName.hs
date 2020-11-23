0d707a9be485d7e50d8d670cf1e326c7416ebede
Hiromi ISHII <konn.jinro@gmail.com>
2020-11-23 15:47:49 +0900

    module name plugin: Workaround for modules starting with lowercase

--- test/functional/ModuleName.hs
+++ test/functional/ModuleName.hs
@@ -7,38 +7,26 @@ module ModuleName
   )
 where
 
-import           Control.Applicative.Combinators
-                                                ( skipManyTill )
-import           Control.Monad.IO.Class         ( MonadIO(liftIO) )
-import qualified Data.Text.IO                  as T
-import           Language.Haskell.LSP.Test      ( fullCaps
-                                                , documentContents
-                                                , executeCommand
-                                                , getCodeLenses
-                                                , openDoc
-                                                , runSession
-                                                , anyMessage
-                                                , message
-                                                )
-import           Language.Haskell.LSP.Types     ( ApplyWorkspaceEditRequest
-                                                , CodeLens(..)
-                                                )
-import           System.FilePath                ( (<.>)
-                                                , (</>)
-                                                )
-import           Test.Hls.Util                  ( hlsCommand )
-import           Test.Tasty                     ( TestTree
-                                                , testGroup
-                                                )
-import           Test.Tasty.HUnit               ( testCase
-                                                , (@?=)
-                                                )
+import           Control.Applicative.Combinators (skipManyTill)
+import           Control.Monad.IO.Class          (MonadIO (liftIO))
+import qualified Data.Text.IO                    as T
+import           Language.Haskell.LSP.Test       (anyMessage, documentContents,
+                                                  executeCommand, fullCaps,
+                                                  getCodeLenses, message,
+                                                  openDoc, runSession)
+import           Language.Haskell.LSP.Types      (ApplyWorkspaceEditRequest,
+                                                  CodeLens (..))
+import           System.FilePath                 ((<.>), (</>))
+import           Test.Hls.Util                   (hlsCommand)
+import           Test.Tasty                      (TestTree, testGroup)
+import           Test.Tasty.HUnit                (testCase, (@?=))
 
 tests :: TestTree
 tests = testGroup
   "moduleName"
   [ testCase "Add module header to empty module" $ goldenTest "TEmptyModule.hs"
   , testCase "Fix wrong module name" $ goldenTest "TWrongModuleName.hs"
+  , testCase "Must infer module name as Main, if the file name starts with a lowercase" $ goldenTest "mainlike.hs"
   ]
 
 goldenTest :: FilePath -> IO ()
