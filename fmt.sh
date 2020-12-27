a9c2a0a4662b1d70ddd8479e350c9cb224d011cd
Pepe Iborra <pepeiborra@gmail.com>
2020-12-27 14:23:52 +0000

    Combine test and nix CI scripts

---
+++ fmt.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -eou pipefail
+curl -sSL https://raw.github.com/ndmitchell/hlint/master/misc/run.sh | sh -s ghcide/src ghcide/exe ghcide/bench shake-bench/src ghcide/test/exe --with-group=extra --hint=ghcide/.hlint.yaml
