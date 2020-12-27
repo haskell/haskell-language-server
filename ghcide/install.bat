a785029d454aca5c48011165647051efca572ea5
Pepe Iborra <pepeiborra@gmail.com>
2020-12-27 14:05:37 +0000

    Delete redundant descriptors and scripts

--- ghcide/install.bat
+++
@@ -1,6 +0,0 @@
-:: Copyright (c) 2019 The DAML Authors. All rights reserved.
-:: SPDX-License-Identifier: Apache-2.0
-
-@REM Install ghcide where cabal install would put it on Windows
-@REM but avoid checking configure or installing local libraries (faster)
-ghc Main -o dist\obj\ghcide.exe -XBangPatterns -XDeriveGeneric -XGeneralizedNewtypeDeriving -XLambdaCase -XNamedFieldPuns -XRecordWildCards -XScopedTypeVariables -XStandaloneDeriving -XTupleSections -XTypeApplications -XViewPatterns -package=ghc -DGHC_STABLE -isrc -iexe -outputdir dist\obj && copy dist\obj\ghcide.exe %AppData%\cabal\bin\ghcide.exe
