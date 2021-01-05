0b0a6a75e5599c2e7b8638df334ac1449ebe7504
Peter Wicks Stringfield <peterwicksstringfield@gmail.com>
2021-01-04 22:24:58 -0600

    Enable more tests (#1143)
    * Make rename tests compile.
    
    (They can't pass since we don't have a renamer yet.)
    
    * Enable some more tests.
    
    Co-authored-by: Pepe Iborra <pepeiborra@gmail.com>
    Co-authored-by: Javier Neira <atreyu.bbb@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- test/functional/HieBios.hs
+++ test/functional/HieBios.hs
@@ -1,36 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
 module HieBios (tests) where
 
-import Control.Applicative.Combinators
+import Control.Lens ((^.))
+import Control.Monad.IO.Class
 import qualified Data.Text as T
 import Language.Haskell.LSP.Test
 import Language.Haskell.LSP.Types
-import Language.Haskell.LSP.Messages
+import qualified Language.Haskell.LSP.Types.Lens as L
 import System.FilePath ((</>))
 import Test.Hls.Util
 import Test.Tasty
-import Test.Tasty.ExpectedFailure (ignoreTestBecause)
 import Test.Tasty.HUnit
 
 tests :: TestTree
 tests = testGroup "hie-bios" [
-    ignoreTestBecause "Broken" $ testCase "loads modules inside main-is" $ do
+    testCase "loads modules inside main-is" $ do
         writeFile (hieBiosErrorPath </> "hie.yaml") ""
         runSession hlsCommand fullCaps "test/testdata/hieBiosMainIs" $ do
-            _ <- openDoc "Main.hs" "haskell"
-            _ <- count 2 waitForDiagnostics
-            return ()
+            doc <- openDoc "Main.hs" "haskell"
+            Just mainHoverText <- getHover doc (Position 3 1)
+            let (HoverContents (MarkupContent _ x)) = mainHoverText ^. L.contents
+            liftIO $ "main :: IO ()" `T.isInfixOf` x
+                     @? "found hover text for main"
 
-    , ignoreTestBecause "Broken" $ testCase "reports errors in hie.yaml" $ do
+    , testCase "reports errors in hie.yaml" $ do
         writeFile (hieBiosErrorPath </> "hie.yaml") ""
         runSession hlsCommand fullCaps hieBiosErrorPath $ do
             _ <- openDoc "Foo.hs" "haskell"
-            _ <- skipManyTill loggingNotification (satisfy isMessage)
-            return ()
+            (diag:_) <- waitForDiagnostics
+            liftIO $ "Expected a cradle: key containing the preferences" `T.isInfixOf` (diag ^. L.message)
+                     @? "Error reported"
     ]
     where
         hieBiosErrorPath = "test/testdata/hieBiosError"
-
-        isMessage (NotShowMessage (NotificationMessage _ _ (ShowMessageParams MtError s))) =
-            "Couldn't parse hie.yaml" `T.isInfixOf` s
-        isMessage _ = False
