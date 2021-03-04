e51c44173b5d064b122d39664e659ac3ac4a3795
Sandy Maguire <sandy@sandymaguire.me>
2021-03-03 20:37:18 -0800

    Cleanup the ExactPrint changes

--- ghcide/src/Development/IDE/GHC/ExactPrint.hs
+++ ghcide/src/Development/IDE/GHC/ExactPrint.hs
@@ -9,7 +9,6 @@ module Development.IDE.GHC.ExactPrint
     ( Graft(..),
       graft,
       graftExpr,
-      graftWithoutParentheses,
       graftDecls,
       graftDeclsWithM,
       annotate,
@@ -66,10 +65,9 @@ import Parser (parseIdentifier)
 import Data.Traversable (for)
 import Data.Foldable (Foldable(fold))
 import Data.Bool (bool)
-import GhcPlugins (PprPrec)
-import Data.Maybe (catMaybes)
 #if __GLASGOW_HASKELL__ == 808
 import Control.Arrow
+import Data.Monoid (All(All))
 #endif
 #if __GLASGOW_HASKELL__ > 808
 import Bag (listToBag)
@@ -182,33 +180,39 @@ transformM dflags ccs uri f a = runExceptT $
         pure $ diffText ccs (uri, T.pack src) (T.pack res) IncludeDeletions
 
 
-needsParens :: HsExpr GhcPs -> Maybe PprPrec
-needsParens HsLam{}         = Nothing
-needsParens HsLamCase{}     = Nothing
-needsParens HsApp{}         = Just appPrec
-needsParens HsAppType{}     = Just appPrec
-needsParens OpApp{}         = Just appPrec
-needsParens HsPar{}         = Nothing
-needsParens SectionL{}      = Nothing
-needsParens SectionR{}      = Nothing
-needsParens ExplicitTuple{} = Nothing
-needsParens ExplicitSum{}   = Nothing
-needsParens HsCase{}        = Nothing
-needsParens HsIf{}          = Nothing
-needsParens HsMultiIf{}     = Nothing
-needsParens HsLet{}         = Nothing
-needsParens HsDo{}          = Nothing
-needsParens ExplicitList{}  = Nothing
-needsParens RecordCon{}     = Nothing
-needsParens RecordUpd{}     = Just appPrec
-needsParens _               = Just appPrec
+-- | Returns whether or not this node requires its immediate children to have a
+needsParensSpace ::
+    HsExpr GhcPs ->
+    -- | (Needs parens, needs space)
+    (All, All)
+needsParensSpace HsLam{}         = (All False, All False)
+needsParensSpace HsLamCase{}     = (All False, All False)
+needsParensSpace HsApp{}         = mempty
+needsParensSpace HsAppType{}     = mempty
+needsParensSpace OpApp{}         = mempty
+needsParensSpace HsPar{}         = (All False, All False)
+needsParensSpace SectionL{}      = (All False, All False)
+needsParensSpace SectionR{}      = (All False, All False)
+needsParensSpace ExplicitTuple{} = (All False, All False)
+needsParensSpace ExplicitSum{}   = (All False, All False)
+needsParensSpace HsCase{}        = (All False, All False)
+needsParensSpace HsIf{}          = (All False, All False)
+needsParensSpace HsMultiIf{}     = (All False, All False)
+needsParensSpace HsLet{}         = (All False, All False)
+needsParensSpace HsDo{}          = (All False, All False)
+needsParensSpace ExplicitList{}  = (All False, All False)
+needsParensSpace RecordCon{}     = (All False, All False)
+needsParensSpace RecordUpd{}     = mempty
+needsParensSpace _               = mempty
 
 
 ------------------------------------------------------------------------------
 
 {- | Construct a 'Graft', replacing the node at the given 'SrcSpan' with the
- given 'LHSExpr'. The node at that position must already be a 'LHsExpr', or
- this is a no-op.
+ given @Located ast@. The node at that position must already be a @Located
+ ast@, or this is a no-op.
+
+ You want to use 'graftExpr' instead of this function when @ast ~ 'HsExpr'@.
 -}
 graft ::
     forall ast a.
@@ -216,18 +220,26 @@ graft ::
     SrcSpan ->
     Located ast ->
     Graft (Either String) a
-graft dst = graftWithoutParentheses dst . maybeParensAST
+graft dst = graft' True dst . maybeParensAST
 
--- | Like 'graft', but trusts that you have correctly inserted the parentheses
--- yourself. If you haven't, the resulting AST will not be valid!
-graftWithoutParentheses ::
+graft' ::
     forall ast a.
     (Data a, ASTElement ast) =>
+    -- | Do we need to insert a space before this grafting? In do blocks, the
+    -- answer is no, or we will break layout. But in function applications,
+    -- the answer is yes, or the function call won't get its argument. Yikes!
+    --
+    -- More often the answer is yes, so when in doubt, use that.
+    --
+    -- For a version of this function that does the right thing for
+    -- expressions without needing to tweak this parameter, look at
+    -- 'graftExpr'.
+    Bool ->
     SrcSpan ->
     Located ast ->
     Graft (Either String) a
-graftWithoutParentheses dst val = Graft $ \dflags a -> do
-    (anns, val') <- annotate dflags val
+graft' needs_space dst val = Graft $ \dflags a -> do
+    (anns, val') <- annotate dflags needs_space val
     modifyAnnsT $ mappend anns
     pure $
         everywhere'
@@ -250,26 +262,18 @@ graftExpr dst val = Graft $ \dflags a -> do
     -- Traverse the tree, looking for our replacement node. But keep track of
     -- the context (parent HsExpr constructor) we're in while we do it. This
     -- lets us determine wehther or not we need parentheses.
-    let do_i_need_parens =
-          everythingWithContext (Nothing :: Maybe PprPrec) (<>)
-            ( mkQ ([], ) $ \x s -> case x of
+    let (All needs_parens, All needs_space) =
+          everythingWithContext (All True, All True) (<>)
+            ( mkQ (mempty, ) $ \x s -> case x of
                 (L src _ :: LHsExpr GhcPs) | src == dst ->
-                  ([s], s)
-                L _ x' -> ([], needsParens x')
+                  (s, s)
+                L _ x' -> (mempty, needsParensSpace x')
             ) a
 
-    let needs_parens = not $ null $ catMaybes do_i_need_parens
-
-    (anns, val') <- annotate2 dflags needs_parens $ bool id maybeParensAST needs_parens val
-    modifyAnnsT $ mappend anns
-    pure $
-        everywhere'
-            ( mkT $
-                \case
-                    (L src _ :: Located ast) | src == dst -> val'
-                    l -> l
-            )
-            a
+    runGraft
+      (graft' needs_space dst $ bool id maybeParensAST needs_parens val)
+      dflags
+      a
 
 
 ------------------------------------------------------------------------------
@@ -291,7 +295,7 @@ graftWithM dst trans = Graft $ \dflags a -> do
                             Just val' -> do
                                 (anns, val'') <-
                                     hoistTransform (either Fail.fail pure) $
-                                        annotate dflags $ maybeParensAST val'
+                                        annotate dflags True $ maybeParensAST val'
                                 modifyAnnsT $ mappend anns
                                 pure val''
                             Nothing -> pure val
@@ -316,7 +320,7 @@ graftWithSmallestM dst trans = Graft $ \dflags a -> do
                             Just val' -> do
                                 (anns, val'') <-
                                     hoistTransform (either Fail.fail pure) $
-                                        annotate dflags $ maybeParensAST val'
+                                        annotate dflags True $ maybeParensAST val'
                                 modifyAnnsT $ mappend anns
                                 pure val''
                             Nothing -> pure val
@@ -453,18 +457,8 @@ fixAnns ParsedModule {..} =
 
 -- | Given an 'LHSExpr', compute its exactprint annotations.
 --   Note that this function will throw away any existing annotations (and format)
-annotate :: ASTElement ast => DynFlags -> Located ast -> TransformT (Either String) (Anns, Located ast)
-annotate dflags ast = do
-    uniq <- show <$> uniqueSrcSpanT
-    let rendered = render dflags ast
-    (anns, expr') <- lift $ mapLeft show $ parseAST dflags uniq rendered
-    let anns' = setPrecedingLines expr' 0 1 anns
-    pure (anns', expr')
-
--- | Given an 'LHSExpr', compute its exactprint annotations.
---   Note that this function will throw away any existing annotations (and format)
-annotate2 :: ASTElement ast => DynFlags -> Bool -> Located ast -> TransformT (Either String) (Anns, Located ast)
-annotate2 dflags needs_space ast = do
+annotate :: ASTElement ast => DynFlags -> Bool -> Located ast -> TransformT (Either String) (Anns, Located ast)
+annotate dflags needs_space ast = do
     uniq <- show <$> uniqueSrcSpanT
     let rendered = render dflags ast
     (anns, expr') <- lift $ mapLeft show $ parseAST dflags uniq rendered
