2f845e80d95ce6dcc979e8e7a6aa491f72d5e435
Sandy Maguire <sandy@sandymaguire.me>
2021-03-07 13:57:37 -0800

    Forgot to exclude `fail` from method choices

--- plugins/hls-tactics-plugin/src/Ide/Plugin/Tactic/Context.hs
+++ plugins/hls-tactics-plugin/src/Ide/Plugin/Tactic/Context.hs
@@ -3,16 +3,14 @@
 
 module Ide.Plugin.Tactic.Context where
 
-import           Bag
-import           Control.Arrow
-import           Control.Monad.Reader
-import           Data.Set (Set)
-import qualified Data.Set as S
-import           Development.IDE.GHC.Compat
-import           Ide.Plugin.Tactic.FeatureSet (FeatureSet)
-import           Ide.Plugin.Tactic.Types
-import           OccName
-import           TcRnTypes
+import Bag
+import Control.Arrow
+import Control.Monad.Reader
+import Development.IDE.GHC.Compat
+import Ide.Plugin.Tactic.FeatureSet (FeatureSet)
+import Ide.Plugin.Tactic.Types
+import OccName
+import TcRnTypes
 
 
 mkContext :: FeatureSet -> [(OccName, CType)] -> TcGblEnv -> Context
@@ -25,20 +23,6 @@ mkContext features locals tcg = Context
                    $ tcg_binds tcg
   , ctxFeatureSet = features
   }
-
-
-------------------------------------------------------------------------------
--- | Many operations are defined in typeclasses for performance reasons, rather
--- than being a true part of the class. This function filters out those, in
--- order to keep our hypothesis space small.
-excludeForbiddenMethods :: [HyInfo a] -> [HyInfo a]
-excludeForbiddenMethods = filter (not . flip S.member forbiddenMethods . hi_name)
-  where
-    forbiddenMethods :: Set OccName
-    forbiddenMethods = S.map mkVarOcc $ S.fromList
-      [ -- monadfail
-        "fail"
-      ]
 
 
 splitId :: Id -> (OccName, CType)
--- plugins/hls-tactics-plugin/src/Ide/Plugin/Tactic/Judgements/Theta.hs
+++ plugins/hls-tactics-plugin/src/Ide/Plugin/Tactic/Judgements/Theta.hs
@@ -4,12 +4,14 @@ module Ide.Plugin.Tactic.Judgements.Theta
   ( getMethodHypothesisAtHole
   ) where
 
-import Data.Maybe (fromMaybe)
-import Development.IDE.GHC.Compat
-import Generics.SYB
-import GhcPlugins (EvVar)
-import Ide.Plugin.Tactic.Machinery
-import Ide.Plugin.Tactic.Types
+import           Data.Maybe (fromMaybe)
+import           Data.Set (Set)
+import qualified Data.Set as S
+import           Development.IDE.GHC.Compat
+import           Generics.SYB
+import           GhcPlugins (EvVar, mkVarOcc)
+import           Ide.Plugin.Tactic.Machinery
+import           Ide.Plugin.Tactic.Types
 
 
 ------------------------------------------------------------------------------
@@ -19,9 +21,24 @@ import Ide.Plugin.Tactic.Types
 getMethodHypothesisAtHole :: SrcSpan -> LHsBinds GhcTc -> Hypothesis CType
 getMethodHypothesisAtHole dst
   = Hypothesis
+  . excludeForbiddenMethods
   . fromMaybe []
   . foldMap methodHypothesis
   . (everything (<>) $ mkQ mempty $ evbinds dst)
+
+
+------------------------------------------------------------------------------
+-- | Many operations are defined in typeclasses for performance reasons, rather
+-- than being a true part of the class. This function filters out those, in
+-- order to keep our hypothesis space small.
+excludeForbiddenMethods :: [HyInfo a] -> [HyInfo a]
+excludeForbiddenMethods = filter (not . flip S.member forbiddenMethods . hi_name)
+  where
+    forbiddenMethods :: Set OccName
+    forbiddenMethods = S.map mkVarOcc $ S.fromList
+      [ -- monadfail
+        "fail"
+      ]
 
 
 ------------------------------------------------------------------------------
