58d4017d09f5fc15de17b2308824b71c6dde1f1d
Sandy Maguire <sandy@sandymaguire.me>
2021-04-14 22:47:20 -0700

    Add EmptyCase tests

---
+++ plugins/hls-tactics-plugin/test/CodeLens/EmptyCaseSpec.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE ViewPatterns          #-}
+
+module CodeLens.EmptyCaseSpec where
+
+import Test.Hspec
+import Utils
+import Wingman.FeatureSet (allFeatures)
+
+
+spec :: Spec
+spec = do
+  let test = mkCodeLensTest allFeatures
+
+  describe "golden" $ do
+    test "EmptyCaseADT.hs"
+    test "EmptyCaseShadow.hs"
+    test "EmptyCaseParens.hs"
+    test "EmptyCaseNested.hs"
+    test "EmptyCaseApply.hs"
+
