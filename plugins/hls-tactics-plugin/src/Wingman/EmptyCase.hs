0ad5186cae63a6aa2173efe7a3facf8597768887
Sandy Maguire <sandy@sandymaguire.me>
2021-04-14 21:35:50 -0700

    Move EmptyCase into its own module

---
+++ plugins/hls-tactics-plugin/src/Wingman/EmptyCase.hs
@@ -0,0 +1,111 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | A plugin that uses tactics to synthesize code
+module Wingman.EmptyCase where
+
+import           Control.Monad
+import           Control.Monad.Trans
+import           Control.Monad.Trans.Maybe
+import           Data.Aeson
+import           Data.Maybe
+import qualified Data.Text as T
+import           Data.Traversable (for)
+import           Development.IDE (realSrcSpanToRange, GetBindings(..))
+import           Development.IDE.Core.Shake (IdeState (..))
+import           Development.IDE.Core.UseStale (TrackedStale(..), unTrack, mapAgeFrom)
+import           Development.IDE.GHC.Compat
+import           Development.IDE.GHC.ExactPrint
+import           Development.IDE.Spans.LocalBindings (getLocalScope)
+import           Ide.Types
+import           Language.LSP.Server
+import           Language.LSP.Types
+import           OccName
+import           Prelude hiding (span)
+import           Wingman.CodeGen (destructionFor)
+import           Wingman.GHC
+import           Wingman.Judgements (hySingleton)
+import           Wingman.LanguageServer
+import           Wingman.Types
+
+
+emptyCaseLensCommandId :: CommandId
+emptyCaseLensCommandId = CommandId "wingman.emptyCase"
+
+
+workspaceEditHandler :: CommandFunction IdeState WorkspaceEdit
+workspaceEditHandler _ideState wedit = do
+  _ <- sendRequest SWorkspaceApplyEdit (ApplyWorkspaceEditParams Nothing wedit) (\_ -> pure ())
+  return $ Right Null
+
+
+codeLensProvider :: PluginMethodHandler IdeState TextDocumentCodeLens
+codeLensProvider state plId (CodeLensParams _ _ (TextDocumentIdentifier uri))
+  | Just nfp <- uriToNormalizedFilePath $ toNormalizedUri uri = do
+      cfg <- getTacticConfig plId
+      ccs <- getClientCapabilities
+      liftIO $ fromMaybeT (Right $ List []) $ do
+        dflags <- getIdeDynflags state nfp
+
+        TrackedStale pm _ <- runStaleIde state nfp GetAnnotatedParsedSource
+        TrackedStale binds bind_map <- runStaleIde state nfp GetBindings
+        holes <- completionForHole  state nfp $ cfg_feature_set cfg
+
+        fmap (Right . List) $ for holes $ \(ss, ty) -> do
+          binds_ss <- liftMaybe $ mapAgeFrom bind_map ss
+          let bindings = getLocalScope (unTrack binds) $ unTrack binds_ss
+              range = realSrcSpanToRange $ unTrack ss
+          matches <-
+            liftMaybe $
+              destructionFor
+                (foldMap (hySingleton . occName . fst) bindings)
+                ty
+          edits <- liftMaybe $ hush $
+                mkWorkspaceEdits dflags ccs uri (unTrack pm) $
+                  graftMatchGroup (RealSrcSpan $ unTrack ss) $
+                    noLoc matches
+
+          pure $
+            CodeLens range
+              (Just
+                $ mkLspCommand
+                    plId
+                    emptyCaseLensCommandId
+                    (mkEmptyCaseLensDesc ty)
+                $ Just $ pure $ toJSON $ edits
+              )
+              Nothing
+codeLensProvider _ _ _ = pure $ Right $ List []
+
+
+mkEmptyCaseLensDesc :: Type -> T.Text
+mkEmptyCaseLensDesc ty =
+  "Complete case constructors (" <> T.pack (unsafeRender ty) <> ")"
+
+
+------------------------------------------------------------------------------
+-- | Silence an error.
+hush :: Either e a -> Maybe a
+hush (Left _) = Nothing
+hush (Right a) = Just a
+
+
+instance MonadFail (Either String) where
+  fail = Left
+
+------------------------------------------------------------------------------
+-- | Graft a 'RunTacticResults' into the correct place in an AST. Correctly
+-- deals with top-level holes, in which we might need to fiddle with the
+-- 'Match's that bind variables.
+graftMatchGroup
+    :: SrcSpan
+    -> Located [LMatch GhcPs (LHsExpr GhcPs)]
+    -> Graft (Either String) ParsedSource
+graftMatchGroup ss l = graftExprWithM ss $ \case
+  L span (HsCase ext scrut mg@_) -> do
+    pure $ Just $ L span $ HsCase ext scrut $ mg { mg_alts = l }
+  (_ :: LHsExpr GhcPs) -> pure Nothing
+
+
+fromMaybeT :: Functor m => a -> MaybeT m a -> m a
+fromMaybeT def = fmap (fromMaybe def) . runMaybeT
+
--- plugins/hls-tactics-plugin/src/Wingman/Plugin.hs
+++ plugins/hls-tactics-plugin/src/Wingman/Plugin.hs
@@ -12,18 +12,14 @@ import           Control.Monad
 import           Control.Monad.Trans
 import           Control.Monad.Trans.Maybe
 import           Data.Aeson
-import           Data.Bifunctor (first)
 import           Data.Data
 import           Data.Foldable (for_)
 import           Data.Maybe
 import qualified Data.Text as T
-import           Data.Traversable (for)
-import           Development.IDE (realSrcSpanToRange, GetBindings(..))
 import           Development.IDE.Core.Shake (IdeState (..))
 import           Development.IDE.Core.UseStale (Tracked, TrackedStale(..), unTrack, mapAgeFrom, unsafeMkCurrent)
 import           Development.IDE.GHC.Compat
 import           Development.IDE.GHC.ExactPrint
-import           Development.IDE.Spans.LocalBindings (getLocalScope)
 import           Ide.Types
 import           Language.LSP.Server
 import           Language.LSP.Types
@@ -32,7 +28,7 @@ import           OccName
 import           Prelude hiding (span)
 import           System.Timeout
 import           Wingman.CaseSplit
-import           Wingman.CodeGen (destructionFor)
+import           Wingman.EmptyCase
 import           Wingman.GHC
 import           Wingman.LanguageServer
 import           Wingman.LanguageServer.TacticProviders
@@ -40,10 +36,6 @@ import           Wingman.Machinery (scoreSolution)
 import           Wingman.Range
 import           Wingman.Tactics
 import           Wingman.Types
-
-
-emptyCaseLensCommandId :: CommandId
-emptyCaseLensCommandId = CommandId "wingman.emptyCase"
 
 
 descriptor :: PluginId -> PluginDescriptor IdeState
@@ -70,58 +62,6 @@ descriptor plId = (defaultPluginDescriptor plId)
   , pluginCustomConfig =
       mkCustomConfig properties
   }
-
-workspaceEditHandler :: CommandFunction IdeState WorkspaceEdit
-workspaceEditHandler _ideState wedit = do
-  _ <- sendRequest SWorkspaceApplyEdit (ApplyWorkspaceEditParams Nothing wedit) (\_ -> pure ())
-  return $ Right Null
-
-
-hySingleton :: OccName -> Hypothesis ()
-hySingleton n = Hypothesis . pure $ HyInfo n UserPrv ()
-
-
-codeLensProvider :: PluginMethodHandler IdeState TextDocumentCodeLens
-codeLensProvider state plId (CodeLensParams _ _ (TextDocumentIdentifier uri))
-  | Just nfp <- uriToNormalizedFilePath $ toNormalizedUri uri = do
-      cfg <- getTacticConfig plId
-      ccs <- getClientCapabilities
-      liftIO $ fromMaybeT (Right $ List []) $ do
-        dflags <- getIdeDynflags state nfp
-        TrackedStale pm _ <- runStaleIde state nfp GetAnnotatedParsedSource
-        TrackedStale binds bind_map <- runStaleIde state nfp GetBindings
-        holes <- completionForHole  state nfp $ cfg_feature_set cfg
-        fmap (Right . List) $ for holes $ \(ss, ty) -> do
-          binds_ss <- liftMaybe $ mapAgeFrom bind_map ss
-          let bindings = getLocalScope (unTrack binds) $ unTrack binds_ss
-              range = realSrcSpanToRange $ unTrack ss
-          matches <-
-            liftMaybe $
-              destructionFor
-                (foldMap (hySingleton . occName . fst) bindings)
-                ty
-          edits <- liftMaybe $ hush $
-                mkWorkspaceEdits dflags ccs uri (unTrack pm) $
-                  graftMatchGroup (RealSrcSpan $ unTrack ss) $
-                    noLoc matches
-
-          pure $
-            CodeLens
-              range
-              (Just
-                $ mkLspCommand
-                    plId
-                    emptyCaseLensCommandId
-                    ("Complete case constructors (" <> T.pack (unsafeRender ty) <> ")")
-                $ Just $ pure $ toJSON $ edits
-              )
-              Nothing
-codeLensProvider _ _ _ = pure $ Right $ List []
-
-
-hush :: Either e a -> Maybe a
-hush (Left _) = Nothing
-hush (Right a) = Just a
 
 
 codeActionProvider :: PluginMethodHandler IdeState TextDocumentCodeAction
@@ -226,37 +166,6 @@ mkTacticResultEdits (unTrack -> span) dflags ccs uri (unTrack -> pm) rtr = do
 
 
 ------------------------------------------------------------------------------
--- | Transform a 'Graft' over the AST into a 'WorkspaceEdit'.
-mkWorkspaceEdits
-    :: DynFlags
-    -> ClientCapabilities
-    -> Uri
-    -> Annotated ParsedSource
-    -> Graft (Either String) ParsedSource
-    -> Either UserFacingMessage WorkspaceEdit
-mkWorkspaceEdits dflags ccs uri pm g = do
-  let response = transform dflags ccs uri g pm
-   in first (InfrastructureError . T.pack) response
-
-
-instance MonadFail (Either String) where
-  fail = Left
-
-------------------------------------------------------------------------------
--- | Graft a 'RunTacticResults' into the correct place in an AST. Correctly
--- deals with top-level holes, in which we might need to fiddle with the
--- 'Match's that bind variables.
-graftMatchGroup
-    :: SrcSpan
-    -> Located [LMatch GhcPs (LHsExpr GhcPs)]
-    -> Graft (Either String) ParsedSource
-graftMatchGroup ss l = graftExprWithM ss $ \case
-  L span (HsCase ext scrut mg@_) -> do
-    pure $ Just $ L span $ HsCase ext scrut $ mg { mg_alts = l }
-  (_ :: LHsExpr GhcPs) -> pure Nothing
-
-
-------------------------------------------------------------------------------
 -- | Graft a 'RunTacticResults' into the correct place in an AST. Correctly
 -- deals with top-level holes, in which we might need to fiddle with the
 -- 'Match's that bind variables.
@@ -307,8 +216,4 @@ graftDecl dflags dst ix make_decl (L src (AMatch (FunRhs (L _ name) _ _) pats _)
           pure alts
         _ -> lift $ Left "annotateDecl didn't produce a funbind"
 graftDecl _ _ _ _ x = pure $ pure x
-
-
-fromMaybeT :: Functor m => a -> MaybeT m a -> m a
-fromMaybeT def = fmap (fromMaybe def) . runMaybeT
 
