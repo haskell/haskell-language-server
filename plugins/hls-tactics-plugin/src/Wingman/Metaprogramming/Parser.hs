0cd6a0e830edf5fef59ce61152951dae3ef5fbbe
Sandy Maguire <sandy@sandymaguire.me>
2021-04-26 07:48:12 -0700

    Use operator parsing for multiple tactics

--- plugins/hls-tactics-plugin/src/Wingman/Metaprogramming/Parser.hs
+++ plugins/hls-tactics-plugin/src/Wingman/Metaprogramming/Parser.hs
@@ -26,36 +26,31 @@ import qualified Data.Text as T
 
 tactic :: Parser (TacticsM ())
 tactic = flip P.makeExprParser operators $  P.choice
-    [ named  "assumption" assumption
+    [ braces tactic
+    , named  "assumption" assumption
     , named' "assume" assume
     , named  "intros" intros
     -- , named' "intro" intro
-    , named' "destruct" $ useNameFromHypothesis destruct
     , named "destruct_all" destructAll
+    , named' "destruct" $ useNameFromHypothesis destruct
     , named' "homo" $ useNameFromHypothesis homo
     , named' "apply" $ useNameFromHypothesis apply
-    -- , named  "split" $ useNameFromHypothesis split
+    , named'  "split" userSplit
     , named  "auto" auto
-    , R.try <$> (keyword "try" *> tactics)
+    , R.try <$> (keyword "try" *> tactic)
     ]
 
-multitactic :: Parser (TacticsM () -> TacticsM ())
-multitactic = P.choice
-    [ (flip (R.<@>)) <$> brackets (P.sepBy1 tactic (symbol ";"))
-    , (flip (>>)) <$> tactic
-    ]
+bindOne :: TacticsM a -> TacticsM a -> TacticsM a
+bindOne t t1 = t R.<@> [t1]
 
 operators :: [[P.Operator Parser (TacticsM ())]]
 operators =
     [ [ P.Prefix (symbol "*" $> R.many_) ]
     , [ P.InfixR (symbol "|" $> (R.<%>) )]
+    , [ P.InfixL (symbol ";" $> (>>))
+      , P.InfixL (symbol "," $> bindOne)
+      ]
     ]
-
-tactics :: Parser (TacticsM ())
-tactics = do
-    t <- tactic
-    ts <- P.many ((symbol ",") *> multitactic)
-    pure $ foldl' (&) t ts
 
 
 skolems :: [Type]
@@ -67,7 +62,7 @@ a_skolem, b_skolem, c_skolem :: Type
 
 attempt_it :: Context -> Judgement -> String -> Either String (LHsExpr GhcPs)
 attempt_it ctx jdg program =
-  case P.runParser (sc *> tactics <* P.eof) "<splice>" (T.pack program) of
+  case P.runParser (sc *> tactic <* P.eof) "<splice>" (T.pack program) of
     Left peb -> Left $ P.errorBundlePretty peb
     Right tt -> do
       case runTactic
