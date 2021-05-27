fc66d833cdd99a88a1188d07c27bcae8c6f42b70
Potato Hatsue <1793913507@qq.com>
2021-05-27 10:23:57 +0800

    Fix typo

--- flake.nix
+++ flake.nix
@@ -72,8 +72,9 @@
           # Don't use `callHackage`, it requires us to override `all-cabal-hashes`
           tweaks = hself: hsuper:
             with haskell.lib; {
+
               # https://github.com/haskell/haskell-language-server/pull/1858
-              # Remove this when nixpkgs has thie package
+              # Remove this override when nixpkgs has this package
               apply-refact_0_9_3_0 = hself.callCabal2nix "apply-refact"
                 (builtins.fetchTarball {
                   url =
@@ -81,13 +82,16 @@
                   sha256 =
                     "1jfq1aw91finlpq5nn7a96za4c8j13jk6jmx2867fildxwrik2qj";
                 }) { };
+
               hls-hlint-plugin = hsuper.hls-hlint-plugin.override {
                 hlint = hself.hlint_3_2_7;
                 apply-refact = hself.apply-refact_0_9_3_0;
               };
+
               hls-tactics-plugin = hsuper.hls-tactics-plugin.override {
                 refinery = hself.refinery_0_3_0_0;
               };
+
             };
 
           hlsSources =
