9a782b7877da571ba589f7e0fd49ac8a413b7e03
Pepe Iborra <pepeiborra@gmail.com>
2021-05-30 09:39:02 +0100

    Use file watches for all workspace files

--- ghcide/src/Development/IDE/Core/FileStore.hs
+++ ghcide/src/Development/IDE/Core/FileStore.hs
@@ -20,7 +20,9 @@ module Development.IDE.Core.FileStore(
     getModificationTimeImpl,
     addIdeGlobal,
     getFileContentsImpl,
-    getModTime
+    getModTime,
+    isWatchSupported,
+    registerFileWatches
     ) where
 
 import           Control.Concurrent.STM                       (atomically)
@@ -49,7 +51,8 @@ import           Development.IDE.Types.Diagnostics
 import           Development.IDE.Types.Location
 import           Development.IDE.Types.Options
 import           HieDb.Create                                 (deleteMissingRealFiles)
-import           Ide.Plugin.Config                            (CheckParents (..))
+import           Ide.Plugin.Config                            (CheckParents (..),
+                                                               Config)
 import           System.IO.Error
 
 #ifdef mingw32_HOST_OS
@@ -63,13 +66,20 @@ import qualified Development.IDE.Types.Logger                 as L
 
 import qualified Data.Binary                                  as B
 import qualified Data.ByteString.Lazy                         as LBS
+import qualified Data.Text                                    as Text
 import           Language.LSP.Server                          hiding
                                                               (getVirtualFile)
 import qualified Language.LSP.Server                          as LSP
-import           Language.LSP.Types                           (FileChangeType (FcChanged),
+import           Language.LSP.Types                           (DidChangeWatchedFilesRegistrationOptions (DidChangeWatchedFilesRegistrationOptions),
+                                                               FileChangeType (FcChanged),
                                                                FileEvent (FileEvent),
+                                                               FileSystemWatcher (..),
+                                                               WatchKind (..),
+                                                               _watchers,
                                                                toNormalizedFilePath,
                                                                uriToFilePath)
+import qualified Language.LSP.Types                           as LSP
+import qualified Language.LSP.Types.Capabilities              as LSP
 import           Language.LSP.VFS
 import           System.FilePath
 
@@ -94,6 +104,15 @@ makeLSPVFSHandle lspEnv = VFSHandle
     , setVirtualFileContents = Nothing
    }
 
+addWatchedFileRule :: (NormalizedFilePath -> Action Bool) -> Rules ()
+addWatchedFileRule isWatched = defineNoDiagnostics $ \AddWatchedFile f -> do
+  isAlreadyWatched <- isWatched f
+  if isAlreadyWatched then pure (Just True) else do
+    ShakeExtras{lspEnv} <- getShakeExtras
+    case lspEnv of
+        Just env -> fmap Just $ liftIO $ LSP.runLspT env $
+            registerFileWatches [fromNormalizedFilePath f]
+        Nothing -> pure Nothing
 
 isFileOfInterestRule :: Rules ()
 isFileOfInterestRule = defineEarlyCutoff $ RuleNoDiagnostics $ \IsFileOfInterest f -> do
@@ -109,45 +128,44 @@ isFileOfInterestRule = defineEarlyCutoff $ RuleNoDiagnostics $ \IsFileOfInterest
     summarize (IsFOI (Modified True))  = BS.singleton 3
 
 
-getModificationTimeRule :: VFSHandle -> (NormalizedFilePath -> Action Bool) -> Rules ()
-getModificationTimeRule vfs isWatched = defineEarlyCutoff $ Rule $ \(GetModificationTime_ missingFileDiags) file ->
-    getModificationTimeImpl vfs isWatched missingFileDiags file
+getModificationTimeRule :: VFSHandle -> Rules ()
+getModificationTimeRule vfs = defineEarlyCutoff $ Rule $ \(GetModificationTime_ missingFileDiags) file ->
+    getModificationTimeImpl vfs missingFileDiags file
 
 getModificationTimeImpl :: VFSHandle
-    -> (NormalizedFilePath -> Action Bool)
     -> Bool
     -> NormalizedFilePath
     -> Action
         (Maybe BS.ByteString, ([FileDiagnostic], Maybe FileVersion))
-getModificationTimeImpl vfs isWatched missingFileDiags file = do
-        let file' = fromNormalizedFilePath file
-        let wrap time = (Just $ LBS.toStrict $ B.encode $ toRational time, ([], Just $ ModificationTime time))
-        mbVirtual <- liftIO $ getVirtualFile vfs $ filePathToUri' file
-        case mbVirtual of
-            Just (virtualFileVersion -> ver) -> do
-                alwaysRerun
-                pure (Just $ LBS.toStrict $ B.encode ver, ([], Just $ VFSVersion ver))
-            Nothing -> do
-                isWF <- isWatched file
-                if isWF
-                    then -- the file is watched so we can rely on FileWatched notifications,
-                         -- but also need a dependency on IsFileOfInterest to reinstall
-                         -- alwaysRerun when the file becomes VFS
-                        void (use_ IsFileOfInterest file)
-                    else if isInterface file
-                        then -- interface files are tracked specially using the closed world assumption
-                            pure ()
-                        else -- in all other cases we will need to freshly check the file system
-                            alwaysRerun
+getModificationTimeImpl vfs missingFileDiags file = do
+    let file' = fromNormalizedFilePath file
+    let wrap time = (Just $ LBS.toStrict $ B.encode $ toRational time, ([], Just $ ModificationTime time))
+    mbVirtual <- liftIO $ getVirtualFile vfs $ filePathToUri' file
+    case mbVirtual of
+        Just (virtualFileVersion -> ver) -> do
+            alwaysRerun
+            pure (Just $ LBS.toStrict $ B.encode ver, ([], Just $ VFSVersion ver))
+        Nothing -> do
+            isWF <- use_ AddWatchedFile file
+            if isWF
+                then -- the file is watched so we can rely on FileWatched notifications,
+                        -- but also need a dependency on IsFileOfInterest to reinstall
+                        -- alwaysRerun when the file becomes VFS
+                    void (use_ IsFileOfInterest file)
+                else if isInterface file
+                    then -- interface files are tracked specially using the closed world assumption
+                        pure ()
+                    else -- in all other cases we will need to freshly check the file system
+                        alwaysRerun
 
-                liftIO $ fmap wrap (getModTime file')
-                    `catch` \(e :: IOException) -> do
-                        let err | isDoesNotExistError e = "File does not exist: " ++ file'
-                                | otherwise = "IO error while reading " ++ file' ++ ", " ++ displayException e
-                            diag = ideErrorText file (T.pack err)
-                        if isDoesNotExistError e && not missingFileDiags
-                            then return (Nothing, ([], Nothing))
-                            else return (Nothing, ([diag], Nothing))
+            liftIO $ fmap wrap (getModTime file')
+                `catch` \(e :: IOException) -> do
+                    let err | isDoesNotExistError e = "File does not exist: " ++ file'
+                            | otherwise = "IO error while reading " ++ file' ++ ", " ++ displayException e
+                        diag = ideErrorText file (T.pack err)
+                    if isDoesNotExistError e && not missingFileDiags
+                        then return (Nothing, ([], Nothing))
+                        else return (Nothing, ([diag], Nothing))
 
 -- | Interface files cannot be watched, since they live outside the workspace.
 --   But interface files are private, in that only HLS writes them.
@@ -239,9 +257,10 @@ getFileContents f = do
 fileStoreRules :: VFSHandle -> (NormalizedFilePath -> Action Bool) -> Rules ()
 fileStoreRules vfs isWatched = do
     addIdeGlobal vfs
-    getModificationTimeRule vfs isWatched
+    getModificationTimeRule vfs
     getFileContentsRule vfs
     isFileOfInterestRule
+    addWatchedFileRule isWatched
 
 -- | Note that some buffer for a specific file has been modified but not
 -- with what changes.
@@ -290,3 +309,43 @@ setSomethingModified state = do
     -- Update database to remove any files that might have been renamed/deleted
     atomically $ writeTQueue (indexQueue $ hiedbWriter $ shakeExtras state) deleteMissingRealFiles
     void $ shakeRestart state []
+
+registerFileWatches :: [String] -> LSP.LspT Config IO Bool
+registerFileWatches globs = do
+      watchSupported <- isWatchSupported
+      if watchSupported
+      then do
+        let
+          regParams    = LSP.RegistrationParams (List [LSP.SomeRegistration registration])
+          -- The registration ID is arbitrary and is only used in case we want to deregister (which we won't).
+          -- We could also use something like a random UUID, as some other servers do, but this works for
+          -- our purposes.
+          registration = LSP.Registration "globalFileWatches"
+                                           LSP.SWorkspaceDidChangeWatchedFiles
+                                           regOptions
+          regOptions =
+            DidChangeWatchedFilesRegistrationOptions { _watchers = List watchers }
+          -- See Note [File existence cache and LSP file watchers] for why this exists, and the choice of watch kind
+          watchKind = WatchKind { _watchCreate = True, _watchChange = True, _watchDelete = True}
+          -- See Note [Which files should we watch?] for an explanation of why the pattern is the way that it is
+          -- The patterns will be something like "**/.hs", i.e. "any number of directory segments,
+          -- followed by a file with an extension 'hs'.
+          watcher glob = FileSystemWatcher { _globPattern = glob, _kind = Just watchKind }
+          -- We use multiple watchers instead of one using '{}' because lsp-test doesn't
+          -- support that: https://github.com/bubba/lsp-test/issues/77
+          watchers = [ watcher (Text.pack glob) | glob <- globs ]
+
+        void $ LSP.sendRequest LSP.SClientRegisterCapability regParams (const $ pure ()) -- TODO handle response
+        return True
+      else return False
+
+isWatchSupported :: LSP.LspT Config IO Bool
+isWatchSupported = do
+      clientCapabilities <- LSP.getClientCapabilities
+      pure $ case () of
+            _ | LSP.ClientCapabilities{_workspace} <- clientCapabilities
+              , Just LSP.WorkspaceClientCapabilities{_didChangeWatchedFiles} <- _workspace
+              , Just LSP.DidChangeWatchedFilesClientCapabilities{_dynamicRegistration} <- _didChangeWatchedFiles
+              , Just True <- _dynamicRegistration
+                -> True
+              | otherwise -> False
