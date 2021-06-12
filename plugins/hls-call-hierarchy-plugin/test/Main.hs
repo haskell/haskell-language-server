118acd759eaf5c6f5d77fff9f297b8afc9ab9f02
July541 <julytreee@gmail.com>
2021-06-12 20:04:03 +0800

    Add prepare call hierarchy tests

--- plugins/hls-call-hierarchy-plugin/test/Main.hs
+++ plugins/hls-call-hierarchy-plugin/test/Main.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import           Control.Lens
+import qualified Data.Text                as T
 import           Ide.Plugin.CallHierarchy
 import qualified Language.LSP.Test        as Test
 import qualified Language.LSP.Types.Lens  as L
@@ -14,26 +16,162 @@ plugin = descriptor "callHierarchy"
 main :: IO ()
 main = defaultTestRunner $ testGroup "Call Hierarchy" [prepareCallHierarchyTests]
 
-
 prepareCallHierarchyTests :: TestTree
 prepareCallHierarchyTests =
   testGroup
   "Prepare Call Hierarchy"
-  [ testCase "simple call hierarchy" $ do
-      runSessionWithServer plugin testDataDir $ do
-        doc <- openDoc "B.hs" "haskell"
-        [item] <- Test.prepareCallHierarchy (mkPrepareCallHierarchyParam doc 11 0)
-        let expectedRange = Range (Position 11 0) (Position 11 1)
-            expected = CallHierarchyItem "a" SkFunction Nothing Nothing (doc ^. L.uri) expectedRange expectedRange Nothing
-        liftIO $ item @?= expected
-  , testCase "typeclass" $ do
-      runSessionWithServer plugin testDataDir $ do
-        doc <- openDoc "B.hs" "haskell"
-        [item] <- Test.prepareCallHierarchy (mkPrepareCallHierarchyParam doc 9 0)
-        let expectedRange = Range (Position 9 0) (Position 9 1)
-            expected = CallHierarchyItem "g" SkFunction Nothing Nothing (doc ^. L.uri) expectedRange expectedRange Nothing
-        liftIO $ item @?= expected
+  [ testCase "variable" $ do
+      let contents = T.unlines ["a=3"]
+          range = mkRange 0 0 0 3
+          selRange = mkRange 0 0 0 1
+          expected = mkCallHierarchyItem "a" SkVariable range selRange
+      oneCaseWithCreate contents 0 0 expected
+  , testCase "function" $ do
+      let contents = T.unlines ["a=(+)"]
+          range = mkRange 0 0 0 5
+          selRange = mkRange 0 0 0 1
+          expected = mkCallHierarchyItem "a" SkFunction range selRange
+      oneCaseWithCreate contents 0 0 expected
+  , testCase "datatype" $ do
+      let contents = T.unlines ["data A=A"]
+          range = mkRange 0 5 0 6
+          selRange = mkRange 0 5 0 6
+          expected = mkCallHierarchyItem "A" SkStruct range selRange
+      oneCaseWithCreate contents 0 5 expected
+  , testCase "data constructor" $ do
+      let contents = T.unlines ["data A=A"]
+          range = mkRange 0 7 0 8
+          selRange = mkRange 0 7 0 8
+          expected = mkCallHierarchyItem "A" SkConstructor range selRange
+      oneCaseWithCreate contents 0 7 expected
+  , testCase "record" $ do
+      let contents = T.unlines ["data A=A{a::Int}"]
+          range = mkRange 0 9 0 14
+          selRange = mkRange 0 9 0 10
+          expected = mkCallHierarchyItem "a" SkField range selRange
+      oneCaseWithCreate contents 0 9 expected
+  , testCase "type operator" $ do
+      let contents = T.unlines ["{-# LANGUAGE TypeOperator -#}", "type (><)=Maybe"]
+          range = mkRange 1 6 1 8
+          selRange = mkRange 1 6 1 8
+          expected = mkCallHierarchyItem "(><)" SkStruct range selRange
+      oneCaseWithCreate contents 1 6 expected
+  , testCase "type class" $ do
+      let contents = T.unlines ["class A a where a :: a -> Int"]
+          range = mkRange 0 6 0 7
+          selRange = mkRange 0 6 0 7
+          expected = mkCallHierarchyItem "A" SkInterface range selRange
+      oneCaseWithCreate contents 0 6 expected
+  , testCase "type class method" $ do
+      let contents = T.unlines ["class A a where a :: a -> Int"]
+          range = mkRange 0 16 0 29
+          selRange = mkRange 0 16 0 17
+          expected = mkCallHierarchyItem "a" SkMethod range selRange
+      oneCaseWithCreate contents 0 16 expected
+  , testCase "type class instance" $ do -- TODO: add details to prevent ambiguous
+      let contents = T.unlines ["class A a where", "instance A () where"]
+          range = mkRange 1 9 1 10
+          selRange = mkRange 1 9 1 10
+          expected = mkCallHierarchyItem "A" SkInterface range selRange
+      oneCaseWithCreate contents 1 9 expected
+  , testGroup "type family"
+      [ testCase "1" $ do
+          let contents = T.unlines ["{-# LANGUAGE TypeFamilies #-}", "type family A"]
+              range = mkRange 1 12 1 13
+              selRange = range
+              expected = mkCallHierarchyItem "A" SkFunction range selRange
+          oneCaseWithCreate contents 1 12 expected
+      , testCase "2" $ do
+          let contents = T.unlines ["{-# LANGUAGE TypeFamilies #-}", "type family A a"]
+              range = mkRange 1 12 1 15
+              selRange = range
+              expected = mkCallHierarchyItem "A a" SkFunction range selRange
+          oneCaseWithCreate contents 1 12 expected
+      ]
+  , testCase "type family instance" $ do
+      let contents = T.unlines
+            [ "{-# LANGUAGE TypeFamilies #-}"
+            , "type family A a"
+            , "type instance A () = ()"
+            ]
+          range = mkRange 2 14 2 17
+          selRange = range
+          expected = mkCallHierarchyItem "A ()" SkInterface range selRange
+      oneCaseWithCreate contents 2 14 expected
+  , testGroup "data family"
+      [ testCase "1" $ do
+          let contents = T.unlines ["{-# LANGUAGE TypeFamilies #-}", "data family A"]
+              range = mkRange 1 12 1 13
+              selRange = range
+              expected = mkCallHierarchyItem "A" SkFunction range selRange
+          oneCaseWithCreate contents 1 12 expected
+      , testCase "2" $ do
+          let contents = T.unlines [ "{-# LANGUAGE TypeFamilies #-}" , "data family A a"]
+              range = mkRange 1 12 1 15
+              selRange = range
+              expected = mkCallHierarchyItem "A a" SkFunction range selRange
+          oneCaseWithCreate contents 1 12 expected
+      ]
+  , testCase "data family instance" $ do
+      let contents = T.unlines
+            [ "{-# LANGUAGE TypeFamilies #-}"
+            , "data family A a"
+            , "data instance A () = A()"
+            ]
+          range = mkRange 2 14 2 17
+          selRange = range
+          expected = mkCallHierarchyItem "A ()" SkInterface range selRange
+      oneCaseWithCreate contents 2 14 expected
+  , testCase "pattern" $ do
+      let contents = T.unlines ["Just x = Just 3"]
+          range = mkRange 0 5 0 6
+          selRange = range
+          expected = mkCallHierarchyItem "x" SkVariable range selRange
+      oneCaseWithCreate contents 0 5 expected
+  , testCase "pattern with type signature" $ do
+      let contents = T.unlines ["{-# LANGUAGE ScopedTypeVariables #-}", "a :: () = ()"]
+          range = mkRange 1 0 1 7
+          selRange = mkRange 1 0 1 1
+          expected = mkCallHierarchyItem "a" SkFunction range selRange
+      oneCaseWithCreate contents 1 0 expected
+  , testCase "type synonym" $ do
+      let contents = T.unlines ["type A=Bool"]
+          range = mkRange 0 5 0 6
+          selRange = range
+          expected = mkCallHierarchyItem "A" SkTypeParameter range selRange
+      oneCaseWithCreate contents 0 5 expected
+  , testCase "GADT" $ do
+      let contents = T.unlines
+            [ "{-# LANGUAGE GADTs #-}"
+            , "data A where A :: Int -> A"
+            ]
+          range = mkRange 1 13 1 26
+          selRange = mkRange 1 13 1 14
+          expected = mkCallHierarchyItem "A" SkConstructor range selRange
+      oneCaseWithCreate contents 1 13 expected
   ]
+
+oneCaseWithOpen :: FilePath -> Int -> Int -> CallHierarchyItem -> Assertion
+oneCaseWithOpen filename queryX queryY expected =
+  runSessionWithServer plugin testDataDir $ do
+    doc <- openDoc filename "haskell"
+    Test.prepareCallHierarchy (mkPrepareCallHierarchyParam doc queryX queryY) >>=
+      \case
+        [item] -> liftIO $ item @?= expected
+        _      -> liftIO $ assertFailure "Not one element"
+
+oneCaseWithCreate :: T.Text -> Int -> Int -> (Uri -> CallHierarchyItem) -> Assertion
+oneCaseWithCreate contents queryX queryY expected =
+  runSessionWithServer plugin testDataDir $ do
+    doc <- createDoc "A.hs" "haskell" contents
+    Test.prepareCallHierarchy (mkPrepareCallHierarchyParam doc queryX queryY) >>=
+      \case
+        [item] -> liftIO $ item @?= expected (doc ^. L.uri)
+        _      -> liftIO $ assertFailure "Not one element"
+
+mkCallHierarchyItem :: T.Text -> SymbolKind -> Range -> Range -> Uri -> CallHierarchyItem
+mkCallHierarchyItem name kind range selRange uri =
+  CallHierarchyItem name kind Nothing Nothing uri range selRange Nothing
 
 testDataDir :: FilePath
 testDataDir = "test" </> "testdata"
