9f4220fe645c21b2732ddc10ae0a8a6eb158b254
Farhad Mehta <farhad.mehta@ost.ch>
2021-06-23 14:28:31 +0200

    Make the eval plugin use the same default language extensions as ghci. (#1596)
    * Enable ExtendedDefaultRules for expressions in eval plugin (#1591)
    
    * Add tests for fix#1591
    
    * Add fix for #1954
    
    * Remove dependency on xopt_set_unlessExplSpec.
    Reason: It was breaking GHC 8.6.2 tests.
    
    * Remove unnecessary comment.
    
    Co-authored-by: Potato Hatsue <berberman@yandex.com>
    
    Co-authored-by: Javier Neira <atreyu.bbb@gmail.com>
    Co-authored-by: Potato Hatsue <1793913507@qq.com>
    Co-authored-by: Potato Hatsue <berberman@yandex.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ plugins/hls-eval-plugin/test/testdata/TSameDefaultLanguageExtensionsAsGhci.expected.hs
@@ -0,0 +1,27 @@
+-- The default language extensions for the eval plugin are the same as those for ghci
+
+module TSameDefaultLanguageExtensionsAsGhci where
+
+{-
+Running `:showi language` within ghci currently lists NoDatatypeContexts, ExtendedDefaultRules, NoMonomorphismRestriction and NondecreasingIndentation.
+
+The flags NoDatatypeContexts and NondecreasingIndentation are globally set in Haskell2021, whereas ExtendedDefaultRules and NoMonomorphismRestriction are set manually within ghci.
+(see https://github.com/ghc/ghc/blob/5abf59976c7335df760e5d8609d9488489478173/ghc/GHCi/UI.hs#L473-L483)
+
+It therefore suffices to test for ExtendedDefaultRules and NoMonomorphismRestriction only.
+-}
+
+
+-- ExtendedDefaultRules
+
+-- >>> []
+-- []
+
+-- >>> reverse []
+-- []
+
+-- NoMonomorphismRestriction
+
+-- >>> plus = (+)
+-- >>> :t plus
+-- plus :: forall a. Num a => a -> a -> a
