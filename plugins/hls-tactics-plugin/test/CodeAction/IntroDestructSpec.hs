bce20820146c20709e6eb7db703ec75dfc63d992
Sandy Maguire <sandy@sandymaguire.me>
2021-08-03 11:06:15 -0700

    Add provider tests

--- plugins/hls-tactics-plugin/test/CodeAction/IntroDestructSpec.hs
+++ plugins/hls-tactics-plugin/test/CodeAction/IntroDestructSpec.hs
@@ -17,3 +17,20 @@ spec = do
     test 2  5 "Many"
     test 4 11 "LetBinding"
 
+  describe "provider" $ do
+    mkTest
+      "Can intro and destruct an algebraic ty"
+      "IntroDestructProvider" 2 12
+      [ (id, IntroAndDestruct, "")
+      ]
+    mkTest
+      "Won't intro and destruct a non-algebraic ty"
+      "IntroDestructProvider" 5 12
+      [ (not, IntroAndDestruct, "")
+      ]
+    mkTest
+      "Can't intro, so no option"
+      "IntroDestructProvider" 8 17
+      [ (not, IntroAndDestruct, "")
+      ]
+
