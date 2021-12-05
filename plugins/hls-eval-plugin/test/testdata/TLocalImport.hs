249b0b03506fb8888a29786caf5e3b7892ec40ee
Pepe Iborra <pepeiborra@gmail.com>
2021-12-05 22:12:42 +0000

    Fix tests

--- plugins/hls-eval-plugin/test/testdata/TLocalImport.hs
+++ plugins/hls-eval-plugin/test/testdata/TLocalImport.hs
@@ -4,4 +4,5 @@ import qualified Util
 
 -- >>> Util.tst 11 11
 
+tst' :: Eq a => a -> a -> Bool
 tst' = Util.tst
--- plugins/hls-eval-plugin/test/testdata/TTransitive.expected.hs
+++ plugins/hls-eval-plugin/test/testdata/TTransitive.expected.hs
@@ -3,4 +3,4 @@ module TTransitive where
 import TLocalImport
 
 -- >>> tst' 11 11
--- No instance for (Num Any) arising from the literal ‘11’
+-- True
