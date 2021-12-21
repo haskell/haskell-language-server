c06978b3cb4bb1b59fa82add8a2e822ed5ee5e9d
Zubin Duggal <zubin.duggal@gmail.com>
2021-12-21 17:36:14 +0530

    9.2 compat for Pragmas plugin

--- ghcide/src/Development/IDE/Spans/Pragmas.hs
+++ ghcide/src/Development/IDE/Spans/Pragmas.hs
@@ -122,13 +122,21 @@ updateParserState token range prevParserState
       ModeInitial ->
         case token of
           ITvarsym "#" -> defaultParserState{ isLastTokenHash = True }
+#if !MIN_VERSION_ghc(9,2,0)
           ITlineComment s
+#else
+          ITlineComment s _
+#endif
             | isDownwardLineHaddock s -> defaultParserState{ mode = ModeHaddock }
             | otherwise ->
                 defaultParserState
                   { nextPragma = NextPragmaInfo (endLine + 1) Nothing
                   , mode = ModeComment }
+#if !MIN_VERSION_ghc(9,2,0)
           ITblockComment s
+#else
+          ITblockComment s _
+#endif
             | isPragma s ->
                 defaultParserState
                   { nextPragma = NextPragmaInfo (endLine + 1) Nothing
@@ -144,7 +152,11 @@ updateParserState token range prevParserState
       ModeComment ->
         case token of
           ITvarsym "#" -> defaultParserState{ isLastTokenHash = True }
+#if !MIN_VERSION_ghc(9,2,0)
           ITlineComment s
+#else
+          ITlineComment s _
+#endif
             | hasDeleteStartedOnSameLine startLine prevLineSplitTextEdits
             , let currLineSplitTextEdits = updateLineSplitTextEdits range s prevLineSplitTextEdits ->
                 defaultParserState{ nextPragma = prevNextPragma{ lineSplitTextEdits = Just currLineSplitTextEdits } }
@@ -156,7 +168,11 @@ updateParserState token range prevParserState
                   , mode = ModeHaddock }
             | otherwise ->
                 defaultParserState { nextPragma = NextPragmaInfo (endLine + 1) Nothing }
+#if !MIN_VERSION_ghc(9,2,0)
           ITblockComment s
+#else
+          ITblockComment s _
+#endif
             | isPragma s ->
                 defaultParserState
                   { nextPragma = NextPragmaInfo (endLine + 1) Nothing
@@ -180,13 +196,21 @@ updateParserState token range prevParserState
         case token of
           ITvarsym "#" ->
             defaultParserState{ isLastTokenHash = True }
+#if !MIN_VERSION_ghc(9,2,0)
           ITlineComment s
+#else
+          ITlineComment s _
+#endif
             | hasDeleteStartedOnSameLine startLine prevLineSplitTextEdits
             , let currLineSplitTextEdits = updateLineSplitTextEdits range s prevLineSplitTextEdits ->
                 defaultParserState{ nextPragma = prevNextPragma{ lineSplitTextEdits = Just currLineSplitTextEdits } }
             | otherwise ->
                 defaultParserState
+#if !MIN_VERSION_ghc(9,2,0)
           ITblockComment s
+#else
+          ITblockComment s _
+#endif
             | isPragma s ->
                 defaultParserState{
                   nextPragma = NextPragmaInfo (endLine + 1) Nothing,
@@ -200,7 +224,11 @@ updateParserState token range prevParserState
       ModePragma ->
         case token of
           ITvarsym "#" -> defaultParserState{ isLastTokenHash = True }
+#if !MIN_VERSION_ghc(9,2,0)
           ITlineComment s
+#else
+          ITlineComment s _
+#endif
             | hasDeleteStartedOnSameLine startLine prevLineSplitTextEdits
             , let currLineSplitTextEdits = updateLineSplitTextEdits range s prevLineSplitTextEdits ->
                 defaultParserState{ nextPragma = prevNextPragma{ lineSplitTextEdits = Just currLineSplitTextEdits } }
@@ -210,7 +238,11 @@ updateParserState token range prevParserState
                 defaultParserState{ nextPragma = prevNextPragma{ lineSplitTextEdits = Just currLineSplitTextEdits } }
             | otherwise ->
                 defaultParserState
+#if !MIN_VERSION_ghc(9,2,0)
           ITblockComment s
+#else
+          ITblockComment s _
+#endif
             | isPragma s ->
                 defaultParserState{ nextPragma = NextPragmaInfo (endLine + 1) Nothing, lastPragmaLine = endLine }
             | hasDeleteStartedOnSameLine startLine prevLineSplitTextEdits
@@ -390,7 +422,7 @@ mkLexerPState dynFlags stringBuffer =
       <*> const False
     finalPState = mkPStatePure (mkLexerParserFlags finalDynFlags) stringBuffer startRealSrcLoc
 #else
-    pState = mkPState finalDynFlags stringBuffer startRealSrcLoc
+    pState = initParserState (initParserOpts finalDynFlags) stringBuffer startRealSrcLoc
     PState{ options = pStateOptions } = pState
     finalExtBitsMap = setBit (pExtsBitmap pStateOptions) (fromEnum UsePosPragsBit)
     finalPStateOptions = pStateOptions{ pExtsBitmap = finalExtBitsMap }
