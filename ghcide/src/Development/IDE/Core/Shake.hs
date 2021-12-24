f951004021a2d3d688ea249a7f52ce087ac4e2f0
Pepe Iborra <pepeiborra@gmail.com>
2021-12-24 14:27:58 +0000

    Remove noisy logMessage notifications in tests
    I have never found these useful and they make it much harder to read logs. If there is a test that needs them, please use a custom logger instead of forcing this madness on everyone.

--- ghcide/src/Development/IDE/Core/Shake.hs
+++ ghcide/src/Development/IDE/Core/Shake.hs
@@ -711,7 +711,7 @@ newSession extras@ShakeExtras{..} shakeDb acts reason = do
                             ++ " (took " ++ showDuration runTime ++ ")"
             liftIO $ do
                 logPriority logger (actionPriority d) msg
-                notifyTestingLogMessage extras msg
+                -- notifyTestingLogMessage extras msg
 
         -- The inferred type signature doesn't work in ghc >= 9.0.1
         workRun :: (forall b. IO b -> IO b) -> IO (IO ())
