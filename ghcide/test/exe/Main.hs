16bd035acf01e0f4be0034d2d967661a42fd49aa
Pepe Iborra <pepeiborra@gmail.com>
2022-01-04 15:54:18 +0100

    Revert "Sanitize hiding tests"
    This reverts commit 4f016ddca2b005e23963c3d401028d127fc87fd0.

--- ghcide/test/exe/Main.hs
+++ ghcide/test/exe/Main.hs
@@ -2119,15 +2119,20 @@ suggestImportDisambiguationTests = testGroup "suggest import disambiguation acti
     ]
   ]
   where
+    hidingDir = "test/data/hiding"
     compareTwo original locs cmd expected =
         withTarget original locs $ \doc actions -> do
-            expected <- liftIO $ readFileUtf8 expected
+            expected <- liftIO $
+                readFileUtf8 (hidingDir </> expected)
             action <- liftIO $ pickActionWithTitle cmd actions
             executeCodeAction action
             contentAfterAction <- documentContents doc
             liftIO $ T.replace "\r\n" "\n" expected @=? contentAfterAction
     compareHideFunctionTo = compareTwo "HideFunction.hs"
-    withTarget file locs k = runWithExtraFiles "hiding" $ \dir -> do
+    auxFiles = ["AVec.hs", "BVec.hs", "CVec.hs", "DVec.hs", "EVec.hs", "FVec.hs"]
+    withTarget file locs k = withTempDir $ \dir -> runInDir dir $ do
+        liftIO $ mapM_ (\fp -> copyFile (hidingDir </> fp) $ dir </> fp)
+            $ file : auxFiles
         doc <- openDoc file "haskell"
         waitForProgressDone
         void $ expectDiagnostics [(file, [(DsError, loc, "Ambiguous occurrence") | loc <- locs])]
