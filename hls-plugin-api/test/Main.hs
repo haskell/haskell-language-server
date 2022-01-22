608b56d03f85c1d19ee71b8cdd6f7803c44b4ac1
kokobd <contact@zelinf.net>
2022-01-22 20:17:57 +0800

    fix ci

--- hls-plugin-api/test/Main.hs
+++ hls-plugin-api/test/Main.hs
@@ -1,10 +1,11 @@
 module Main where
 
-import qualified Ide.PluginUtilsTest as PluginUtilsTest
+import qualified Ide.PluginUtilsTest          as PluginUtilsTest
 import           Test.Tasty
+import           Test.Tasty.Ingredients.Rerun
 
 main :: IO ()
-main = defaultMain tests
+main = defaultMainWithRerun tests
 
 tests :: TestTree
 tests = testGroup "Main"
