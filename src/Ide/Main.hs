3d7134817fa2123edf1cc3bf9ff80986c1ed2584
Jon Shen <shenjonathan0@gmail.com>
2022-02-20 12:27:06 -0500

    fix -WError

--- src/Ide/Main.hs
+++ src/Ide/Main.hs
@@ -35,12 +35,9 @@ import           Ide.Plugin.ConfigUtils        (pluginsToDefaultConfig,
 import           Ide.Types                     (IdePlugins, PluginId (PluginId),
                                                 ipMap)
 import           Ide.Version
-import qualified Language.LSP.Server           as LSP
 import           System.Directory
 import qualified System.Directory.Extra        as IO
 import           System.FilePath
-import           System.IO
-import qualified System.Log.Logger             as L
 
 data Log
   = LogVersion !String
