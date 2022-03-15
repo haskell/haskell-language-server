1f9217504b05449c4b719e942d1da90e98c2448c
Zubin Duggal <zubin.duggal@gmail.com>
2022-03-15 12:17:44 +0530

    Never compile boot files

--- ghcide/src/Development/IDE/Core/Rules.hs
+++ ghcide/src/Development/IDE/Core/Rules.hs
@@ -1040,6 +1040,7 @@ getLinkableType f = use_ NeedsCompilation f
 
 -- needsCompilationRule :: Rules ()
 needsCompilationRule :: NormalizedFilePath  -> Action (IdeResultNoDiagnosticsEarlyCutoff (Maybe LinkableType))
+needsCompilationRule file | "boot" `isSuffixOf` (fromNormalizedFilePath file) = pure (Just $ encodeLinkableType Nothing, Just Nothing)
 needsCompilationRule file = do
   graph <- useNoFile GetModuleGraph
   res <- case graph of
