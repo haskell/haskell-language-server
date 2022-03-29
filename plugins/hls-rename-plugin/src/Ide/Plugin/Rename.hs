6f16defc31207a71e0047026ea0f1d3d3ad0c705
Oliver Madine <30090176+OliverMadine@users.noreply.github.com>
2022-03-29 03:18:33 +0100

    refactor rename plugin   - add context to error messages   - remove unnecessary unwrapping of ParsedSource   - use HashSet for references   - consistent naming, whitespace, indentation, imports style

--- plugins/hls-rename-plugin/src/Ide/Plugin/Rename.hs
+++ plugins/hls-rename-plugin/src/Ide/Plugin/Rename.hs
@@ -8,38 +8,41 @@
 
 module Ide.Plugin.Rename (descriptor) where
 
-import           Control.Monad
-import           Control.Monad.IO.Class               (MonadIO (liftIO))
+#if MIN_VERSION_ghc(9,2,1)
+import           GHC.Parser.Annotation
+#endif
+
+import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.Except
-import           Data.Containers.ListUtils
 import           Data.Generics
-import           Data.List.Extra                      hiding (nubOrd, replace)
+import           Data.HashSet                         (HashSet)
+import qualified Data.HashSet                         as HS
+import           Data.Hashable
+import           Data.List.Extra
 import qualified Data.Map                             as M
 import           Data.Maybe
+import           Data.Mod.Word
 import qualified Data.Text                            as T
 import           Development.IDE                      hiding (pluginHandlers)
 import           Development.IDE.Core.PositionMapping
 import           Development.IDE.Core.Shake
 import           Development.IDE.GHC.Compat
+import           Development.IDE.GHC.ExactPrint
 import           Development.IDE.Spans.AtPoint
-#if MIN_VERSION_ghc(9,2,1)
-import           GHC.Parser.Annotation                (AnnContext, AnnList,
-                                                       AnnParen, AnnPragma)
-#endif
-#if MIN_VERSION_ghc(9,0,1)
-import           GHC.Types.Name
-#else
-import           Name
-#endif
-import           Development.IDE.GHC.ExactPrint       (GetAnnotatedParsedSource (GetAnnotatedParsedSource))
 import           HieDb.Query
 import           Ide.Plugin.Config
 import           Ide.PluginUtils
 import           Ide.Types
-import           Language.Haskell.GHC.ExactPrint
 import           Language.LSP.Server
 import           Language.LSP.Types
+import Control.Monad
+
+instance Hashable Location
+instance Hashable Range
+instance Hashable Position
+instance Hashable UInt
+instance Hashable (Mod a) where hash n = hash (unMod n)
 
 descriptor :: PluginId -> PluginDescriptor IdeState
 descriptor pluginId = (defaultPluginDescriptor pluginId) {
@@ -51,138 +54,136 @@ renameProvider state pluginId (RenameParams (TextDocumentIdentifier uri) pos _pr
     response $ do
         nfp <- safeUriToNfp uri
         oldName <- getNameAtPos state nfp pos
-        workspaceRefs <- refsAtName state nfp oldName
-        let filesRefs = groupOn locToUri workspaceRefs
-            getFileEdits = ap (getSrcEdits state . renameModRefs newNameText) (locToUri . head)
-        fileEdits <- mapM getFileEdits filesRefs
+        refLocs <- refsAtName state nfp oldName
+        let newName = mkTcOcc $ T.unpack newNameText
+            filesRefs = collectWith locToUri refLocs
+            getFileEdit = flip $ getSrcEdit state . renameRefs newName
+        fileEdits <- mapM (uncurry getFileEdit) filesRefs
         pure $ foldl' (<>) mempty fileEdits
 
--------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
 -- Source renaming
 
--- | Compute a `WorkspaceEdit` by applying a given function to the `ParsedModule` for a given `Uri`.
-getSrcEdits ::
+-- | Apply a function to a `ParsedSource` for a given `Uri` to compute a `WorkspaceEdit`.
+getSrcEdit ::
     (MonadLsp config m) =>
     IdeState ->
-#if MIN_VERSION_ghc(9,0,1)
-    (HsModule -> HsModule) ->
-#else
-    (HsModule GhcPs -> HsModule GhcPs) ->
-#endif
+    (ParsedSource -> ParsedSource) ->
     Uri ->
     ExceptT String m WorkspaceEdit
-getSrcEdits state updateMod uri = do
+getSrcEdit state updatePs uri = do
     ccs <- lift getClientCapabilities
     nfp <- safeUriToNfp uri
-    annotatedAst <-
-        handleMaybeM "Error: could not get parsed source" $ liftIO $ runAction
-            "Rename.GetParsedModuleWithComments"
-            state
-            (use GetAnnotatedParsedSource nfp)
-    let (ps, anns) = (astA annotatedAst, annsA annotatedAst)
+    annAst <- handleMaybeM ("No parsed source for: " ++ show nfp) $ liftIO $ runAction
+        "Rename.GetAnnotatedParsedSource"
+        state
+        (use GetAnnotatedParsedSource nfp)
+    let (ps, anns) = (astA annAst, annsA annAst)
 #if !MIN_VERSION_ghc(9,2,1)
     let src = T.pack $ exactPrint ps anns
-        res = T.pack $ exactPrint (updateMod <$> ps) anns
+        res = T.pack $ exactPrint (updatePs ps) anns
 #else
     let src = T.pack $ exactPrint ps
-        res = T.pack $ exactPrint (updateMod <$> ps)
+        res = T.pack $ exactPrint (updatePs ps)
 #endif
-
     pure $ diffText ccs (uri, src) res IncludeDeletions
 
--- | Replace a name at every given `Location` (in a given `HsModule`) with a given new name.
-renameModRefs ::
-    T.Text ->
-    [Location] ->
-#if MIN_VERSION_ghc(9,0,1)
-    HsModule
-    -> HsModule
-#else
-    HsModule GhcPs
-    -> HsModule GhcPs
-#endif
+-- | Replace names at every given `Location` (in a given `ParsedSource`) with a given new name.
+renameRefs ::
+    OccName ->
+    HashSet Location ->
+    ParsedSource ->
+    ParsedSource
 #if MIN_VERSION_ghc(9,2,1)
-renameModRefs newNameText refs = everywhere $
+renameRefs newName refs = everywhere $
     -- there has to be a better way...
-    mkT (replace @AnnListItem) `extT`
-    -- replace @AnnList `extT` -- not needed
-    -- replace @AnnParen `extT`   -- not needed
-    -- replace @AnnPragma `extT` -- not needed
-    -- replace @AnnContext `extT` -- not needed
-    -- replace @NoEpAnns `extT` -- not needed
-    replace @NameAnn
+    mkT (replaceLoc @AnnListItem) `extT`
+    -- replaceLoc @AnnList `extT` -- not needed
+    -- replaceLoc @AnnParen `extT`   -- not needed
+    -- replaceLoc @AnnPragma `extT` -- not needed
+    -- replaceLoc @AnnContext `extT` -- not needed
+    -- replaceLoc @NoEpAnns `extT` -- not needed
+    replaceLoc @NameAnn
     where
-        replace :: forall an. Typeable an => LocatedAn an RdrName -> LocatedAn an RdrName
-        replace (L srcSpan oldRdrName)
-            | isRef (locA srcSpan) = L srcSpan $ newRdrName oldRdrName
-        replace lOldRdrName = lOldRdrName
+        replaceLoc :: forall an. Typeable an => LocatedAn an RdrName -> LocatedAn an RdrName
+        replaceLoc (L srcSpan oldRdrName)
+            | isRef (locA srcSpan) = L srcSpan $ replace oldRdrName
+        replaceLoc lOldRdrName = lOldRdrName
 #else
-renameModRefs newNameText refs = everywhere $ mkT replace
+renameRefs newName refs = everywhere $ mkT replaceLoc
     where
-        replace :: Located RdrName -> Located RdrName
-        replace (L srcSpan oldRdrName)
-            | isRef srcSpan = L srcSpan $ newRdrName oldRdrName
-        replace lOldRdrName = lOldRdrName
+        replaceLoc :: Located RdrName -> Located RdrName
+        replaceLoc (L srcSpan oldRdrName)
+            | isRef srcSpan = L srcSpan $ replace oldRdrName
+        replaceLoc lOldRdrName = lOldRdrName
 #endif
+
+        replace :: RdrName -> RdrName
+        replace (Qual modName _) = Qual modName newName
+        replace _                = Unqual newName
 
         isRef :: SrcSpan -> Bool
         isRef = (`elem` refs) . fromJust . srcSpanToLocation
 
-        newRdrName :: RdrName -> RdrName
-        newRdrName oldRdrName = case oldRdrName of
-            Qual modName _ -> Qual modName newOccName
-            _              -> Unqual newOccName
-
-        newOccName = mkTcOcc $ T.unpack newNameText
--------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
 -- Reference finding
 
 -- | Note: We only find exact name occurences (i.e. type reference "depth" is 0).
-refsAtName :: IdeState -> NormalizedFilePath -> Name -> ExceptT [Char] (LspT Config IO) [Location]
+refsAtName ::
+    MonadIO m =>
+    IdeState ->
+    NormalizedFilePath ->
+    Name ->
+    ExceptT String m (HashSet Location)
 refsAtName state nfp name = do
     ShakeExtras{withHieDb} <- liftIO $ runAction "Rename.HieDb" state getShakeExtras
     ast <- safeGetHieAst state nfp
-    astRefs <- handleMaybe "Error: Could not get name AST references" $ getNameAstLocations name ast
     dbRefs <- case nameModule_maybe name of
         Nothing -> pure []
-        Just mod -> liftIO $ mapMaybe rowToLoc <$>
-            withHieDb (\hieDb ->
-              findReferences
-                  hieDb
-                  True
-                  (nameOccName name)
-                  (Just $ moduleName mod)
-                  (Just $ moduleUnit mod)
-                  [fromNormalizedFilePath nfp]
-              )
-    pure $ nubOrd $ astRefs ++ dbRefs
+        Just mod -> liftIO $ mapMaybe rowToLoc <$> withHieDb (\hieDb ->
+            findReferences
+                hieDb
+                True
+                (nameOccName name)
+                (Just $ moduleName mod)
+                (Just $ moduleUnit mod)
+                [fromNormalizedFilePath nfp]
+            )
+    pure $ HS.fromList $ getNameLocs name ast ++ dbRefs
 
-getNameAstLocations :: Name -> (HieAstResult, PositionMapping) -> Maybe [Location]
-getNameAstLocations name (HAR _ _ rm _ _, mapping) =
-    mapMaybe (toCurrentLocation mapping . realSrcSpanToLocation . fst) <$> M.lookup (Right name) rm
+getNameLocs :: Name -> (HieAstResult, PositionMapping) -> [Location]
+getNameLocs name (HAR _ _ rm _ _, pm) =
+    mapMaybe (toCurrentLocation pm . realSrcSpanToLocation . fst)
+             (concat $ M.lookup (Right name) rm)
 
--------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
 -- Util
 
 getNameAtPos :: IdeState -> NormalizedFilePath -> Position -> ExceptT String (LspT Config IO) Name
 getNameAtPos state nfp pos = do
-    (HAR{hieAst}, mapping) <- safeGetHieAst state nfp
-    handleMaybe "Error: could not find name at position" $ listToMaybe $
-        getAstNamesAtPoint hieAst pos mapping
-
-nfpToUri :: NormalizedFilePath -> Uri
-nfpToUri = filePathToUri . fromNormalizedFilePath
-
-safeUriToNfp :: (Monad m) => Uri -> ExceptT String m NormalizedFilePath
-safeUriToNfp = handleMaybe "Error: Could not get uri" . fmap toNormalizedFilePath . uriToFilePath
+    (HAR{hieAst}, pm) <- safeGetHieAst state nfp
+    handleMaybe ("No name at position: " ++ show pos) $ listToMaybe $ getNamesAtPoint hieAst pos pm
 
 safeGetHieAst ::
     MonadIO m =>
     IdeState ->
     NormalizedFilePath ->
     ExceptT String m (HieAstResult, PositionMapping)
-safeGetHieAst state = handleMaybeM "Error: Could not get AST" . liftIO .
-    runAction "Rename.GetHieAst" state . useWithStale GetHieAst
+safeGetHieAst state nfp = handleMaybeM
+    ("No AST for file: " ++ show nfp)
+    (liftIO $ runAction "Rename.GetHieAst" state $ useWithStale GetHieAst nfp)
+
+safeUriToNfp :: (Monad m) => Uri -> ExceptT String m NormalizedFilePath
+safeUriToNfp uri = handleMaybe
+    ("No filepath for uri: " ++ show uri)
+    (toNormalizedFilePath <$> uriToFilePath uri)
+
+-- Head is safe since groups are non-empty
+collectWith :: (Hashable a, Eq a, Eq b) => (a -> b) -> HashSet a -> [(b, HashSet a)]
+collectWith f = map (\a -> (f $ head a, HS.fromList a)) . groupOn f . HS.toList
 
 locToUri :: Location -> Uri
 locToUri (Location uri _) = uri
+
+nfpToUri :: NormalizedFilePath -> Uri
+nfpToUri = filePathToUri . fromNormalizedFilePath
