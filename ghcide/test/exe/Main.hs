0b2821fd1ea775e9f61c132c75d82bd322af8377
Zubin Duggal <zubin.duggal@gmail.com>
2022-04-16 23:41:50 +0530

    Disable flaky test 'add missing module (non workspace)'

--- ghcide/test/exe/Main.hs
+++ ghcide/test/exe/Main.hs
@@ -421,7 +421,7 @@ diagnosticTests = testGroup "diagnostics"
       let contentA = T.unlines [ "module ModuleA where" ]
       _ <- createDoc "ModuleA.hs" "haskell" contentA
       expectDiagnostics [("ModuleB.hs", [])]
-  , ignoreInWindowsBecause "Broken in windows" $ testSessionWait "add missing module (non workspace)" $ do
+  , ignoreTestBecause "Flaky #2831" $ testSessionWait "add missing module (non workspace)" $ do
       -- need to canonicalize in Mac Os
       tmpDir <- liftIO $ canonicalizePath =<< getTemporaryDirectory
       let contentB = T.unlines
