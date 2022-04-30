89cdb059ca6fd642aceffaa18f63c8414cafd326
Providence Salumu <Providence.Salumu@smunix.com>
2022-04-30 13:22:38 -0400

    ghc884: fixup aeson version to 1.5.2.0

--- configuration-ghc-88.nix
+++ configuration-ghc-88.nix
@@ -24,6 +24,9 @@ let
       stylish-haskell = hself.callCabal2nixWithOptions "stylish-haskell"
         inputs.stylish-haskell-01220 (pkgs.lib.concatStringsSep " " [ ]) { };
 
+      aeson = hself.callCabal2nixWithOptions "aeson" inputs.aeson-1520
+        (pkgs.lib.concatStringsSep " " [ ]) { };
+
       brittany = hself.callCabal2nixWithOptions "brittany" inputs.brittany-01312
         (pkgs.lib.concatStringsSep " " [ ]) { };
 
