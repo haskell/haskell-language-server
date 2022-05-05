796b292f4fa103c337b9bfd00ec2012d0e449309
Lei Zhu <julytreee@gmail.com>
2022-05-05 17:16:24 +0800

    Fix hls-call-hierarchy-plugin tests (#2888)
    * Wait for kick done
    
    * Enable test on Windows
    
    * Wait for kick done

--- plugins/hls-call-hierarchy-plugin/test/Main.hs
+++ plugins/hls-call-hierarchy-plugin/test/Main.hs
@@ -197,6 +197,7 @@ incomingCallsTests =
       testCase "xdata unavailable" $
         runSessionWithServer plugin testDataDir $ do
           doc <- createDoc "A.hs" "haskell" $ T.unlines ["a=3", "b=a"]
+          waitForKickDone
           [item] <- Test.prepareCallHierarchy (mkPrepareCallHierarchyParam doc 1 0)
           let expected = [CallHierarchyIncomingCall item (List [mkRange 1 2 1 3])]
           Test.prepareCallHierarchy (mkPrepareCallHierarchyParam doc 0 0) >>=
@@ -321,6 +322,7 @@ outgoingCallsTests =
       testCase "xdata unavailable" $ withTempDir $ \dir ->
         runSessionWithServer plugin dir $ do
           doc <- createDoc "A.hs" "haskell" $ T.unlines ["a=3", "b=a"]
+          waitForKickDone
           [item] <- Test.prepareCallHierarchy (mkPrepareCallHierarchyParam doc 0 1)
           let expected = [CallHierarchyOutgoingCall item (List [mkRange 1 2 1 3])]
           Test.prepareCallHierarchy (mkPrepareCallHierarchyParam doc 1 0) >>=
@@ -424,6 +426,7 @@ incomingCallTestCase :: T.Text -> Int -> Int -> [(Int, Int)] -> [Range] -> Asser
 incomingCallTestCase contents queryX queryY positions ranges = withTempDir $ \dir ->
   runSessionWithServer plugin dir $ do
     doc <- createDoc "A.hs" "haskell" contents
+    waitForKickDone
     items <- concatMapM (\((x, y), range) ->
       Test.prepareCallHierarchy (mkPrepareCallHierarchyParam doc x y)
           <&> map (, range)
@@ -443,9 +446,11 @@ incomingCallMultiFileTestCase :: FilePath -> Int -> Int -> M.Map FilePath [((Int
 incomingCallMultiFileTestCase filepath queryX queryY mp =
   runSessionWithServer plugin testDataDir $ do
     doc <- openDoc filepath "haskell"
-    items <- fmap concat $ sequence $ M.elems $ M.mapWithKey (\fp pr ->
-              openDoc fp "haskell" >>= \p ->
-                concatMapM (\((x, y), range) ->
+    waitForKickDone
+    items <- fmap concat $ sequence $ M.elems $ M.mapWithKey (\fp pr -> do
+              p <- openDoc fp "haskell"
+              waitForKickDone
+              concatMapM (\((x, y), range) ->
                   Test.prepareCallHierarchy (mkPrepareCallHierarchyParam p x y)
                     <&> map (, range)
                 ) pr) mp
@@ -463,6 +468,7 @@ outgoingCallTestCase :: T.Text -> Int -> Int -> [(Int, Int)] -> [Range] -> Asser
 outgoingCallTestCase contents queryX queryY positions ranges = withTempDir $ \dir ->
   runSessionWithServer plugin dir $ do
     doc <- createDoc "A.hs" "haskell" contents
+    waitForKickDone
     items <- concatMapM (\((x, y), range) ->
       Test.prepareCallHierarchy (mkPrepareCallHierarchyParam doc x y)
           <&> map (, range)
@@ -481,9 +487,11 @@ outgoingCallMultiFileTestCase :: FilePath -> Int -> Int -> M.Map FilePath [((Int
 outgoingCallMultiFileTestCase filepath queryX queryY mp =
   runSessionWithServer plugin testDataDir $ do
     doc <- openDoc filepath "haskell"
-    items <- fmap concat $ sequence $ M.elems $ M.mapWithKey (\fp pr ->
-              openDoc fp "haskell" >>= \p ->
-                concatMapM (\((x, y), range) ->
+    waitForKickDone
+    items <- fmap concat $ sequence $ M.elems $ M.mapWithKey (\fp pr -> do
+              p <- openDoc fp "haskell"
+              waitForKickDone
+              concatMapM (\((x, y), range) ->
                   Test.prepareCallHierarchy (mkPrepareCallHierarchyParam p x y)
                     <&> map (, range)
                 ) pr) mp
@@ -500,6 +508,7 @@ oneCaseWithCreate :: T.Text -> Int -> Int -> (Uri -> CallHierarchyItem) -> Asser
 oneCaseWithCreate contents queryX queryY expected = withTempDir $ \dir ->
   runSessionWithServer plugin dir $ do
     doc <- createDoc "A.hs" "haskell" contents
+    waitForKickDone
     Test.prepareCallHierarchy (mkPrepareCallHierarchyParam doc queryX queryY) >>=
       \case
         [item] -> liftIO $ item @?= expected (doc ^. L.uri)
