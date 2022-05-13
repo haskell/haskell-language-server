haskell/haskell-language-server

commit 8cbefb71bc3aff7c19d4874607b6a374f2307fc3

Consolidate all cabal.project files (#2866)

configuration-ghc-921.nix
@@ -1,51 +1,0 @@
-# nix version of cabal-ghc901.project
-{ pkgs, inputs }:
-
-let
-  disabledPlugins = [
-    "hls-brittany-plugin"
-    "hls-hlint-plugin"
-    "hls-haddock-comments-plugin"
-    "hls-tactics-plugin"
-    # That one is not technically a plugin, but by putting it in this list, we
-    # get it removed from the top level list of requirement and it is not pull
-    # in the nix shell.
-    "shake-bench"
-  ];
-
-  hpkgsOverride = hself: hsuper:
-    with pkgs.haskell.lib;
-    {
-      hlsDisabledPlugins = disabledPlugins;
-
-      fourmolu = hself.callCabal2nix "fourmolu" inputs.fourmolu {};
-      primitive-extras = hself.primitive-extras_0_10_1_2;
-      ghc-exactprint = hself.callCabal2nix "ghc-exactprint" inputs.ghc-exactprint {};
-      constraints-extras = hself.callCabal2nix "constraints-extras" inputs.constraints-extras {};
-      retrie = hself.callCabal2nix "retrie" inputs.retrie {};
-
-      # Hlint is still broken
-      hlint = doJailbreak (hself.callCabal2nix "hlint" inputs.hlint {});
-      hiedb = hself.hiedb_0_4_1_0;
-
-      # Re-generate HLS drv excluding some plugins
-      haskell-language-server =
-        hself.callCabal2nixWithOptions "haskell-language-server" ./.
-        (pkgs.lib.concatStringsSep " " [
-          "-f-brittany"
-          "-f-hlint"
-          "-f-haddockComments"
-          "-f-tactics"
-        ]) { };
-
-      # YOLO
-      mkDerivation = args:
-        hsuper.mkDerivation (args // {
-          jailbreak = true;
-          doCheck = false;
-        });
-    };
-in {
-  inherit disabledPlugins;
-  tweakHpkgs = hpkgs: hpkgs.extend hpkgsOverride;
-}

.github/actions/setup-build/action.yml
@@ -121,6 +121,7 @@ runs:
 
     - name: Form the package list ('cabal.project.freeze')
       run: |
+        rm cabal.project.freeze || \
         cabal v2-freeze && \
         echo "" && \
         echo 'Output:' && \

.gitlab-ci.yml
@@ -16,11 +16,11 @@ variables:
     - GHC_VERSION: 8.10.7
       CABAL_PROJECT: cabal.project
     - GHC_VERSION: 9.0.2
-      CABAL_PROJECT: cabal-ghc90.project
+      CABAL_PROJECT: cabal.project
     - GHC_VERSION: 9.2.2
-      CABAL_PROJECT: cabal-ghc92.project
+      CABAL_PROJECT: cabal.project
     - GHC_VERSION: 9.2.1
-      CABAL_PROJECT: cabal-ghc92.project
+      CABAL_PROJECT: cabal.project
 
 workflow:
   rules:

bindist/ghcs
@@ -1,6 +1,6 @@
 8.6.5,cabal.project
 8.8.4,cabal.project
 8.10.7,cabal.project
-9.0.2,cabal-ghc90.project
-9.2.1,cabal-ghc92.project
-9.2.2,cabal-ghc92.project
+9.0.2,cabal.project
+9.2.1,cabal.project
+9.2.2,cabal.project

bindist/ghcs-FreeBSD
@@ -1,2 +1,2 @@
 8.10.7,cabal.project
-9.0.2,cabal-ghc90.project
+9.0.2,cabal.project

bindist/ghcs-Msys
@@ -1,4 +1,4 @@
 8.10.7,cabal.project
-9.0.2,cabal-ghc90.project
-9.2.2,cabal-ghc92.project
-9.2.1,cabal-ghc92.project
+9.0.2,cabal.project
+9.2.2,cabal.project
+9.2.1,cabal.project

cabal-ghc90.project
@@ -1,58 +1,0 @@
-packages:
-         ./
-         ./hie-compat
-         ./shake-bench
-         ./hls-graph
-         ./ghcide
-         ./hls-plugin-api
-         ./hls-test-utils
-         ./plugins/hls-tactics-plugin
-         ./plugins/hls-brittany-plugin
-         ./plugins/hls-stylish-haskell-plugin
-         ./plugins/hls-fourmolu-plugin
-         ./plugins/hls-class-plugin
-         ./plugins/hls-eval-plugin
-         ./plugins/hls-explicit-imports-plugin
-         ./plugins/hls-refine-imports-plugin
-         ./plugins/hls-hlint-plugin
-         ./plugins/hls-rename-plugin
-         ./plugins/hls-retrie-plugin
-         ./plugins/hls-haddock-comments-plugin
-         ./plugins/hls-splice-plugin
-         ./plugins/hls-qualify-imported-names-plugin
-         ./plugins/hls-floskell-plugin
-         ./plugins/hls-pragmas-plugin
-         ./plugins/hls-module-name-plugin
-         ./plugins/hls-ormolu-plugin
-         ./plugins/hls-call-hierarchy-plugin
-         ./plugins/hls-alternate-number-format-plugin
-         ./plugins/hls-selection-range-plugin
-         ./plugins/hls-change-type-signature-plugin
-
-tests: true
-
-package *
-  ghc-options: -haddock
-  test-show-details: direct
-
-write-ghc-environment-files: never
-
-index-state: 2022-04-27T09:22:49Z
-
-constraints:
-
--- although we are not building all plugins cabal solver phase is run for all packages
--- this way we track explicitly all transitive dependencies which need support for ghc-9
-allow-newer:
-
-  -- brittany: update ghc bounds in hls.cabal when those are removed
-  -- https://github.com/lspitzner/multistate/pull/8
-  multistate:base,
-  -- https://github.com/lspitzner/data-tree-print/pull/3
-  data-tree-print:base,
-  -- https://github.com/lspitzner/butcher/pull/8
-  butcher:base,
-
-  -- ghc-9.0.2 specific
-  -- for ghcide:test via ghc-typelits-knownnat
-  ghc-typelits-natnormalise:ghc-bignum

cabal-ghc92.project
@@ -1,93 +1,0 @@
-packages:
-         ./
-         ./hie-compat
-         ./shake-bench
-         ./hls-graph
-         ./ghcide
-         ./hls-plugin-api
-         ./hls-test-utils
-         ./plugins/hls-tactics-plugin
-         ./plugins/hls-brittany-plugin
-         ./plugins/hls-stylish-haskell-plugin
-         ./plugins/hls-fourmolu-plugin
-         ./plugins/hls-class-plugin
-         ./plugins/hls-eval-plugin
-         ./plugins/hls-explicit-imports-plugin
-         ./plugins/hls-refine-imports-plugin
-         ./plugins/hls-hlint-plugin
-         ./plugins/hls-rename-plugin
-         ./plugins/hls-retrie-plugin
-         ./plugins/hls-haddock-comments-plugin
-         ./plugins/hls-splice-plugin
-         ./plugins/hls-qualify-imported-names-plugin
-         ./plugins/hls-floskell-plugin
-         ./plugins/hls-pragmas-plugin
-         ./plugins/hls-module-name-plugin
-         ./plugins/hls-ormolu-plugin
-         ./plugins/hls-call-hierarchy-plugin
-         ./plugins/hls-alternate-number-format-plugin
-         ./plugins/hls-selection-range-plugin
-         ./plugins/hls-change-type-signature-plugin
-
-with-compiler: ghc-9.2.2
-
-tests: true
-
-package *
-  ghc-options: -haddock
-  test-show-details: direct
-
-write-ghc-environment-files: never
-
-index-state: 2022-04-27T09:22:49Z
-
-constraints:
-  -- These plugins don't build/work on GHC92 yet
-  haskell-language-server
-    +ignore-plugins-ghc-bounds
-    -brittany
-    -haddockComments
-    -retrie
-    -splice
-    -tactic,
-  ghc-lib-parser-ex -auto,
-  hlint +ghc-lib,
-  stylish-haskell +ghc-lib
-
-allow-newer:
-  -- for shake-bench
-  Chart:lens,
-  Chart-diagrams:lens,
-
-  -- for head.hackage
-  primitive-unlifted:base,
-
-  brittany:ghc-boot,
-  brittany:ghc-boot-th,
-  brittany:ghc,
-  brittany:ghc-exactprint,
-  brittany:bytestring,
-  brittany:base,
-  -- https://github.com/lspitzner/multistate/pull/8
-  multistate:base,
-  -- https://github.com/lspitzner/data-tree-print/pull/3
-  data-tree-print:base,
-  -- https://github.com/lspitzner/butcher/pull/8
-  butcher:base,
-
-  hls-haddock-comments-plugin:ghc-exactprint,
-
-  implicit-hie-cradle:bytestring,
-  implicit-hie-cradle:time,
-
-  -- For tactics
-  ghc-source-gen:ghc,
-
-  -- for ghcide:test via ghc-typelits-knownnat
-  ghc-typelits-natnormalise:ghc-bignum,
-
-  hiedb:base,
-  retrie:ghc-exactprint
-
-allow-older:
-  primitive-extras:primitive-unlifted

commit 3dabe1e41af2d8f5b3dace54f8615e8833e14f7b

wip: restore support for nix for GHC 921
Support was broken by
https://github.com/haskell/haskell-language-server/pull/2866 where the
configuration-ghc-921 file was removed.

https://github.com/haskell/haskell-language-server/pull/2892 will indeed
restore these file and fix the build with GHC 922, but I was in need a
GHC 921 support at work.

configuration-ghc-921.nix
@@ -1,0 +1,55 @@
+# nix version of cabal-ghc901.project
+{ pkgs, inputs }:
+
+let
+  disabledPlugins = [
+    "hls-brittany-plugin"
+    "hls-hlint-plugin"
+    "hls-haddock-comments-plugin"
+    "hls-tactics-plugin"
+    "hls-stylish-haskell-plugin"
+    "hls-class-plugin"
+    # That one is not technically a plugin, but by putting it in this list, we
+    # get it removed from the top level list of requirement and it is not pull
+    # in the nix shell.
+    "shake-bench"
+  ];
+
+  hpkgsOverride = hself: hsuper:
+    with pkgs.haskell.lib;
+    {
+      hlsDisabledPlugins = disabledPlugins;
+
+      fourmolu = hself.callCabal2nix "fourmolu" inputs.fourmolu {};
+      primitive-extras = hself.primitive-extras_0_10_1_2;
+      ghc-exactprint = hself.callCabal2nix "ghc-exactprint" inputs.ghc-exactprint {};
+      constraints-extras = hself.callCabal2nix "constraints-extras" inputs.constraints-extras {};
+      retrie = hself.callCabal2nix "retrie" inputs.retrie {};
+
+      # Hlint is still broken
+      hlint = doJailbreak (hself.callCabal2nix "hlint" inputs.hlint {});
+      hiedb = hself.hiedb_0_4_1_0;
+
+      # Re-generate HLS drv excluding some plugins
+      haskell-language-server =
+        hself.callCabal2nixWithOptions "haskell-language-server" ./.
+        (pkgs.lib.concatStringsSep " " [
+          "-f-brittany"
+          "-f-hlint"
+          "-f-haddockComments"
+          "-f-tactics"
+          "-f-stylishHaskell"
+          "-f-class"
+        ]) { };
+
+      # YOLO
+      mkDerivation = args:
+        hsuper.mkDerivation (args // {
+          jailbreak = true;
+          doCheck = false;
+        });
+    };
+in {
+  inherit disabledPlugins;
+  tweakHpkgs = hpkgs: hpkgs.extend hpkgsOverride;
+}