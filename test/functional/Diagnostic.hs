ef84255dcc7d76770248f96f66c75a341d514415
kokobd <contact@zelinf.net>
2022-08-09 19:43:51 +0800

    Revert "fix func-tests"
    This reverts commit 2ecd76d0bafc25df12ac818f1c72b0f56a79c9df.

--- test/functional/Diagnostic.hs
+++ test/functional/Diagnostic.hs
@@ -23,7 +23,7 @@ basicTests = testGroup "Diagnostics work" [
             diags <- waitForDiagnosticsFromSource doc "example2"
             reduceDiag <- liftIO $ inspectDiagnostic diags ["example2 diagnostic, hello world"]
             liftIO $ do
-                length diags @?= 2
+                length diags @?= 1
                 reduceDiag ^. LSP.range @?= Range (Position 0 0) (Position 1 0)
                 reduceDiag ^. LSP.severity @?= Just DsError
     ]
