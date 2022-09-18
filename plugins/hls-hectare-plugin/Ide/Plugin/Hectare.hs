ceac012dda7ef192ac72a2221963fbe87ffd1a6b
Pepe Iborra <pepeiborra@fb.com>
2022-09-18 13:50:59 +0200

    Disabled by default

--- plugins/hls-hectare-plugin/Ide/Plugin/Hectare.hs
+++ plugins/hls-hectare-plugin/Ide/Plugin/Hectare.hs
@@ -1,13 +1,22 @@
 module Ide.Plugin.Hectare (descriptor) where
 
+import           Data.Default
 import           Development.IDE.GHC.Compat
 import           Development.IDE.GHC.Compat.Util
 import qualified ECTA.Plugin                     as GHC
+import           Ide.Plugin.Config
 import           Ide.Types
 
 descriptor :: PluginId -> PluginDescriptor s
 descriptor pluginId = (defaultPluginDescriptor pluginId)
   { pluginModifyDynflags = staticPlugin
+  -- This plugin is opt-in
+  , pluginConfigDescriptor = defaultConfigDescriptor {
+    configHasDiagnostics = True,
+    configInitialGenericConfig = Just def {
+        plcGlobalOn = False
+    }
+  }
   }
 
 staticPlugin :: DynFlagsModifications
