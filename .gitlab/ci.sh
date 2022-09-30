e905ee169c63fe84192c603acc3292bcc36ddf20
Zubin Duggal <zubin.duggal@gmail.com>
2022-10-01 00:31:21 +0530

    debug

--- .gitlab/ci.sh
+++ .gitlab/ci.sh
@@ -36,6 +36,7 @@ case "$(uname -s)" in
 		case "$(/usr/bin/arch)" in
 			aarch64|arm64|armv8l)
 				export PATH="$CI_PROJECT_DIR/.brew/opt/llvm@14/bin:$PATH"
+				ls "$CI_PROJECT_DIR/.brew/opt/llvm@14/bin"
 				curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | arch -arm64 /bin/bash
 				export C_INCLUDE_PATH="`xcrun --show-sdk-path`/usr/include/ffi"
 				;;
