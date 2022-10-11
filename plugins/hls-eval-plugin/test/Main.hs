5f5abab1367618a05a71ba98c397fb5985570c7d
andys8 <andys8@users.noreply.github.com>
2022-10-11 23:20:41 +0200

    Eval: Disable CPP windows test

--- plugins/hls-eval-plugin/test/Main.hs
+++ plugins/hls-eval-plugin/test/Main.hs
@@ -140,7 +140,9 @@ tests =
   , goldenWithEval "Can handle eval inside nested comment properly" "TNested" "hs"
   , goldenWithEval "Test on last line insert results correctly" "TLastLine" "hs"
   , testGroup "with preprocessors"
-    [ goldenWithEval "CPP support" "TCPP" "hs"
+    [ knownBrokenInEnv [HostOS Windows]
+        "CPP eval on Windows and/or GHC <= 8.6 fails for some reasons" $
+        goldenWithEval "CPP support" "TCPP" "hs"
     , goldenWithEval "Literate Haskell Bird Style" "TLHS" "lhs"
     ]
   , goldenWithEval "Works with NoImplicitPrelude" "TNoImplicitPrelude" "hs"
