f8e2549e5cb5c660a685b633f74d7cc4f12cd8da
Berk Ozkutuk <berk.ozkutuk@tweag.io>
2022-10-21 15:25:32 +0200

    Minor code reorganization

--- plugins/hls-explicit-record-fields-plugin/src/Ide/Plugin/ExplicitFields.hs
+++ plugins/hls-explicit-record-fields-plugin/src/Ide/Plugin/ExplicitFields.hs
@@ -75,35 +75,6 @@ import           Language.LSP.Types              (CodeAction (..),
 import qualified Language.LSP.Types.Lens         as L
 
 
--- `Outputable` instance of `HsRecFields` does smart things to print
--- the records that originally had wildcards with dots, even after they
--- are removed by the renamer pass. Here `rec_dotdot` is set to
--- `Nothing` so that fields are printed without such post-processing.
-preprocessRecord :: HsRecFields (GhcPass c) arg -> HsRecFields (GhcPass c) arg
-preprocessRecord flds = flds { rec_dotdot = Nothing , rec_flds = rec_flds' }
-  where
-    no_pun_count = maybe (length (rec_flds flds)) unLoc (rec_dotdot flds)
-    -- Field binds of the explicit form (e.g. `{ a = a' }`) should be
-    -- left as is, hence the split.
-    (no_puns, puns) = splitAt no_pun_count (rec_flds flds)
-    -- `hsRecPun` is set to `True` in order to pretty-print the fields as field
-    -- puns (since there is similar mechanism in the `Outputable` instance as
-    -- explained above).
-    puns' = map (mapLoc (\fld -> fld { hfbPun = True })) puns
-    rec_flds' = no_puns <> puns'
-
-showRecordPat :: Outputable (Pat (GhcPass c)) => Pat (GhcPass c) -> Maybe Text
-showRecordPat pat@(ConPat _ _ (RecCon flds)) =
-  Just $ printOutputable $
-    pat { pat_args = RecCon (preprocessRecord flds) }
-showRecordPat _ = Nothing
-
-showRecordCon :: Outputable (HsExpr (GhcPass c)) => HsExpr (GhcPass c) -> Maybe Text
-showRecordCon expr@(RecordCon _ _ flds) =
-  Just $ printOutputable $
-    expr { rcon_flds = preprocessRecord flds }
-showRecordCon _ = Nothing
-
 data Log = LogShake Shake.Log
 
 instance Pretty Log where
@@ -115,98 +86,6 @@ descriptor recorder plId = (defaultPluginDescriptor plId)
   { pluginHandlers = mkPluginHandler STextDocumentCodeAction codeActionProvider
   , pluginRules = collectRecordsRule recorder
   }
-
-data CollectRecords = CollectRecords
-                    deriving (Eq, Show, Generic)
-
-instance Hashable CollectRecords
-instance NFData CollectRecords
-
-data CollectRecordsResult = CRR
-  { recordInfos       :: ![RenderedRecordInfo]
-  , enabledExtensions :: ![GhcExtension]
-  }
-  deriving (Generic)
-
-instance NFData CollectRecordsResult
-
-instance Show CollectRecordsResult where
-  show _ = "<CollectRecordsResult>"
-
-type instance RuleResult CollectRecords = CollectRecordsResult
-
-newtype GhcExtension = GhcExtension { unExt :: Extension }
-
-instance NFData GhcExtension where
-  rnf x = x `seq` ()
-
-data RecordInfo
-  = RecordInfoPat !SrcSpan !(Pat (GhcPass 'Renamed))
-  | RecordInfoCon !SrcSpan !(HsExpr (GhcPass 'Renamed))
-
-data RenderedRecordInfo = RenderedRecordInfo
-  { renderedSrcSpan :: !SrcSpan
-  , renderedRecord  :: !Text
-  }
-  deriving (Generic)
-
-instance NFData RenderedRecordInfo
-
-renderRecordInfo :: RecordInfo -> Maybe RenderedRecordInfo
-renderRecordInfo (RecordInfoPat ss pat) = RenderedRecordInfo ss <$> showRecordPat pat
-renderRecordInfo (RecordInfoCon ss expr) = RenderedRecordInfo ss <$> showRecordCon expr
-
--- collectRecords :: GenericQ [LPat (GhcPass 'Renamed)]
-collectRecords :: GenericQ [RecordInfo]
-collectRecords = everything (<>) (maybeToList . (Nothing `mkQ` getRecPatterns `extQ` getRecCons))
-
-getRecCons :: LHsExpr (GhcPass 'Renamed) -> Maybe RecordInfo
-getRecCons e@(unLoc -> RecordCon _ _ flds)
-  | isJust (rec_dotdot flds) = Just $ mkRecInfo e
-  where
-    mkRecInfo :: LHsExpr (GhcPass 'Renamed) -> RecordInfo
-    mkRecInfo expr = RecordInfoCon (getLoc expr) (unLoc expr)
-getRecCons _ = Nothing
-
-getRecPatterns :: LPat (GhcPass 'Renamed) -> Maybe RecordInfo
-getRecPatterns conPat@(unLoc -> ConPat _ _ (RecCon flds))
-  | isJust (rec_dotdot flds) = Just $ mkRecInfo conPat
-  where
-    mkRecInfo :: LPat (GhcPass 'Renamed) -> RecordInfo
-    mkRecInfo pat = RecordInfoPat (getLoc pat) (unLoc pat)
-getRecPatterns _ = Nothing
-
-getEnabledExtensions :: TcModuleResult -> [GhcExtension]
-getEnabledExtensions = map GhcExtension . toList . extensionFlags . ms_hspp_opts . pm_mod_summary . tmrParsed
-
-getRecords :: TcModuleResult -> [RecordInfo]
-getRecords (tmrRenamed -> (hs_valds -> valBinds,_,_,_)) =
-  collectRecords valBinds
-
-collectRecordsRule :: Recorder (WithPriority Log) -> Rules ()
-collectRecordsRule recorder = define (cmapWithPrio LogShake recorder) $ \CollectRecords nfp -> do
-  tmr <- use TypeCheck nfp
-  let exts = getEnabledExtensions <$> tmr
-      recs = getRecords <$> tmr
-      renderedRecs = mapMaybe renderRecordInfo <$> recs
-  pure ([], CRR <$> renderedRecs <*> exts)
-
-collectRecords' :: MonadIO m => IdeState -> NormalizedFilePath -> ExceptT String m CollectRecordsResult
-collectRecords' ideState =
-  handleMaybeM "Unable to TypeCheck"
-    . liftIO
-    . runAction "ExplicitFields" ideState
-    . use CollectRecords
-
-collectRecordsInRange :: MonadIO m => Range -> IdeState -> NormalizedFilePath -> ExceptT String m CollectRecordsResult
-collectRecordsInRange range ideState nfp = do
-  CRR renderedRecs exts <- collectRecords' ideState nfp
-  pure $ CRR (filter inRange renderedRecs) exts
-
-  where
-    inRange :: RenderedRecordInfo -> Bool
-    inRange (RenderedRecordInfo ss _) = maybe False (subRange range) (srcSpanToRange ss)
-
 
 codeActionProvider :: PluginMethodHandler IdeState 'TextDocumentCodeAction
 codeActionProvider ideState pId (CodeActionParams _ _ docId range _) = pluginResponse $ do
@@ -250,13 +129,132 @@ codeActionProvider ideState pId (CodeActionParams _ _ docId range _) = pluginRes
       where
         changes = Just $ HashMap.singleton (fromNormalizedUri (normalizedFilePathToUri nfp)) (List edits)
 
-mkCodeActionTitle :: [Extension] -> Text
-mkCodeActionTitle exts =
-  if NamedFieldPuns `elem` exts
-    then title
-    else title <> " (needs extension: NamedFieldPuns)"
-    where
-      title = "Expand record wildcard"
+    mkCodeActionTitle :: [Extension] -> Text
+    mkCodeActionTitle exts =
+      if NamedFieldPuns `elem` exts
+        then title
+        else title <> " (needs extension: NamedFieldPuns)"
+        where
+          title = "Expand record wildcard"
+
+collectRecordsRule :: Recorder (WithPriority Log) -> Rules ()
+collectRecordsRule recorder = define (cmapWithPrio LogShake recorder) $ \CollectRecords nfp -> do
+  tmr <- use TypeCheck nfp
+  let exts = getEnabledExtensions <$> tmr
+      recs = getRecords <$> tmr
+      renderedRecs = mapMaybe renderRecordInfo <$> recs
+  pure ([], CRR <$> renderedRecs <*> exts)
+
+getEnabledExtensions :: TcModuleResult -> [GhcExtension]
+getEnabledExtensions = map GhcExtension . toList . extensionFlags . ms_hspp_opts . pm_mod_summary . tmrParsed
+
+getRecords :: TcModuleResult -> [RecordInfo]
+getRecords (tmrRenamed -> (hs_valds -> valBinds,_,_,_)) =
+  collectRecords valBinds
+
+data CollectRecords = CollectRecords
+                    deriving (Eq, Show, Generic)
+
+instance Hashable CollectRecords
+instance NFData CollectRecords
+
+data CollectRecordsResult = CRR
+  { recordInfos       :: ![RenderedRecordInfo]
+  , enabledExtensions :: ![GhcExtension]
+  }
+  deriving (Generic)
+
+instance NFData CollectRecordsResult
+
+instance Show CollectRecordsResult where
+  show _ = "<CollectRecordsResult>"
+
+type instance RuleResult CollectRecords = CollectRecordsResult
+
+newtype GhcExtension = GhcExtension { unExt :: Extension }
+
+instance NFData GhcExtension where
+  rnf x = x `seq` ()
+
+data RecordInfo
+  = RecordInfoPat !SrcSpan !(Pat (GhcPass 'Renamed))
+  | RecordInfoCon !SrcSpan !(HsExpr (GhcPass 'Renamed))
+
+data RenderedRecordInfo = RenderedRecordInfo
+  { renderedSrcSpan :: !SrcSpan
+  , renderedRecord  :: !Text
+  }
+  deriving (Generic)
+
+instance NFData RenderedRecordInfo
+
+renderRecordInfo :: RecordInfo -> Maybe RenderedRecordInfo
+renderRecordInfo (RecordInfoPat ss pat) = RenderedRecordInfo ss <$> showRecordPat pat
+renderRecordInfo (RecordInfoCon ss expr) = RenderedRecordInfo ss <$> showRecordCon expr
+
+-- `Outputable` instance of `HsRecFields` does smart things to print
+-- the records that originally had wildcards with dots, even after they
+-- are removed by the renamer pass. Here `rec_dotdot` is set to
+-- `Nothing` so that fields are printed without such post-processing.
+preprocessRecord :: HsRecFields (GhcPass c) arg -> HsRecFields (GhcPass c) arg
+preprocessRecord flds = flds { rec_dotdot = Nothing , rec_flds = rec_flds' }
+  where
+    no_pun_count = maybe (length (rec_flds flds)) unLoc (rec_dotdot flds)
+    -- Field binds of the explicit form (e.g. `{ a = a' }`) should be
+    -- left as is, hence the split.
+    (no_puns, puns) = splitAt no_pun_count (rec_flds flds)
+    -- `hsRecPun` is set to `True` in order to pretty-print the fields as field
+    -- puns (since there is similar mechanism in the `Outputable` instance as
+    -- explained above).
+    puns' = map (mapLoc (\fld -> fld { hfbPun = True })) puns
+    rec_flds' = no_puns <> puns'
+
+showRecordPat :: Outputable (Pat (GhcPass c)) => Pat (GhcPass c) -> Maybe Text
+showRecordPat pat@(ConPat _ _ (RecCon flds)) =
+  Just $ printOutputable $
+    pat { pat_args = RecCon (preprocessRecord flds) }
+showRecordPat _ = Nothing
+
+showRecordCon :: Outputable (HsExpr (GhcPass c)) => HsExpr (GhcPass c) -> Maybe Text
+showRecordCon expr@(RecordCon _ _ flds) =
+  Just $ printOutputable $
+    expr { rcon_flds = preprocessRecord flds }
+showRecordCon _ = Nothing
+
+collectRecords :: GenericQ [RecordInfo]
+collectRecords = everything (<>) (maybeToList . (Nothing `mkQ` getRecPatterns `extQ` getRecCons))
+
+getRecCons :: LHsExpr (GhcPass 'Renamed) -> Maybe RecordInfo
+getRecCons e@(unLoc -> RecordCon _ _ flds)
+  | isJust (rec_dotdot flds) = Just $ mkRecInfo e
+  where
+    mkRecInfo :: LHsExpr (GhcPass 'Renamed) -> RecordInfo
+    mkRecInfo expr = RecordInfoCon (getLoc expr) (unLoc expr)
+getRecCons _ = Nothing
+
+getRecPatterns :: LPat (GhcPass 'Renamed) -> Maybe RecordInfo
+getRecPatterns conPat@(unLoc -> ConPat _ _ (RecCon flds))
+  | isJust (rec_dotdot flds) = Just $ mkRecInfo conPat
+  where
+    mkRecInfo :: LPat (GhcPass 'Renamed) -> RecordInfo
+    mkRecInfo pat = RecordInfoPat (getLoc pat) (unLoc pat)
+getRecPatterns _ = Nothing
+
+collectRecords' :: MonadIO m => IdeState -> NormalizedFilePath -> ExceptT String m CollectRecordsResult
+collectRecords' ideState =
+  handleMaybeM "Unable to TypeCheck"
+    . liftIO
+    . runAction "ExplicitFields" ideState
+    . use CollectRecords
+
+collectRecordsInRange :: MonadIO m => Range -> IdeState -> NormalizedFilePath -> ExceptT String m CollectRecordsResult
+collectRecordsInRange range ideState nfp = do
+  CRR renderedRecs exts <- collectRecords' ideState nfp
+  pure $ CRR (filter inRange renderedRecs) exts
+
+  where
+    inRange :: RenderedRecordInfo -> Bool
+    inRange (RenderedRecordInfo ss _) = maybe False (subRange range) (srcSpanToRange ss)
 
 -- Copied from hls-alternate-number-format-plugin
 getFirstPragma :: MonadIO m => PluginId -> IdeState -> NormalizedFilePath -> ExceptT String m NextPragmaInfo
