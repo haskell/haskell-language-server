27e9a1033ce47546e95e2475c88643d9f2bb1e3e
Matt Revelle <drone@bitrit.es>
2022-11-19 14:17:13 -0700

    Relocated list of OccName prefixes from the Completions plugin to the CoreFile module. Updated the check for GHC-generated OccName prefixes in AtPoint to use the shared list of OccName prefixes.

--- ghcide/src/Development/IDE/Spans/AtPoint.hs
+++ ghcide/src/Development/IDE/Spans/AtPoint.hs
@@ -34,6 +34,7 @@ import           Development.IDE.Core.PositionMapping
 import           Development.IDE.Core.RuleTypes
 import           Development.IDE.GHC.Compat
 import qualified Development.IDE.GHC.Compat.Util      as Util
+import           Development.IDE.GHC.CoreFile         (occNamePrefixes)
 import           Development.IDE.GHC.Util             (printOutputable)
 import           Development.IDE.Spans.Common
 import           Development.IDE.Types.Options
@@ -231,12 +232,10 @@ atPoint IdeOptions{} (HAR _ hf _ _ kind) (DKMap dm km) env pos = listToMaybe $ p
         isInternal :: (Identifier, IdentifierDetails a) -> Bool
         isInternal (Right n, _) =
           let name = printOutputable n
-              prefix = T.take 2 name
-          in elem prefix ["$d", "$c"]
+          in any (`T.isPrefixOf` name) occNamePrefixes
         isInternal (Left _, _) = False
         filteredNames = filter (not . isInternal) names
         types = nodeType info
-
         prettyNames :: [T.Text]
         prettyNames = map prettyName filteredNames
         prettyName (Right n, dets) = T.unlines $
