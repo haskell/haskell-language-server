288cbf151179988a78467e606ec73f323607e590
Berk Ozkutuk <berk.ozkutuk@tweag.io>
2022-11-19 14:34:31 +0100

    Add test to ensure both filtering approaches are identical

--- hls-plugin-api/test/Ide/PluginUtilsTest.hs
+++ hls-plugin-api/test/Ide/PluginUtilsTest.hs
@@ -1,19 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
 
 module Ide.PluginUtilsTest
     ( tests
     ) where
 
-import           Data.Char          (isPrint)
-import qualified Data.Text          as T
-import           Ide.PluginUtils    (positionInRange, unescape)
-import           Language.LSP.Types (Position (Position), Range (Range))
+import           Data.Char             (isPrint)
+import qualified Data.Set              as Set
+import qualified Data.Text             as T
+import qualified Ide.Plugin.RangeMap   as RangeMap
+import           Ide.PluginUtils       (positionInRange, unescape)
+import           Language.LSP.Types    (Position (..), Range (Range), UInt,
+                                        isSubrangeOf)
 import           Test.Tasty
 import           Test.Tasty.HUnit
+import           Test.Tasty.QuickCheck
 
 tests :: TestTree
 tests = testGroup "PluginUtils"
     [ unescapeTest
+    , localOption (QuickCheckMaxSize 10000) $
+        testProperty "RangeMap-List filtering identical" (prop_rangemapListEq @Int)
     ]
 
 unescapeTest :: TestTree
@@ -33,3 +40,26 @@ unescapeTest = testGroup "unescape"
     , testCase "control characters should not be escaped" $
         unescape "\"\\n\\t\"" @?= "\"\\n\\t\""
     ]
+
+genRange :: Gen Range
+genRange = do
+  x1 <- genPosition
+  delta <- genRangeLength
+  let x2 = x1 { _character = _character x1 + delta }
+  pure $ Range x1 x2
+  where
+    genRangeLength :: Gen UInt
+    genRangeLength = fromInteger <$> chooseInteger (5, 50)
+
+genPosition :: Gen Position
+genPosition = Position
+  <$> (fromInteger <$> chooseInteger (0, 1000))
+  <*> (fromInteger <$> chooseInteger (0, 150))
+
+instance Arbitrary Range where
+  arbitrary = genRange
+
+prop_rangemapListEq :: (Show a, Eq a, Ord a) => Range -> [(Range, a)] -> Property
+prop_rangemapListEq r xs =
+  Set.fromList ((map snd . filter (isSubrangeOf r . fst)) xs)
+    === Set.fromList (RangeMap.filterByRange r (RangeMap.fromList' xs))
