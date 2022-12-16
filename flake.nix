a235ec9d9e51176aafa5e01f66f2d653e8b7929d
Ian-Woo Kim <ianwookim@gmail.com>
2022-12-16 09:25:00 -0800

    Fix fourmolu with -f-fixity-th in nix env (#3400)
    * fix fourmolu with -f-fixity-th in nix env
    
    * added a small comment and link to the issue.

--- flake.nix
+++ flake.nix
@@ -129,7 +129,12 @@
                 then overrideCabal hsuper.ormolu (_: { enableSeparateBinOutput = false; })
                 else hsuper.ormolu;
 
-              fourmolu = hself.callCabal2nix "fourmolu" inputs.fourmolu {};
+              # Due to the following issue, fixity-th should be disabled, especially for darwin.
+              # https://github.com/fourmolu/fourmolu/issues/238
+              fourmolu =
+                addBuildDepend
+                  (appendConfigureFlag (hself.callCabal2nix "fourmolu" inputs.fourmolu {}) "-f-fixity-th")
+                  hself.file-embed;
             };
 
           hlsSources =
