ec9d8b3aad17d5b3bfc51239845ea4d2161d27f1
Finley McIlwaine <finleymcilwaine@gmail.com>
2023-02-27 17:02:51 -0700

    Some refactor tests no longer broken for 9.2

--- plugins/hls-refactor-plugin/test/Main.hs
+++ plugins/hls-refactor-plugin/test/Main.hs
@@ -2416,8 +2416,7 @@ addTypeAnnotationsToLiteralsTest = testGroup "add type annotations to literals t
                , ""
                , "f = seq (\"debug\" :: " <> listOfChar <> ") traceShow \"debug\""
                ])
-  , knownBrokenForGhcVersions [GHC92] "GHC 9.2 only has 'traceShow' in error span" $
-    testSession "add default type to satisfy two constraints" $
+  , testSession "add default type to satisfy two constraints" $
     testFor
     (T.unlines [ "{-# OPTIONS_GHC -Wtype-defaults #-}"
                , "{-# LANGUAGE OverloadedStrings #-}"
@@ -2441,8 +2440,7 @@ addTypeAnnotationsToLiteralsTest = testGroup "add type annotations to literals t
                , ""
                , "f a = traceShow (\"debug\" :: " <> listOfChar <> ") a"
                ])
-  , knownBrokenForGhcVersions [GHC92] "GHC 9.2 only has 'traceShow' in error span" $
-    testSession "add default type to satisfy two constraints with duplicate literals" $
+  , testSession "add default type to satisfy two constraints with duplicate literals" $
     testFor
     (T.unlines [ "{-# OPTIONS_GHC -Wtype-defaults #-}"
                , "{-# LANGUAGE OverloadedStrings #-}"
