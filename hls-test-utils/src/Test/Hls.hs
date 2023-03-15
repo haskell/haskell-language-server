33822d6ea6a1bac74d0a0b91f4e15361f6b4fbe5
Fendor <walross.power@gmail.com>
2023-03-15 17:12:37 +0100

    Remove hslogger from codebase

--- hls-test-utils/src/Test/Hls.hs
+++ hls-test-utils/src/Test/Hls.hs
@@ -240,7 +240,7 @@ pluginTestRecorder = do
 -- See 'runSessionWithServer'' for details.
 initialiseTestRecorder :: Pretty a => [String] -> IO (Recorder (WithPriority a), WithPriority (Doc ann) -> IO ())
 initialiseTestRecorder envVars = do
-    docWithPriorityRecorder <- makeDefaultStderrRecorder Nothing Debug
+    docWithPriorityRecorder <- makeDefaultStderrRecorder Nothing
     -- There are potentially multiple environment variables that enable this logger
     definedEnvVars <- forM envVars (\var -> fromMaybe "0" <$> lookupEnv var)
     let logStdErr = any (/= "0") definedEnvVars
