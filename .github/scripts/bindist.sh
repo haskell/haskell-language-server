4a1336305e7f42cab7c8cce225f8aae508a5fd1e
Julian Ospald <hasufell@posteo.de>
2023-05-27 11:31:54 +0800

    Build 9.6.2 and 9.2.8 in release CI

--- .github/scripts/bindist.sh
+++ .github/scripts/bindist.sh
@@ -25,8 +25,8 @@ case "${TARBALL_EXT}" in
 		: "${GHCS:="$(cd "$CI_PROJECT_DIR/out/${ARTIFACT}" && rm -f ./*.json && for ghc in * ; do printf "%s\n" "$ghc" ; done | sort -r | tr '\n' ' ')"}"
 		emake --version
 		emake GHCUP=ghcup ARTIFACT="${ARTIFACT}" GHCS="${GHCS}" bindist
-		emake GHCUP=ghcup ARTIFACT="${ARTIFACT}"                bindist-tar
 		emake GHCUP=ghcup                        GHCS="${GHCS}" clean-ghcs
+		emake GHCUP=ghcup ARTIFACT="${ARTIFACT}"                bindist-tar
         ;;
     *)
         fail "Unknown TARBALL_EXT: ${TARBALL_EXT}"
