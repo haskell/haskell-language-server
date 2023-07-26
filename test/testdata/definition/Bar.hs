a5628113187db549078ed4523757e321cc294496
Elodie Lander <lander89@gmail.com>
2023-07-26 16:50:52 -0500

    Add gotoDefinition dependency test

--- test/testdata/definition/Bar.hs
+++ test/testdata/definition/Bar.hs
@@ -1,5 +1,7 @@
 module Bar where
 
+import Data.Set (Set, empty)
+
 a = 42
 
 -- These blank lines are here
@@ -7,3 +9,6 @@ a = 42
 -- on a line number larger than
 -- the number of lines in Foo.hs.
 b = 43
+
+emptySet :: Set Integer
+emptySet = empty
