3b9d2423eaf8ebc396781c08d445ddf29bb9d6a6
kokobd <contact@zelinf.net>
2023-08-15 18:53:47 +0000

    wip haddock comments for type signatures

--- plugins/hls-haddock-comments-plugin/src-96/Ide/Plugin/HaddockComments.hs
+++ plugins/hls-haddock-comments-plugin/src-96/Ide/Plugin/HaddockComments.hs
@@ -1,32 +1,29 @@
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE CPP                   #-}
 {-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NamedFieldPuns        #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE ViewPatterns          #-}
 
 module Ide.Plugin.HaddockComments (descriptor, E.Log) where
 
 
-import           Control.Monad                         (join, when)
+import           Control.Monad                          (join)
 import           Control.Monad.IO.Class
-import           Control.Monad.Trans.Class             (lift)
-import qualified Data.Map                              as Map
-import qualified Data.Text                             as T
-import           Development.IDE                       hiding (pluginHandlers)
+import qualified Data.Map                               as Map
+import qualified Data.Text                              as T
+import           Development.IDE                        hiding (pluginHandlers)
 import           Development.IDE.GHC.Compat
 import           Development.IDE.GHC.Compat.ExactPrint
-import           Development.IDE.GHC.ExactPrint        (GetAnnotatedParsedSource (..))
-import qualified Development.IDE.GHC.ExactPrint        as E
+import           Development.IDE.GHC.ExactPrint         (GetAnnotatedParsedSource (..))
+import qualified Development.IDE.GHC.ExactPrint         as E
 import           Development.IDE.Plugin.CodeAction
--- import           Ide.Plugin.HaddockComments.Data       (genForDataDecl)
--- import           Ide.Plugin.HaddockComments.Prelude
+import           Data.Maybe                             (catMaybes)
+import           Development.IDE.Plugin.CodeAction.Util (traceAst)
 import           Ide.Types
-import           Language.Haskell.GHC.ExactPrint
-import           Language.Haskell.GHC.ExactPrint.Types hiding (GhcPs)
-import           Language.Haskell.GHC.ExactPrint.Utils
 import           Language.LSP.Protocol.Message
 import           Language.LSP.Protocol.Types
 
------------------------------------------------------------------------------
 descriptor :: Recorder (WithPriority E.Log) -> PluginId -> PluginDescriptor IdeState
 descriptor recorder plId = mkExactprintPluginDescriptor recorder $
   (defaultPluginDescriptor plId)
@@ -34,11 +31,75 @@ descriptor recorder plId = mkExactprintPluginDescriptor recorder $
     }
 
 codeActionProvider :: PluginMethodHandler IdeState Method_TextDocumentCodeAction
-codeActionProvider ideState _pId (CodeActionParams _ _ (TextDocumentIdentifier uri) range CodeActionContext {_diagnostics = diags}) =
-  do
-    let noErr = and $ (/= Just DiagnosticSeverity_Error) . _severity <$> diags
-        nfp = uriToNormalizedFilePath $ toNormalizedUri uri
-    (fmap astA . join -> pm) <- liftIO $ runAction "HaddockComments.GetAnnotatedParsedSource" ideState $ use GetAnnotatedParsedSource `traverse` nfp
-    case pm of
-      Nothing -> pure . Right $ InL []
-      Just (ast :: ParsedSource) -> pure . Right $ InL []
+codeActionProvider ideState _pId (CodeActionParams _ _ (TextDocumentIdentifier uri) selectionRange CodeActionContext {_diagnostics = diags}) =
+    if noErr
+      then do
+        (fmap astA . join -> pm) <- liftIO $ runAction "HaddockComments.GetAnnotatedParsedSource" ideState $ use GetAnnotatedParsedSource `traverse` nfp
+        case pm of
+          Nothing                    -> respondEmpty
+          Just (pm' :: ParsedSource) ->
+            let topLevelDecls = hsmodDecls . unLoc $ pm'
+                codeActions = catMaybes
+                  [ runGenerator generator decl selectionRange uri |
+                    decl <- topLevelDecls,
+                    generator <- registeredGenerators
+                  ]
+             in pure . Right . InL . fmap InR $ codeActions
+      else respondEmpty
+  where
+    noErr = notElem (Just DiagnosticSeverity_Error) . fmap _severity $ diags
+    nfp = uriToNormalizedFilePath $ toNormalizedUri uri
+    respondEmpty = pure . Right $ InL []
+
+-- | Interface for defining how to generate a code action for a top-level declaration.
+data Generator = Generator
+  { title     :: T.Text
+  , updateAst :: LHsDecl GhcPs -> Maybe (LHsDecl GhcPs)
+  }
+
+runGenerator :: Generator -> LHsDecl GhcPs -> Range -> Uri -> Maybe CodeAction
+runGenerator generator decl@(L annDecl _) selectionRange uri
+  | selectionRange `isIntersectWith` locA annDecl = do
+      modifiedDecl <- updateAst generator decl
+      range' <- toRange (locA annDecl)
+      pure CodeAction {
+        _title = title generator,
+        _kind = Just CodeActionKind_QuickFix,
+        _diagnostics = Nothing,
+        _command = Nothing,
+        _edit = Just WorkspaceEdit
+          { _changes = Just $ Map.singleton uri [TextEdit range' (T.pack $ exactPrint modifiedDecl)]
+          , _documentChanges = Nothing
+          , _changeAnnotations = Nothing
+          },
+        _isPreferred = Nothing,
+        _disabled = Nothing,
+        _data_ = Nothing
+      }
+  | otherwise = Nothing
+
+registeredGenerators :: [Generator]
+registeredGenerators =
+  [ genForSig
+  ]
+
+genForSig :: Generator
+genForSig =
+    Generator {
+      title = "Generate signature comments",
+      updateAst = updateAstForSig
+    }
+
+updateAstForSig :: LHsDecl GhcPs -> Maybe (LHsDecl GhcPs)
+updateAstForSig (L loc (SigD _ typeSig@(TypeSig ann ids (HsWC _ (L _ x))))) =
+  traceAst "TypeSig" typeSig `seq` Nothing
+
+toRange :: SrcSpan -> Maybe Range
+toRange src
+  | (RealSrcSpan s _) <- src,
+    range' <- realSrcSpanToRange s =
+    Just range'
+  | otherwise = Nothing
+
+isIntersectWith :: Range -> SrcSpan -> Bool
+isIntersectWith Range {_start, _end} x = isInsideSrcSpan _start x || isInsideSrcSpan _end x
