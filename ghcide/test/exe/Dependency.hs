95afde55871347f193a106421ad6dc4961efa42b
Elodie Lander <lander89@gmail.com>
2023-08-14 19:09:46 -0500

    Move dependencyTest to top level

--- ghcide/test/exe/Dependency.hs
+++ ghcide/test/exe/Dependency.hs
@@ -32,41 +32,42 @@ tests =
     testGroup "gotoDefinition for dependencies"
         [ dependencyTest
         ]
+
+fileDoneIndexing :: [String] -> Session FilePath
+fileDoneIndexing fpSuffix =
+    skipManyTill anyMessage indexedFile
     where
-        dependencyTest :: TestTree
-        dependencyTest = testSessionWithExtraFiles "dependency" "gotoDefinition in async" $
-            \dir -> do
-                doc <- openDoc (dir </> "Dependency" <.> "hs") "haskell"
-                _hieFile <- fileDoneIndexing ["Control", "Concurrent", "Async.hie"]
-                defs <- getDefinitions doc (Position 5 20)
-                let expRange = Range (Position 430 22) (Position 430 36)
-                case defs of
-                    InL (Definition (InR [Location fp actualRange])) ->
-                        liftIO $ do
-                            let locationDirectories :: [String]
-                                locationDirectories =
-                                    maybe [] splitDirectories $
-                                        uriToFilePath fp
-                            assertBool "AsyncCancelled found in a module that is not Control.Concurrent Async"
-                                $ ["Control", "Concurrent", "Async.hs"]
-                                    `isSuffixOf` locationDirectories
-                            actualRange @?= expRange
-                    wrongLocation ->
-                        liftIO $
-                            assertFailure $ "Wrong location for AsyncCancelled: "
-                                ++ show wrongLocation
-        fileDoneIndexing :: [String] -> Session FilePath
-        fileDoneIndexing fpSuffix =
-            skipManyTill anyMessage indexedFile
-            where
-                indexedFile :: Session FilePath
-                indexedFile = do
-                    NotMess TNotificationMessage{_params} <-
-                        customNotification (Proxy @"ghcide/reference/ready")
-                    case A.fromJSON _params of
-                        A.Success fp -> do
-                            let fpDirs :: [String]
-                                fpDirs = splitDirectories fp
-                            bool Applicative.empty (pure fp) $
-                                fpSuffix `isSuffixOf` fpDirs
-                        other -> error $ "Failed to parse ghcide/reference/ready file: " <> show other
+        indexedFile :: Session FilePath
+        indexedFile = do
+            NotMess TNotificationMessage{_params} <-
+                customNotification (Proxy @"ghcide/reference/ready")
+            case A.fromJSON _params of
+                A.Success fp -> do
+                    let fpDirs :: [String]
+                        fpDirs = splitDirectories fp
+                    bool Applicative.empty (pure fp) $
+                        fpSuffix `isSuffixOf` fpDirs
+                other -> error $ "Failed to parse ghcide/reference/ready file: " <> show other
+
+dependencyTest :: TestTree
+dependencyTest = testSessionWithExtraFiles "dependency" "gotoDefinition in async" $
+    \dir -> do
+        doc <- openDoc (dir </> "Dependency" <.> "hs") "haskell"
+        _hieFile <- fileDoneIndexing ["Control", "Concurrent", "Async.hie"]
+        defs <- getDefinitions doc (Position 5 20)
+        let expRange = Range (Position 430 22) (Position 430 36)
+        case defs of
+            InL (Definition (InR [Location fp actualRange])) ->
+                liftIO $ do
+                    let locationDirectories :: [String]
+                        locationDirectories =
+                            maybe [] splitDirectories $
+                                uriToFilePath fp
+                    assertBool "AsyncCancelled found in a module that is not Control.Concurrent Async"
+                        $ ["Control", "Concurrent", "Async.hs"]
+                            `isSuffixOf` locationDirectories
+                    actualRange @?= expRange
+            wrongLocation ->
+                liftIO $
+                    assertFailure $ "Wrong location for AsyncCancelled: "
+                        ++ show wrongLocation
