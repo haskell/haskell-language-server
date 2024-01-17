78dacc5a633fa0a3cf843e7c365cd2535d3df191
Jan Hrcek <2716069+jhrcek@users.noreply.github.com>
2024-01-17 10:34:10 +0100

    Fix -Wall and -Wunused-packages in hls-class-plugin (#3972)
    * Fix -Wunused-packages in class plugin
    
    * -Wall and hlint fixes
    
    * Fix type annotation for ghc 9.6+

--- plugins/hls-class-plugin/src/Ide/Plugin/Class/ExactPrint.hs
+++ plugins/hls-class-plugin/src/Ide/Plugin/Class/ExactPrint.hs
@@ -13,10 +13,11 @@ import           Language.Haskell.GHC.ExactPrint
 import           Language.Haskell.GHC.ExactPrint.Parsers
 
 import           Data.Either.Extra                       (eitherToMaybe)
+import           Data.Functor.Identity                   (Identity)
 import           GHC.Parser.Annotation
+import           Language.LSP.Protocol.Types             (Range)
 
 makeEditText :: Monad m => ParsedModule -> DynFlags -> AddMinimalMethodsParams -> MaybeT m (T.Text, T.Text)
--- addMethodDecls :: ParsedSource -> [(LHsDecl GhcPs, LHsDecl GhcPs)] -> Range -> Bool -> TransformT Identity (Located HsModule)
 makeEditText pm df AddMinimalMethodsParams{..} = do
     mDecls <- MaybeT . pure $ traverse (makeMethodDecl df) methodGroup
     let ps = makeDeltaAst $ pm_parsed_source pm
@@ -31,14 +32,21 @@ makeMethodDecl df (mName, sig) = do
     sig' <- eitherToMaybe $ parseDecl df (T.unpack sig) $ T.unpack sig
     pure (name, sig')
 
+#if MIN_VERSION_ghc(9,5,0)
+addMethodDecls :: ParsedSource -> [(LHsDecl GhcPs, LHsDecl GhcPs)] -> Range -> Bool -> TransformT Identity (Located (HsModule GhcPs))
+#else
+addMethodDecls :: ParsedSource -> [(LHsDecl GhcPs, LHsDecl GhcPs)] -> Range -> Bool -> TransformT Identity (Located HsModule)
+#endif
 addMethodDecls ps mDecls range withSig
     | withSig = go (concatMap (\(decl, sig) -> [sig, decl]) mDecls)
     | otherwise = go (map fst mDecls)
     where
     go inserting = do
         allDecls <- hsDecls ps
-        let (before, ((L l inst): after)) = break (inRange range . getLoc) allDecls
-        replaceDecls ps (before ++ (L l (addWhere inst)): (map newLine inserting ++ after))
+        case break (inRange range . getLoc) allDecls of
+            (before, L l inst : after) -> replaceDecls ps (before ++ L l (addWhere inst):(map newLine inserting ++ after))
+            (before, []) -> replaceDecls ps before
+
     -- Add `where` keyword for `instance X where` if `where` is missing.
     --
     -- The `where` in ghc-9.2 is now stored in the instance declaration
@@ -48,15 +56,17 @@ addMethodDecls ps mDecls range withSig
     --
     -- See the link for the original definition:
     --   https://hackage.haskell.org/package/ghc-9.2.1/docs/Language-Haskell-Syntax-Extension.html#t:XCClsInstDecl
-    addWhere (InstD xInstD (ClsInstD ext decl@ClsInstDecl{..})) =
-        let (EpAnn entry anns comments, key) = cid_ext
-        in InstD xInstD (ClsInstD ext decl {
-        cid_ext = (EpAnn
-                    entry
-                    (AddEpAnn AnnWhere (EpaDelta (SameLine 1) []) : anns)
-                    comments
-                    , key)
-        })
+    addWhere instd@(InstD xInstD (ClsInstD ext decl@ClsInstDecl{..})) =
+        case cid_ext of
+            (EpAnn entry anns comments, key) ->
+                    InstD xInstD (ClsInstD ext decl {
+                    cid_ext = (EpAnn
+                                entry
+                                (AddEpAnn AnnWhere (EpaDelta (SameLine 1) []) : anns)
+                                comments
+                                , key)
+                    })
+            _ -> instd
     addWhere decl = decl
 
     newLine (L l e) =
