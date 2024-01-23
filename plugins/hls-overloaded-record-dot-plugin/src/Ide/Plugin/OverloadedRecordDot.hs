e34ac21ba1446b9b195ad1f3a30849742f89115c
Jan Hrček <honza.hrk@gmail.com>
2024-01-23 12:47:22 +0100

    Fix -Wall and -Wunused-packages in module name and overloaded record dot plugins

--- plugins/hls-overloaded-record-dot-plugin/src/Ide/Plugin/OverloadedRecordDot.hs
+++ plugins/hls-overloaded-record-dot-plugin/src/Ide/Plugin/OverloadedRecordDot.hs
@@ -36,11 +36,6 @@ import           Development.IDE.Core.RuleTypes       (TcModuleResult (..),
 import           Development.IDE.Core.Shake           (define, useWithStale)
 import qualified Development.IDE.Core.Shake           as Shake
 
-#if __GLASGOW_HASKELL__ >= 903
-import           Development.IDE.GHC.Compat           (HsExpr (HsRecSel))
-#else
-import           Development.IDE.GHC.Compat           (HsExpr (HsRecFld))
-#endif
 
 import           Control.DeepSeq                      (rwhnf)
 import           Development.IDE.Core.PluginUtils
@@ -49,7 +44,13 @@ import           Development.IDE.Core.PositionMapping (PositionMapping,
 import           Development.IDE.GHC.Compat           (Extension (OverloadedRecordDot),
                                                        GhcPass,
                                                        HsExpansion (HsExpanded),
-                                                       HsExpr (HsApp, HsVar, OpApp, XExpr),
+                                                       HsExpr (HsApp, HsVar, OpApp, XExpr,
+#if __GLASGOW_HASKELL__ >= 903
+                                                             HsRecSel
+#else
+                                                             HsRecFld
+#endif
+                                                       ),
                                                        LHsExpr, Outputable,
                                                        Pass (..), appPrec,
                                                        dollarName, getLoc,
