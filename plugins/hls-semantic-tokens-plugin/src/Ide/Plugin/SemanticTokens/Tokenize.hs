fbcce3f6d4b547ffc4e92ba73026e9d94b505edf
Patrick <fwy996602672@gmail.com>
2024-02-06 01:22:06 +0800

    cleanup

--- plugins/hls-semantic-tokens-plugin/src/Ide/Plugin/SemanticTokens/Tokenize.hs
+++ plugins/hls-semantic-tokens-plugin/src/Ide/Plugin/SemanticTokens/Tokenize.hs
@@ -63,9 +63,6 @@ mkPTokenState vf =
       cursor = Char.Position 0 0,
       columnsInUtf16 = 0
     }
--- instance Monoid (DList a) where
---   mempty = DL.empty
---   mappend = (DL.++)
 
 -- lift a Tokenizer Maybe a to Tokenizer m a,
 -- if the Maybe is Nothing, do nothing, recover the state
