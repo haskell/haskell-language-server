cb4a527700251b6b9d6c5cb853d063a5ae7099f2
Patrick <fwy996602672@gmail.com>
2024-02-22 02:20:57 +0800

    through error on `LogTypecheckedFOI`

--- ghcide/src/Development/IDE/Core/Rules.hs
+++ ghcide/src/Development/IDE/Core/Rules.hs
@@ -664,7 +664,7 @@ typeCheckRule recorder = define (cmapWithPrio LogShake recorder) $ \TypeCheck fi
     -- Keeping typechecked modules in memory for other files is
     -- very expensive.
     when (foi == NotFOI) $
-      logWith recorder Logger.Warning $ LogTypecheckedFOI file
+      error $ show $ pretty $ LogTypecheckedFOI file
     typeCheckRuleDefinition hsc pm
 
 knownFilesRule :: Recorder (WithPriority Log) -> Rules ()
