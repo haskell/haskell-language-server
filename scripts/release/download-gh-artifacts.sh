712bf076293b7bed51967fc81d95d0494d651bd1
Fendor <power.walross@gmail.com>
2024-02-27 09:10:16 +0100

    Update release docs, and add missing ChangeLog entry

--- scripts/release/download-gh-artifacts.sh
+++ scripts/release/download-gh-artifacts.sh
@@ -22,12 +22,21 @@ cd "gh-release-artifacts/haskell-language-server-${RELEASE}"
 # github
 gh release download "$RELEASE"
 
+## We can't do cirrus releases any more, as we build HLS releases with ghcup vanilla binaries.
+## Vanilla means "upstream", aka GHC HQ, and GHC HQ does not provide bindists for FreeBSD.
+## Until we start using ghcup's mainstream distribution channel, we can't even begin to build
+## binaries for FreeBSD. We keep this here for the next generation or when the situation changes.
+##
+## We don't use ghcup's mainstream distribution channel, as we only provide vanilla binaries
+## as requested by the ghcup distribution channel team.
 # cirrus
-curl --fail -L -o "haskell-language-server-${RELEASE}-x86_64-freebsd.tar.xz" \
-	"https://api.cirrus-ci.com/v1/artifact/github/haskell/haskell-language-server/bindist/bindist/out/haskell-language-server-${RELEASE}-x86_64-freebsd.tar.xz?branch=${RELEASE}"
+# curl --fail -L -o "haskell-language-server-${RELEASE}-x86_64-freebsd.tar.xz" \
+# 	"https://api.cirrus-ci.com/v1/artifact/github/haskell/haskell-language-server/bindist/bindist/out/haskell-language-server-${RELEASE}-x86_64-freebsd.tar.xz?branch=${RELEASE}"
 
 
 sha256sum haskell-language-server-* > SHA256SUMS
 gpg --detach-sign -u "${SIGNER}" SHA256SUMS
 
-gh release upload "$RELEASE" "haskell-language-server-${RELEASE}-x86_64-freebsd.tar.xz" SHA256SUMS SHA256SUMS.sig
+## see comment above
+# gh release upload "$RELEASE" "haskell-language-server-${RELEASE}-x86_64-freebsd.tar.xz" SHA256SUMS SHA256SUMS.sig
+gh release upload "$RELEASE" SHA256SUMS SHA256SUMS.sig
