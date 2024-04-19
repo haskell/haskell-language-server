84d63838cc1c560cbacef5c873affa94e2f60aee
Patrick <fwy996602672@gmail.com>
2024-04-19 22:58:34 +0800

    migrate initializeResponseTests

--- ghcide/test/exe/Config.hs
+++ ghcide/test/exe/Config.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE PatternSynonyms #-}
+
 module Config where
+
 import           Ide.Types           (defaultPluginDescriptor)
 import           System.FilePath     ((</>))
-import           Test.Hls            (PluginTestDescriptor,
-                                      mkPluginTestDescriptor)
+import           Test.Hls
 import qualified Test.Hls.FileSystem as FS
 
 testDataDir :: FilePath
@@ -13,4 +15,10 @@ mkIdeTestFs = FS.mkVirtualFileTree testDataDir
 
 -- * A dummy plugin for testing ghcIde
 dummyPlugin :: PluginTestDescriptor ()
-dummyPlugin = mkPluginTestDescriptor (\_ pid ->defaultPluginDescriptor pid "dummyTestPlugin") "core"
+dummyPlugin = mkPluginTestDescriptor (\_ pid -> defaultPluginDescriptor pid "dummyTestPlugin") "core"
+
+runWithDummyPlugin :: FS.VirtualFileTree -> Session a -> IO a
+runWithDummyPlugin = runSessionWithServerInTmpDir def dummyPlugin
+
+pattern R :: UInt -> UInt -> UInt -> UInt -> Range
+pattern R x y x' y' = Range (Position x y) (Position x' y')
