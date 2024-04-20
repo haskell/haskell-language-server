9888d2b0c1581d098664a5ddd24dac7e340a39e5
Jinser Kafka <aimer@purejs.icu>
2024-04-20 08:57:57 +0800

    hls-inlay-hints-plugin: Add configuration support

--- plugins/hls-inlay-hints-plugin/src/Ide/Plugin/InlayHints.hs
+++ plugins/hls-inlay-hints-plugin/src/Ide/Plugin/InlayHints.hs
@@ -3,26 +3,33 @@
 
 module Ide.Plugin.InlayHints(descriptor) where
 
-import           Data.Foldable                      (sequenceA_, traverse_)
-import           Development.IDE                    (IdeState)
+import           Control.Monad.Cont                 (MonadIO (liftIO))
+import           Data.Foldable                      (traverse_)
+import           Development.IDE                    (IdeState, runAction)
 import           Development.IDE.Core.PluginUtils   (runActionE)
 import           Ide.Logger                         (Recorder, WithPriority)
 import           Ide.Plugin.Error                   (getNormalizedFilePathE)
+import           Ide.Plugin.InlayHints.Config       (InlayHintsConfig (..),
+                                                     getInlayHintsConfig,
+                                                     properties)
 import           Ide.Plugin.InlayHints.Fixity       (fixityInlayHints,
                                                      fixityRule)
 import           Ide.Plugin.InlayHints.Hole         (holeInlayHints, holeRule)
 import           Ide.Plugin.InlayHints.LocalBinding (localBindingInlayHints,
                                                      localBindingRule)
 import           Ide.Plugin.InlayHints.Types        (InlayHintLog)
-import           Ide.Types                          (PluginDescriptor (pluginHandlers, pluginRules),
+import           Ide.Types                          (ConfigDescriptor (configCustomConfig),
+                                                     PluginDescriptor (pluginConfigDescriptor, pluginHandlers, pluginRules),
                                                      PluginId,
+                                                     defaultConfigDescriptor,
                                                      defaultPluginDescriptor,
+                                                     mkCustomConfig,
                                                      mkPluginHandler)
 import           Language.LSP.Protocol.Message      (SMethod (SMethod_TextDocumentInlayHint))
 import           Language.LSP.Protocol.Types        (InlayHintParams (InlayHintParams),
                                                      Null (Null),
                                                      TextDocumentIdentifier (TextDocumentIdentifier),
-                                                     type (|?) (InR))
+                                                     type (|?) (InL, InR))
 
 descriptor :: Recorder (WithPriority InlayHintLog) -> PluginId -> PluginDescriptor IdeState
 descriptor recorder pluginId = (defaultPluginDescriptor pluginId "Provides Info in Inlay Hints")
@@ -36,10 +43,17 @@ descriptor recorder pluginId = (defaultPluginDescriptor pluginId "Provides Info 
         $ \state _pid (InlayHintParams _ (TextDocumentIdentifier uri) _) -> do
             nfp <- getNormalizedFilePathE uri
             runActionE "InlayHints" state $ do
-                fmap (foldr (<>) (InR Null)) $ traverse ($ nfp)
-                    [
-                    fixityInlayHints
-                    , holeInlayHints
-                    , localBindingInlayHints
+                inlayHintsCfg <- liftIO $ runAction "inlay hints: config" state $ getInlayHintsConfig pluginId
+                let optional p x = if any ($ inlayHintsCfg) [p, enableAll]
+                                   then x
+                                   else const $ pure (InL [])
+
+                fmap (foldr (<>) (InR Null)) $ traverse (($ nfp) . uncurry optional) [
+                    (enableFixity, fixityInlayHints)
+                    , (enableHole, holeInlayHints)
+                    , (enableLocalBinding, localBindingInlayHints)
                     ]
+    , pluginConfigDescriptor = defaultConfigDescriptor
+      { configCustomConfig = mkCustomConfig properties
+      }
     }
