863d0cdca6dbf04505873b3c10f9dbd134a0d1e9
soulomoon <fwy996602672@gmail.com>
2024-05-05 21:24:50 +0800

    Add performace diff benchmarks (#4203)
    * add performance diff `resultDiff.csv` showing the performance different between two version
    
    * add resultDiff CI
    
    ---------
    
    Co-authored-by: Michael Peyton Jones <me@michaelpj.com>
    Co-authored-by: Jan Hrcek <2716069+jhrcek@users.noreply.github.com>

--- bench/Main.hs
+++ bench/Main.hs
@@ -163,6 +163,7 @@ createBuildSystem config = do
 
   buildRules build hlsBuildRules
   benchRules build (MkBenchRules (askOracle $ GetSamples ()) benchHls warmupHls "haskell-language-server" (parallelism configStatic))
+  addGetParentOracle
   csvRules build
   svgRules build
   heapProfileRules build
