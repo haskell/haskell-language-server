24ec73fc6d67c75996e95d589f05e7ff54da0a22
soulomoon <fwy996602672@gmail.com>
2024-05-09 21:30:45 +0800

    Update Session.hs
    Co-authored-by: Jan Hrcek <2716069+jhrcek@users.noreply.github.com>

--- ghcide/session-loader/Development/IDE/Session.hs
+++ ghcide/session-loader/Development/IDE/Session.hs
@@ -615,10 +615,10 @@ loadSessionWithOptions recorder SessionLoadingOptions{..} dir = do
           void $ modifyVar' filesMap $ flip HM.union (HM.fromList (map ((,hieYaml) . fst) $ concatMap toFlagsMap all_targets))
           -- The VFS doesn't change on cradle edits, re-use the old one.
           -- Invalidate all the existing GhcSession build nodes by restarting the Shake session
-          key2 <- invalidateShakeCache
+          keys2 <- invalidateShakeCache
           restartShakeSession VFSUnmodified "new component" [] $ do
-            key1 <- extendKnownTargets all_targets
-            return [key1, key2]
+            keys1 <- extendKnownTargets all_targets
+            return [keys1, keys2]
 
           -- Typecheck all files in the project on startup
           checkProject <- getCheckProject
