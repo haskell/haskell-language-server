d1775e636989a09025f2aee2b4a60b0711af3376
Patrick <fwy996602672@gmail.com>
2024-05-27 14:44:38 +0800

    add doc

--- ghcide/src/Development/IDE/Core/Thread.hs
+++ ghcide/src/Development/IDE/Core/Thread.hs
@@ -4,15 +4,32 @@ import           Control.Concurrent.STM
 import           Control.Monad            (forever)
 import           Control.Monad.Cont       (ContT (ContT))
 
+-- Note [Serializing runs]
+-- ~~~~~~~~~~~~~~~~~~~~~~
+-- In a lof cases we want to have a separate thread that will serialize the runs of the actions.
+-- Like the db writes, session loading in session loader, shake session restarts.
+--
+-- Originally we used various ways to implement this, but it was hard to maintain and error prone.
+-- Moreover, we can not stop these threads uniformly when we are shutting down the server.
+--
+-- `Development.IDE.Core.Thread` module provides a declarative api to implement this easily.
+-- In `ThreadRun` data type:
+-- * `tRunWithResource`: is used to create the resources needed to perform the long running action.
+-- * `tWorker`: is the action we want to run in separate thread serially.
+--
+-- runInThread will create a worker thread to run along with the main thread.
+-- runInThread provides `resource` created by `tRunWithResource` and a `TQueue` to send the actions to run.
+-- The worker thread will serialize the runs of the actions from the TQueue.
 
-data ThreadRun input threadResource resource arg = ThreadRun {
+
+data ThreadRun input workerResource resource arg = ThreadRun {
         tRunWithResource ::
             input -- ^ input of running
-            -> (threadResource -> resource -> IO ()) -- ^ the long running action
+            -> (workerResource -> resource -> IO ()) -- ^ the long running action need to run with resource
             -> IO (),
         tWorker -- ^ A single action we want to run in separate thread serially
             :: input -- ^ input of running
-            -> threadResource -- ^ writer resource
+            -> workerResource -- ^ writer resource
             -> arg -- ^ argument to run
             -> IO ()
 }
@@ -21,7 +38,7 @@ data ThreadRun input threadResource resource arg = ThreadRun {
 -- Run a long running action with a additional running thread
 -- The additional thread will serialize runs of the actions from the TQueue.
 -- Return ContT to run the action
-runInThread :: ThreadRun input threadResource resource arg -> input -> ContT () IO (resource, TQueue arg)
+runInThread :: ThreadRun input workerResource resource arg -> input -> ContT () IO (resource, TQueue arg)
 runInThread ThreadRun{..} ip = ContT $ \f -> do
     tRunWithResource ip $ \w r -> do
         q <- newTQueueIO
