632f5a036fe54ca6034538358d5e69c08ae8c81f
George Gerasev <george30032002@gmail.com>
2024-06-15 00:15:58 +0300

    trivial outline with rule invocation

--- plugins/hls-cabal-plugin/src/Ide/Plugin/Cabal/Outline.hs
+++ plugins/hls-cabal-plugin/src/Ide/Plugin/Cabal/Outline.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE ViewPatterns          #-}
 {-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE RecordWildCards #-}
 
 
 module Ide.Plugin.Cabal.Outline
@@ -14,37 +15,63 @@ module Ide.Plugin.Cabal.Outline
 where
 
 import           Control.Monad.IO.Class
-import           Data.Foldable                  (toList)
-import           Data.Functor
-import           Data.List.NonEmpty             (nonEmpty)
 import           Data.Maybe
 import           Development.IDE.Core.Rules
-import           Development.IDE.Core.Shake
-import           Development.IDE.GHC.Compat
-import           Development.IDE.GHC.Error      (rangeToRealSrcSpan,
-                                                 realSrcSpanToRange)
-import           Development.IDE.Types.Location
-import           Development.IDE.GHC.Util       (printOutputable)
+import Development.IDE.Core.Shake ( IdeState(shakeExtras), runIdeAction, useWithStaleFast )
+import Development.IDE.Types.Location ( toNormalizedFilePath')
 import           Ide.Types
-import           Language.LSP.Protocol.Types             (DocumentSymbol (..),
-                                                 DocumentSymbolParams (DocumentSymbolParams, _textDocument),
-                                                 SymbolKind (..),
-                                                 TextDocumentIdentifier (TextDocumentIdentifier),
-                                                 type (|?) (InL, InR), uriToFilePath, mkRange, SymbolInformation (_deprecated))
-import Language.LSP.Protocol.Message
-    ( Method(Method_TextDocumentDocumentSymbol) )
+import qualified Language.LSP.Protocol.Types as LSP
+import qualified Language.LSP.Protocol.Message           as LSP
+
+import Data.Text.Encoding (decodeASCII)
+
+import           Ide.Plugin.Cabal.Completion.Types           (ParseCabalFields (..))
+import           Ide.Plugin.Cabal.Orphans                    ()
+
+import Distribution.Fields.Field                             (Field (Field), Name (Name))
+import Distribution.Parsec.Position                          (Position (Position))
 
 import qualified Data.Text                      as T
+import           Debug.Trace                    as Debug
 
-moduleOutline :: PluginMethodHandler IdeState Method_TextDocumentDocumentSymbol
-moduleOutline ideState _ DocumentSymbolParams{ _textDocument = TextDocumentIdentifier uri }
-  = liftIO $ case uriToFilePath uri of
-    Just (toNormalizedFilePath' -> fp) -> pure $ InR (InL [DocumentSymbol {_name="hello!"
-                                                                          ,_detail=Nothing
-                                                                          ,_kind=SymbolKind_Module
-                                                                          ,_tags=Nothing
-                                                                          ,_range=mkRange 1 0 1 11
-                                                                          ,_deprecated=Nothing
-                                                                          ,_selectionRange=mkRange 1 0 1 11
-                                                                          ,_children=Nothing}])
-    Nothing -> pure $ InL []
+moduleOutline :: PluginMethodHandler IdeState LSP.Method_TextDocumentDocumentSymbol
+moduleOutline ideState _ LSP.DocumentSymbolParams{ _textDocument = LSP.TextDocumentIdentifier uri }
+  = case LSP.uriToFilePath uri of
+    Just (toNormalizedFilePath' -> fp) -> do
+      mFields <- liftIO $ runIdeAction "cabal-plugin.fields" (shakeExtras ideState) (useWithStaleFast ParseCabalFields fp)
+      let debug = fmap fst mFields
+      -- Debug.traceShowM debug
+      case fmap fst mFields of
+        Just fieldPositions -> pure $ LSP.InR (LSP.InL allSymbols)
+          where
+            allSymbols = mapMaybe documentSymbolForField fieldPositions
+          -- pure $ InR (InL [DocumentSymbol {_name="hello!"
+          --                               ,_detail=Nothing
+          --                               ,_kind=SymbolKind_Module
+          --                               ,_tags=Nothing
+          --                               ,_range=mkRange 1 0 1 11
+          --                               ,_deprecated=Nothing
+          --                               ,_selectionRange=mkRange 1 0 1 11
+          --                               ,_children=Nothing}])
+        Nothing -> pure $ LSP.InL []
+    Nothing -> pure $ LSP.InL []
+
+documentSymbolForField :: Field Position -> Maybe LSP.DocumentSymbol
+documentSymbolForField (Field (Name pos@(Position line char) fieldName) _ )= Just $ LSP.DocumentSymbol { .. } where
+  _detail         = Nothing
+  _deprecated     = Nothing
+  _name           = decodeASCII fieldName
+
+  _kind           = LSP.SymbolKind_Field
+  _range          = LSP.Range (parserToLSPPosition pos) (parserToLSPPosition (Position line char))
+  _selectionRange = LSP.Range (parserToLSPPosition pos) (parserToLSPPosition (Position line char))
+  _children       = Nothing
+  _tags           = Nothing
+
+  -- addNameLength :: UInt -> FieldName -> UInt
+  -- addNameLength char name = toEnum (fromEnum char + length name)
+documentSymbolForField _ = Nothing
+
+
+parserToLSPPosition :: Position -> LSP.Position
+parserToLSPPosition (Position start end) = LSP.Position (toEnum start) (toEnum end)
