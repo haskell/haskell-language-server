012e8090542dd8cd269ab10efd4373b265d7b73a
soulomoon <fwy996602672@gmail.com>
2024-07-01 20:20:41 +0800

    Simplify initPlugins (#4344)
    * simplify initPlugins
    
    * fix cabal
    
    * remove initializePlugins
    
    ---------
    
    Co-authored-by: Michael Peyton Jones <me@michaelpj.com>

--- ghcide/src/Development/IDE/GHC/Compat/Plugins.hs
+++ ghcide/src/Development/IDE/GHC/Compat/Plugins.hs
@@ -7,8 +7,6 @@ module Development.IDE.GHC.Compat.Plugins (
     defaultPlugin,
     PluginWithArgs(..),
     applyPluginsParsedResultAction,
-    initializePlugins,
-    initPlugins,
 
     -- * Static plugins
     StaticPlugin(..),
@@ -20,7 +18,6 @@ module Development.IDE.GHC.Compat.Plugins (
     ) where
 
 import           Development.IDE.GHC.Compat.Core
-import           Development.IDE.GHC.Compat.Env    (hscSetFlags, hsc_dflags)
 import           Development.IDE.GHC.Compat.Parser as Parser
 
 import qualified GHC.Driver.Env                    as Env
@@ -32,7 +29,6 @@ import           GHC.Driver.Plugins                (ParsedResult (..),
                                                     defaultPlugin,
                                                     staticPlugins, withPlugins)
 import qualified GHC.Parser.Lexer                  as Lexer
-import qualified GHC.Runtime.Loader                as Loader
 
 
 getPsMessages :: PState -> PsMessages
@@ -48,16 +44,6 @@ applyPluginsParsedResultAction env ms parsed msgs = do
       applyPluginAction
       (ParsedResult (HsParsedModule parsed []) msgs)
 
-initializePlugins :: HscEnv -> IO HscEnv
-initializePlugins env = do
-    Loader.initializePlugins env
-
--- | Plugins aren't stored in ModSummary anymore since GHC 9.2, but this
--- function still returns it for compatibility with 8.10
-initPlugins :: HscEnv -> ModSummary -> IO (ModSummary, HscEnv)
-initPlugins session modSummary = do
-    session1 <- initializePlugins (hscSetFlags (ms_hspp_opts modSummary) session)
-    return (modSummary{ms_hspp_opts = hsc_dflags session1}, session1)
 
 hsc_static_plugins :: HscEnv -> [StaticPlugin]
 hsc_static_plugins = staticPlugins . Env.hsc_plugins
