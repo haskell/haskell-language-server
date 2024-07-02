9860ea3a71a85de4a355eca5192f69c66bd133d9
George Gerasev <george30032002@gmail.com>
2024-07-02 07:15:43 +0300

    cabal outline test imports change

--- plugins/hls-cabal-plugin/test/Outline.hs
+++ plugins/hls-cabal-plugin/test/Outline.hs
@@ -1,24 +1,24 @@
-{-# LANGUAGE DisambiguateRecordFields #-}
-{-# LANGUAGE OverloadedStrings        #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Outline (
     outlineTests
 ) where
 
-import qualified Language.LSP.Protocol.Types       as LSP
-import           Test.Hls
+import           Language.LSP.Protocol.Types (DocumentSymbol (..),
+                                              Position (..), Range (..))
+import qualified Test.Hls                    as T
 import           Utils
 
-testSymbols :: (HasCallStack) => TestName -> FilePath -> [DocumentSymbol] -> TestTree
+testSymbols :: (T.HasCallStack) => T.TestName -> FilePath -> [DocumentSymbol] -> T.TestTree
 testSymbols testName path expectedSymbols =
   runCabalTestCaseSession testName "outline-cabal" $ do
-    docId <- openDoc path "cabal"
-    symbols <- getDocumentSymbols docId
-    liftIO $ symbols @?= Right expectedSymbols
+    docId <- T.openDoc path "cabal"
+    symbols <- T.getDocumentSymbols docId
+    T.liftIO $ symbols T.@?= Right expectedSymbols
 
-outlineTests :: TestTree
+outlineTests :: T.TestTree
 outlineTests =
-  testGroup
+  T.testGroup
     "Cabal Outline Tests"
     [ testSymbols
         "cabal Field outline test"
@@ -42,43 +42,43 @@ outlineTests =
     fieldDocumentSymbol = (defDocumentSymbol (Range {_start = Position {_line = 0, _character = 0},
                                                                  _end = Position {_line = 0, _character = 8}}))
                                     { _name = "homepage",
-                                      _kind = SymbolKind_Field,
+                                      _kind = T.SymbolKind_Field,
                                       _children = Nothing
                                     }
     fieldLineDocumentSymbol :: DocumentSymbol
     fieldLineDocumentSymbol = (defDocumentSymbol (Range {_start = Position {_line = 0, _character = 0},
                                                                      _end = Position {_line = 0, _character = 13}}))
                                     { _name = "cabal-version",
-                                      _kind = SymbolKind_Field,
+                                      _kind = T.SymbolKind_Field,
                                       _children = Nothing -- the values of fieldLine are removed from the outline
                                     }
     sectionDocumentSymbol :: DocumentSymbol
     sectionDocumentSymbol = (defDocumentSymbol (Range {_start = Position {_line = 0, _character = 2},
                                                                      _end = Position {_line = 0, _character = 15}}))
                                     { _name = "build-depends",
-                                      _kind = SymbolKind_Field,
+                                      _kind = T.SymbolKind_Field,
                                       _children = Nothing -- the values of fieldLine are removed from the outline
                                     }
     sectionArgDocumentSymbol :: DocumentSymbol
     sectionArgDocumentSymbol = (defDocumentSymbol (Range {_start = Position {_line = 0, _character = 2},
                                                                      _end = Position {_line = 0, _character = 19}}))
                                     { _name = "if os ( windows )",
-                                      _kind = SymbolKind_Object,
+                                      _kind = T.SymbolKind_Object,
                                       _children = Just $ [sectionArgChildrenDocumentSymbol]                                    }
     sectionArgChildrenDocumentSymbol :: DocumentSymbol
     sectionArgChildrenDocumentSymbol = (defDocumentSymbol (Range {_start = Position {_line = 1, _character = 4},
                                                                      _end = Position {_line = 1, _character = 17}}))
                                     { _name = "build-depends",
-                                      _kind = SymbolKind_Field,
+                                      _kind = T.SymbolKind_Field,
                                       _children = Nothing
                                     }
 
-defDocumentSymbol :: LSP.Range -> DocumentSymbol
+defDocumentSymbol :: Range -> DocumentSymbol
 defDocumentSymbol range = DocumentSymbol
   { _detail = Nothing
   , _deprecated = Nothing
   , _name = ""
-  , _kind = LSP.SymbolKind_File
+  , _kind = T.SymbolKind_File
   , _range = range
   , _selectionRange = range
   , _children = Nothing
