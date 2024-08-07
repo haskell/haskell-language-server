05b65a43d2a83fe4135c7a4edb702a1406e949d9
George Gerasev <george30032002@gmail.com>
2024-08-07 18:11:46 +0300

    test assertion order fix

--- plugins/hls-cabal-plugin/test/Main.hs
+++ plugins/hls-cabal-plugin/test/Main.hs
@@ -249,4 +249,4 @@ codeActionTests = testGroup "Code Actions"
         mapM_ executeCodeAction selectedCas
         _ <- skipManyTill anyMessage $ getDocumentEdit cabDoc -- Wait for the changes in cabal file
         contents <- documentContents cabDoc
-        liftIO $ assertEqual (T.unpack dependency <> " isn't found in the cabal file") (Text.indices dependency contents) indicesRes
+        liftIO $ assertEqual (T.unpack dependency <> " isn't found in the cabal file") indicesRes (Text.indices dependency contents)
