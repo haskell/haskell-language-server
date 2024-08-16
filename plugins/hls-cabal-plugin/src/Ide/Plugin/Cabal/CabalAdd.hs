78913d3416add3c4ff081dea523a45b406678775
George Gerasev <george30032002@gmail.com>
2024-08-16 20:16:16 +0300

    imports

--- plugins/hls-cabal-plugin/src/Ide/Plugin/Cabal/CabalAdd.hs
+++ plugins/hls-cabal-plugin/src/Ide/Plugin/Cabal/CabalAdd.hs
@@ -42,14 +42,14 @@ import           Distribution.PackageDescription               (GenericPackageDe
                                                                 specVersion)
 import           Distribution.PackageDescription.Configuration (flattenPackageDescription)
 import           Distribution.PackageDescription.Quirks        (patchQuirks)
-import           Distribution.Pretty                           (pretty)
+import qualified Distribution.Pretty                           as Pretty
 import           Distribution.Simple.BuildTarget               (BuildTarget,
                                                                 buildTargetComponentName,
                                                                 readBuildTargets)
 import           Distribution.Simple.Utils                     (safeHead)
 import           Distribution.Verbosity                        (silent,
                                                                 verboseNoStderr)
-import qualified Ide.Logger                                    as Logger
+import           Ide.Logger
 import           Ide.Plugin.Cabal.Completion.Types             (ParseCabalFields (..),
                                                                 ParseCabalFile (..))
 import           Ide.Plugin.Cabal.Orphans                      ()
@@ -91,12 +91,12 @@ data Log
   | LogExecutedCommand
   deriving (Show)
 
-instance Logger.Pretty Log where
+instance Pretty Log where
     pretty = \case
-      LogFoundResponsibleCabalFile fp -> "Located the responsible cabal file at " Logger.<+> Logger.pretty fp
+      LogFoundResponsibleCabalFile fp -> "Located the responsible cabal file at " <+> pretty fp
       LogCalledCabalAddCodeAction -> "The CabalAdd CodeAction is called"
-      LogCalledCabalAddCommand params -> "Called CabalAdd command with:\n" Logger.<+> Logger.pretty params
-      LogCreatedEdit edit -> "Created inplace edit:\n" Logger.<+> Logger.pretty edit
+      LogCalledCabalAddCommand params -> "Called CabalAdd command with:\n" <+> pretty params
+      LogCreatedEdit edit -> "Created inplace edit:\n" <+> pretty edit
       LogExecutedCommand -> "Executed CabalAdd command"
 
 cabalAddCommand :: IsString p => p
@@ -112,13 +112,13 @@ data CabalAddCommandParams =
   deriving (Generic, Show)
   deriving anyclass (FromJSON, ToJSON)
 
-instance Logger.Pretty CabalAddCommandParams where
+instance Pretty CabalAddCommandParams where
   pretty CabalAddCommandParams{..} =
-    "CabalAdd parameters:" Logger.<+> Logger.vcat
-      [ "cabal path:" Logger.<+> Logger.pretty cabalPath
-      , "target:" Logger.<+> Logger.pretty buildTarget
-      , "dependendency:" Logger.<+> Logger.pretty dependency
-      , "version:" Logger.<+> Logger.pretty version
+    "CabalAdd parameters:" <+> vcat
+      [ "cabal path:" <+> pretty cabalPath
+      , "target:" <+> pretty buildTarget
+      , "dependendency:" <+> pretty dependency
+      , "version:" <+> pretty version
       ]
 
 -- | Creates a code action that calls the `cabalAddCommand`,
@@ -133,7 +133,7 @@ instance Logger.Pretty CabalAddCommandParams where
 --   build target, but if there will be a way to get all build targets from a file
 --   it will be possible to support addition to a build target of choice.
 addDependencySuggestCodeAction
-  :: Logger.Recorder (Logger.WithPriority Log)
+  :: Recorder (WithPriority Log)
   -> PluginId
   -> VersionedTextDocumentIdentifier -- ^ Cabal's versioned text identifier
   -> [(T.Text, T.Text)] -- ^ A dependency-version suggestion pairs
@@ -143,7 +143,7 @@ addDependencySuggestCodeAction
   -> IO [CodeAction]
 addDependencySuggestCodeAction recorder plId verTxtDocId suggestions haskellFilePath cabalFilePath gpd = do
     buildTargets <- liftIO $ getBuildTargets gpd cabalFilePath haskellFilePath
-    Logger.logWith recorder Logger.Info LogCalledCabalAddCodeAction
+    logWith recorder Info LogCalledCabalAddCodeAction
     case buildTargets of
       [] -> pure $ mkCodeAction cabalFilePath Nothing <$> suggestions
       targets -> pure $ concat [mkCodeAction cabalFilePath (Just $ buildTargetToStringRepr target) <$>
@@ -152,7 +152,7 @@ addDependencySuggestCodeAction recorder plId verTxtDocId suggestions haskellFile
     -- | Note the use of `pretty` funciton.
     -- It converts the `BuildTarget` to an acceptable string representation.
     -- It will be used in as the input for `cabal-add`'s `executeConfig`.
-    buildTargetToStringRepr target = render $ pretty $ buildTargetComponentName target
+    buildTargetToStringRepr target = render $ Pretty.pretty $ buildTargetComponentName target
 
     -- | Gives the build targets that are used in the `CabalAdd`.
     -- Note the unorthodox usage of `readBuildTargets`:
@@ -199,9 +199,9 @@ hiddenPackageSuggestion maxCompletions diag = take maxCompletions $ getMatch (ms
     getMatch (_, _, _, [dependency, _, cleanVersion]) = [(dependency, cleanVersion)]
     getMatch (_, _, _, _) = error "Impossible pattern matching case"
 
-command :: Logger.Recorder (Logger.WithPriority Log) -> CommandFunction IdeState CabalAddCommandParams
+command :: Recorder (WithPriority Log) -> CommandFunction IdeState CabalAddCommandParams
 command recorder state _ params@(CabalAddCommandParams {cabalPath = path, verTxtDocId = verTxtDocId, buildTarget = target, dependency = dep, version = mbVer}) = do
-  Logger.logWith recorder Logger.Debug $ LogCalledCabalAddCommand params
+  logWith recorder Debug $ LogCalledCabalAddCommand params
   let specifiedDep = case mbVer of
         Nothing  -> dep
         Just ver -> dep <> " ^>=" <> ver
@@ -209,7 +209,7 @@ command recorder state _ params@(CabalAddCommandParams {cabalPath = path, verTxt
   let env = (state, caps, verTxtDocId)
   edit <- getDependencyEdit recorder env path target (fromList [T.unpack specifiedDep])
   void $ lift $ pluginSendRequest SMethod_WorkspaceApplyEdit (ApplyWorkspaceEditParams Nothing edit) (\_ -> pure ())
-  Logger.logWith recorder Logger.Debug LogExecutedCommand
+  logWith recorder Debug LogExecutedCommand
   pure $ InR Null
 
 -- | Constructs prerequisites for the @executeConfig@
@@ -217,7 +217,7 @@ command recorder state _ params@(CabalAddCommandParams {cabalPath = path, verTxt
 --   Given the new contents of the cabal file constructs and returns the @edit@.
 --   Inspired by @main@ in cabal-add,
 --   Distribution.Client.Main
-getDependencyEdit :: MonadIO m => Logger.Recorder (Logger.WithPriority Log) -> (IdeState, ClientCapabilities, VersionedTextDocumentIdentifier) ->
+getDependencyEdit :: MonadIO m => Recorder (WithPriority Log) -> (IdeState, ClientCapabilities, VersionedTextDocumentIdentifier) ->
     FilePath -> Maybe String -> NonEmpty String -> ExceptT PluginError m WorkspaceEdit
 getDependencyEdit recorder env cabalFilePath buildTarget dependency = do
   let (state, caps, verTxtDocId) = env
@@ -260,7 +260,7 @@ getDependencyEdit recorder env cabalFilePath buildTarget dependency = do
     Nothing -> throwE $ PluginInternalError $ T.pack $ "Cannot extend build-depends in " ++ cabalFilePath
     Just newContents  -> do
               let edit = diffText caps (verTxtDocId, T.decodeUtf8 cnfOrigContents) (T.decodeUtf8 newContents) SkipDeletions
-              Logger.logWith recorder Logger.Debug $ LogCreatedEdit edit
+              logWith recorder Debug $ LogCreatedEdit edit
               pure edit
 
 -- | Given a path to a haskell file, returns the closest cabal file.
