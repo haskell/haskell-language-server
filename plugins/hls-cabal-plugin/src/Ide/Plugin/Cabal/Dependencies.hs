d949f651116140f2c048c8ec961a2d3cf9543233
lucalabs-de <gspitzer@leoapp-slg.de>
2024-10-14 16:16:54 +0200

    Revert "formatting"
    VSCode formatting introduced too many changes

--- plugins/hls-cabal-plugin/src/Ide/Plugin/Cabal/Dependencies.hs
+++ plugins/hls-cabal-plugin/src/Ide/Plugin/Cabal/Dependencies.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings     #-}
 
 module Ide.Plugin.Cabal.Dependencies (
     DependencyInstance(..),
@@ -7,18 +7,16 @@ module Ide.Plugin.Cabal.Dependencies (
     planJsonPath,
 ) where
 
-import qualified Distribution.Fields               as Syntax
-import qualified Distribution.Parsec.Position      as Syntax
+import Distribution.Fields qualified as Syntax
+import Distribution.Parsec.Position qualified as Syntax
 
-import qualified Data.Text                         as T
-import qualified Data.Text.Encoding                as Encoding
-import           System.FilePath                   ((<.>), (</>))
+import Data.Text.Encoding qualified as Encoding
+import Data.Text qualified as T
+import System.FilePath ((</>), (<.>))
 
-import           Data.ByteString                   (ByteString)
-import           Ide.Plugin.Cabal.Completion.Types
-import           Text.Regex.TDFA                   (AllMatches (getAllMatches),
-                                                    AllTextMatches (getAllTextMatches),
-                                                    (=~))
+import Text.Regex.TDFA ((=~), AllTextMatches (getAllTextMatches), AllMatches(getAllMatches))
+import Data.ByteString (ByteString)
+import Ide.Plugin.Cabal.Completion.Types
 
 planJsonPath :: FilePath
 planJsonPath = "dist-newstyle" </> "cache" </> "plan" <.> "json" -- hard coded for now
@@ -29,16 +27,16 @@ parseDeps (Syntax.Field (Syntax.Name _ "build-depends") fls) = concatMap mkPosDe
 parseDeps (Syntax.Section _ _ fls) = concatMap parseDeps fls
 parseDeps _ = []
 
--- | Matches valid Cabal dependency names
+-- | Matches valid Cabal dependency names 
 packageRegex :: T.Text
 packageRegex = "[a-zA-Z0-9_-]+" -- not sure if this is correct
 
 -- | Parses a single FieldLine of Cabal dependencies. Returns a list since a single line may
 -- contain multiple dependencies.
 mkPosDeps :: Syntax.FieldLine Syntax.Position -> [Positioned PkgName]
-mkPosDeps (Syntax.FieldLine pos dep) = zipWith
-        (\n (o, _) -> Positioned (Syntax.Position (Syntax.positionRow pos) (Syntax.positionCol pos + o + 1)) n)
-        (getPackageNames dep)
+mkPosDeps (Syntax.FieldLine pos dep) = zipWith 
+        (\n (o, _) -> Positioned (Syntax.Position (Syntax.positionRow pos) (Syntax.positionCol pos + o + 1)) n) 
+        (getPackageNames dep) 
         (getPackageNameOffsets dep)
     where
         getPackageNames :: ByteString -> [T.Text]
