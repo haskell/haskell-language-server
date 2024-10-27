755f0ddca9df8bbe56f755c60a124dd5729cc0ef
Elodie Lander <lander89@gmail.com>
2024-10-27 05:46:08 -0500

    Attempt going back to monomorphic Q

--- ghcide/src/Development/IDE/Core/InputPath.hs
+++ ghcide/src/Development/IDE/Core/InputPath.hs
@@ -1,24 +1,59 @@
+{-# LANGUAGE DerivingStrategies #-}
+
 module Development.IDE.Core.InputPath where
 
-import Development.IDE.Graph.Internal.RuleInput (Input(ProjectHaskellFile, DependencyHaskellFile))
-import Language.LSP.Protocol.Types (NormalizedFilePath, fromNormalizedFilePath)
+import Control.DeepSeq
+import Data.Hashable
 import Data.List (isInfixOf)
+import Data.Typeable
+import Development.IDE.Types.Location (emptyFilePath)
+import Language.LSP.Protocol.Types (NormalizedFilePath, fromNormalizedFilePath)
 import System.FilePath (splitDirectories)
 
-newtype InputPath (i :: Input) =
-    InputPath { unInputPath :: NormalizedFilePath } deriving Eq
+data InputClass
+    = ProjectHaskellFiles
+    | AllHaskellFiles
 
-data PartitionedInputs = PartitionedInputs
-  { projectFiles :: [InputPath ProjectHaskellFile]
-  , dependencyFiles :: [InputPath DependencyHaskellFile]
-  }
+newtype InputPath (i :: InputClass) =
+    InputPath { unInputPath :: NormalizedFilePath }
+    deriving newtype (Eq, Hashable, NFData, Typeable, Show)
 
-partitionInputs :: [NormalizedFilePath] -> PartitionedInputs
-partitionInputs = foldr classifyInputPath emptyInputs
+class HasNormalizedFilePath input where
+    getNormalizedFilePath :: input -> NormalizedFilePath
+
+instance HasNormalizedFilePath (InputPath ProjectHaskellFiles) where
+    getNormalizedFilePath (InputPath nfp) = nfp
+
+instance HasNormalizedFilePath (InputPath AllHaskellFiles) where
+    getNormalizedFilePath (InputPath nfp) = nfp
+
+instance HasNormalizedFilePath () where
+    getNormalizedFilePath _ = emptyFilePath
+
+-- All Haskell files are valid, and we assume all
+-- files are Haskell files (for now) so there is
+-- no need to filter out any FilePaths.
+classifyAllHaskellInputs :: [NormalizedFilePath] -> [InputPath ProjectHaskellFiles]
+classifyAllHaskellInputs = map InputPath
+
+-- Dependency files should not be considered
+-- ProjectHaskellFiles, so we filter them out
+-- before classifying all other files as
+-- ProjectHaskellFiles.
+classifyProjectHaskellInputs :: [NormalizedFilePath] -> [InputPath ProjectHaskellFiles]
+classifyProjectHaskellInputs = foldr classifyInputPath []
   where
-    classifyInputPath :: NormalizedFilePath -> PartitionedInputs -> PartitionedInputs
-    classifyInputPath nfp inputs@(PartitionedInputs projectInputs dependencyInputs) =
-        case [".hls", "dependencies"] `isInfixOf` splitDirectories (fromNormalizedFilePath nfp) of
-            True -> inputs{ projectFiles = InputPath nfp : projectInputs }
-            False -> inputs{ dependencyFiles = InputPath nfp : dependencyInputs }
-    emptyInputs = PartitionedInputs [] []
+    classifyInputPath :: NormalizedFilePath -> [InputPath ProjectHaskellFiles] -> [InputPath ProjectHaskellFiles]
+    classifyInputPath nfp projectInputs =
+        case dependencyDirectory `isInfixOf` rawInput of
+            -- The input is a dependency, so don't include
+            -- it in the project inputs.
+            True -> projectInputs
+            -- The input is not a depencency, so include it
+            -- in the project inputs
+            False -> InputPath nfp : projectInputs
+        where
+            dependencyDirectory :: [FilePath]
+            dependencyDirectory = [".hls", "dependencies"]
+            rawInput :: [FilePath]
+            rawInput = splitDirectories (fromNormalizedFilePath nfp)
