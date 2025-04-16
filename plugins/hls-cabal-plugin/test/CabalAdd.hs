242f2afd9da815568bdae3d790b71a6787c1bc4f
VeryMilkyJoe <jana.chadt@nets.at>
2025-04-16 14:21:40 +0200

    Add Code Action for adding a module to the cabal file
    For diagnostics complaining about the current module being unknown,
    we now offer code actions to add the module to any possible field in
    the responsible cabal file.
    
    Additionally, refactor the cabal-plugin into smaller modules and
    refactor the add-package feature to have some shared functions to be
    used for both add-package and add-module.

--- plugins/hls-cabal-plugin/test/CabalAdd.hs
+++ plugins/hls-cabal-plugin/test/CabalAdd.hs
@@ -1,32 +1,102 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module CabalAdd (
-  cabalAddTests,
+    cabalAddDependencyTests,
+    cabalAddModuleTests,
 ) where
 
-import           Control.Lens                ((^.))
-import           Control.Lens.Fold           ((^?))
-import qualified Data.Maybe                  as Maybe
-import qualified Data.Text                   as T
-import qualified Data.Text.Internal.Search   as T
-import           Distribution.Utils.Generic  (safeHead)
-import           Ide.Plugin.Cabal.CabalAdd   (hiddenPackageSuggestion)
-import qualified Language.LSP.Protocol.Lens  as L
-import           Language.LSP.Protocol.Types (Diagnostic (..), mkRange)
+import           Control.Lens                                  ((^.))
+import           Control.Lens.Fold                             ((^?))
+import qualified Data.Maybe                                    as Maybe
+import qualified Data.Text                                     as T
+import qualified Data.Text.Encoding                            as T
+import qualified Data.Text.Internal.Search                     as T
+import           Distribution.ModuleName                       (fromString)
+import           Distribution.PackageDescription
+import           Distribution.PackageDescription.Configuration (flattenPackageDescription)
+import qualified Distribution.Pretty                           as Pretty
+import           Distribution.Types.Component
+import           Distribution.Utils.Generic                    (safeHead)
+import           Ide.Plugin.Cabal.CabalAdd.CodeAction          (hiddenPackageSuggestion)
+import           Ide.Plugin.Cabal.Parse                        (parseCabalFileContents)
+import qualified Language.LSP.Protocol.Lens                    as L
+import           Language.LSP.Protocol.Types                   (Diagnostic (..),
+                                                                mkRange)
 import           System.FilePath
-import           Test.Hls                    (Session, TestTree, _R, anyMessage,
-                                              assertEqual, documentContents,
-                                              executeCodeAction,
-                                              getAllCodeActions,
-                                              getDocumentEdit, liftIO, openDoc,
-                                              skipManyTill, testCase, testGroup,
-                                              waitForDiagnosticsFrom, (@?=))
+import           Test.Hls                                      (Session,
+                                                                TestTree, _R,
+                                                                anyMessage,
+                                                                assertBool,
+                                                                assertEqual,
+                                                                assertFailure,
+                                                                documentContents,
+                                                                executeCodeAction,
+                                                                getAllCodeActions,
+                                                                getDocumentEdit,
+                                                                liftIO, openDoc,
+                                                                skipManyTill,
+                                                                testCase,
+                                                                testGroup,
+                                                                waitForDiagnosticsFrom,
+                                                                (@?=))
 import           Utils
 
-cabalAddTests :: TestTree
-cabalAddTests =
+cabalAddModuleTests :: TestTree
+cabalAddModuleTests =
+    testGroup
+        "Add Module"
+        [ runHaskellTestCaseSession "Code Actions - Can add module to benchmark" ("cabal-add-module" </> "library") $ do
+            let compName = CBenchName "test1"
+            pd <- generateAddDependencyTestSession "test.cabal" "Main.hs" compName
+            checkModuleAddedTo pd "Main" $ compName
+        , runHaskellTestCaseSession "Code Actions - Can add module to executable" ("cabal-add-module" </> "library") $ do
+            let compName = CExeName "test"
+            pd <- generateAddDependencyTestSession "test.cabal" "Main.hs" compName
+            checkModuleAddedTo pd "Main" $ compName
+        , runHaskellTestCaseSession "Code Actions - Can add module to test-suite" ("cabal-add-module" </> "library") $ do
+            let compName = CTestName "test2"
+            pd <- generateAddDependencyTestSession "test.cabal" "Main.hs" compName
+            checkModuleAddedTo pd "Main" $ compName
+        , runHaskellTestCaseSession "Code Actions - Can add module to library" ("cabal-add-module" </> "library") $ do
+            let compName = CLibName $ LSubLibName "test3"
+            pd <- generateAddDependencyTestSession "test.cabal" "Main.hs" compName
+            checkModuleAddedTo pd "Main" $ compName
+        , runHaskellTestCaseSession "Code Actions - Can add module to main library" ("cabal-add-module" </> "library") $ do
+            let compName = CLibName LMainLibName
+            pd <- generateAddDependencyTestSession "test.cabal" "Main.hs" compName
+            checkModuleAddedTo pd "Main" $ compName
+        ]
+  where
+    generateAddDependencyTestSession :: FilePath -> FilePath -> ComponentName -> Session PackageDescription
+    generateAddDependencyTestSession cabalFile haskellFile compName = do
+        haskellDoc <- openDoc haskellFile "haskell"
+        cabalDoc <- openDoc cabalFile "cabal"
+        _ <- waitForDiagnosticsFrom haskellDoc
+        cas <- Maybe.mapMaybe (^? _R) <$> getAllCodeActions haskellDoc
+        let selectedCas = filter (\ca -> (T.pack $ "Add to " <> (Pretty.prettyShow compName)) `T.isPrefixOf` (ca ^. L.title)) cas
+        mapM_ executeCodeAction $ selectedCas
+        _ <- skipManyTill anyMessage $ getDocumentEdit cabalDoc -- Wait for the changes in cabal file
+        contents <- documentContents cabalDoc
+        case parseCabalFileContents $ T.encodeUtf8 contents of
+            (_, Right gpd) -> pure $ flattenPackageDescription gpd
+            _ -> liftIO $ assertFailure "could not parse cabal file to gpd"
+
+    checkModuleAddedTo :: PackageDescription -> String -> ComponentName -> Session ()
+    checkModuleAddedTo pd modName compName = do
+        let comp = getComponent pd compName
+            compModules = case comp of
+                CLib lib     -> explicitLibModules lib
+                CFLib fLib   -> foreignLibModules fLib
+                CExe exe     -> exeModules exe
+                CTest test   -> testModules test
+                CBench bench -> benchmarkModules bench
+            testDescription = (modName <> " was added to " <> (showComponentName compName))
+        liftIO $ assertBool testDescription $ (fromString modName) `elem` compModules
+
+cabalAddDependencyTests :: TestTree
+cabalAddDependencyTests =
   testGroup
-    "CabalAdd Tests"
+    "Add Package"
     [ runHaskellTestCaseSession "Code Actions - Can add hidden package to an executable" ("cabal-add-testdata" </> "cabal-add-exe")
         (generateAddDependencyTestSession "cabal-add-exe.cabal" ("src" </> "Main.hs") "split" [253])
     , runHaskellTestCaseSession "Code Actions - Can add hidden package to a library" ("cabal-add-testdata" </> "cabal-add-lib")
@@ -156,7 +226,7 @@ cabalAddTests =
         liftIO $ assertEqual (T.unpack dependency <> " isn't found in the cabal file") indicesRes (T.indices dependency contents)
     testHiddenPackageSuggestions :: String -> [T.Text] -> [(T.Text, T.Text)] -> TestTree
     testHiddenPackageSuggestions testTitle messages suggestions =
-        let diags = map (\msg -> messageToDiagnostic msg ) messages
+        let diags = map (\msg -> messageToDiagnostic msg) messages
             suggestions' = map (safeHead . hiddenPackageSuggestion) diags
             assertions   = zipWith (@?=) suggestions' (map Just suggestions)
             testNames    = map (\(f, s) -> "Check if " ++ T.unpack f ++ (if s == "" then "" else "-") ++ T.unpack s ++ " was parsed correctly") suggestions
@@ -175,9 +245,8 @@ cabalAddTests =
           , _data_ = Nothing
         }
 
-
     generatePackageYAMLTestSession :: FilePath -> Session ()
-    generatePackageYAMLTestSession haskellFile  = do
+    generatePackageYAMLTestSession haskellFile = do
         hsdoc <- openDoc haskellFile "haskell"
         _ <- waitForDiagnosticsFrom hsdoc
         cas <- Maybe.mapMaybe (^? _R) <$> getAllCodeActions hsdoc
