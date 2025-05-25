8a32756e948b56168bd94edc8676b849c0498300
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2025-05-25 17:20:38 +0400

    refactor: Module name to filepath optimisation
    This is related to https://github.com/haskell/haskell-language-server/issues/4598.
    
    This changes the file to module associating logic done during dependency
    graph building.
    
    Before, each time an import of module `Foo.Bar` is found, HLS is testing
    inside all the import path for the existence of a relevant file. It
    means that for `i` import paths and `m` modules to locate, `m * n`
    filesystem operations are done. Note also that this involves a lot of
    complex string concatenation primitive to build the `FilePath`.
    
    A module is tested for each `import` for each of the file of the
    project. We also test for `boot` files, doubling the number of test.
    
    In #4598 we have a project with `1100` modules, in more than 250 import
    paths and we count more than `17000` `import` statments, resulting on
    over 6 millions test for file existences. This project was blocking for
    more than 3 minutes during HLS startup.
    
    This commit changes the way this is computed:
    
    - A new rule `GetModulesPaths` generates a `ModuleToFilenames` map,
      associating module name to `NormalizedFilePath`. This rules builds a
      cache of all the know module -> filepath associations, based on a scan
      of the filesystem extended with the content of the `knownTargets` and
      `VFS` in `extendModuleMapWithKnownTargets`. This rule is cached in
      `ShakeExtras` in `moduleToPathCache`.
    - The lookup phase is not changed much, but instead of testing the
      knownTargets and VFS for all possibly paths, taking into account all
      possible import path, it just does an immediate lookup in the
      `ModuleToFilenames` maps.
    
    The performance improvement is as follows:
    
    - The number of IO operation is dramatically reduced, from multiples
      millions to a few recursive directories listing.
    - A lot of the boilerplate of converting path had be removed.
    - On my project, the graph building time is reduced from a few minutes
      to 3s.
    
    A note about performance
    
    Most users won't see the benefits of these change, but I think they
    apply to everbody:
    
    - We are still doing 1 lookup per `import` per module. But the lookup
      result is not multiples IO, so it should be faster by a large amount.
    - Most project only have 1 (or a few) import paths so won't benefit as
      dramatically as me from this.
    
    Points opens for discussions:
    
    a) In the previous implementation, a lookup in `knownTargets` was
    followed by a lookup in the VFS in case the value was not found in
    `knownTargets`. We observed in https://github.com/haskell/haskell-language-server/issues/4754 that there may be a race condition when populating `knownTargets` in the context of `-boot` files, leading to sometime missing either the `.hs-boot` or the `.hs` file. The previous implementation was still working because of the fallsback on the VFS. In this new implementation, I tried to only target the VFS, however it seems that some file are not part of the VFS, this is unclear for me yet. Extending the module maps (read from the real filesystem) by both the content of the VFS and `knownTargets` give good results.
    b) I'm not confident about the implementation of reexported modules,
    especially in multiple home unit context, but tests are greens.
    c) Cache invalidation. I had not fully understood the logic of the build
    graph and cache invalidation. The current status is that the
    `GetModulesPaths` rule is always rerun for each file, but the content is
    manually cached inside `ShakeExtras` and never invalidated. As a result,
    we cannot observe any change inside the import paths directories, and
    changes on `knownTargets` and the VFS are also ignored.

--- ghcide/src/Development/IDE/Core/Rules.hs
+++ ghcide/src/Development/IDE/Core/Rules.hs
@@ -141,11 +141,11 @@ import           GHC.Iface.Ext.Types                          (HieASTs (..))
 import           GHC.Iface.Ext.Utils                          (generateReferencesMap)
 import qualified GHC.LanguageExtensions                       as LangExt
 #if MIN_VERSION_ghc(9,13,0)
-import           GHC.Types.PkgQual                            (PkgQual (NoPkgQual))
 import           GHC.Types.Basic                              (ImportLevel (..))
-import           GHC.Unit.Types                               (GenWithIsBoot(..))
+import           GHC.Types.PkgQual                            (PkgQual (NoPkgQual))
 import           GHC.Unit.Module.Graph                        (mkModuleEdge)
 import           GHC.Unit.Module.ModNodeKey                   (mnkModuleName)
+import           GHC.Unit.Types                               (GenWithIsBoot (..))
 #endif
 import           HIE.Bios.Ghc.Gap                             (hostIsDynamic)
 import qualified HieDb
@@ -166,11 +166,13 @@ import           Ide.Plugin.Properties                        (HasProperty,
                                                                useProperty,
                                                                usePropertyByPath)
 import           Ide.Types                                    (DynFlagsModifications (dynFlagsModifyGlobal, dynFlagsModifyParser),
-                                                               PluginId, getVirtualFileFromVFS)
+                                                               PluginId,
+                                                               getVirtualFileFromVFS)
 import qualified Language.LSP.Protocol.Lens                   as JL
 import           Language.LSP.Protocol.Message                (SMethod (SMethod_CustomMethod, SMethod_WindowShowMessage))
 import           Language.LSP.Protocol.Types                  (MessageType (MessageType_Info),
-                                                               ShowMessageParams (ShowMessageParams))
+                                                               ShowMessageParams (ShowMessageParams),
+                                                               uriToNormalizedFilePath)
 import           Language.LSP.Server                          (LspT)
 import qualified Language.LSP.Server                          as LSP
 import           Language.LSP.VFS
@@ -179,8 +181,18 @@ import           System.Directory                             (doesFileExist)
 import           System.Info.Extra                            (isWindows)
 
 
+import           Data.Char                                    (isUpper)
+import qualified Data.HashSet                                 as HS
 import qualified Data.IntMap                                  as IM
+import qualified Data.Map.Strict                              as Map
 import           GHC.Fingerprint
+import           System.Directory.Extra                       (listFilesInside)
+import           System.FilePath                              (dropExtension,
+                                                               equalFilePath,
+                                                               normalise,
+                                                               splitDirectories,
+                                                               takeExtension,
+                                                               takeFileName)
 
 data Log
   = LogShake Shake.Log
@@ -319,8 +331,8 @@ getParsedModuleDefinition packageState opt file ms = do
 getLocatedImportsRule :: Recorder (WithPriority Log) -> Rules ()
 getLocatedImportsRule recorder =
     define (cmapWithPrio LogShake recorder) $ \GetLocatedImports file -> do
+
         ModSummaryResult{msrModSummary = ms} <- use_ GetModSummaryWithoutTimestamps file
-        (KnownTargets targets) <- useNoFile_ GetKnownTargets
 #if MIN_VERSION_ghc(9,13,0)
         let imports = [(False, lvl, mbPkgName, modName) | (lvl, mbPkgName, modName) <- ms_textual_imps ms]
                    ++ [(True, NormalLevel, NoPkgQual, noLoc modName) | L _ modName <- ms_srcimps ms]
@@ -332,25 +344,16 @@ getLocatedImportsRule recorder =
         let import_dirs = map (second homeUnitEnv_dflags) $ hugElts $ hsc_HUG env
         let dflags = hsc_dflags env
         opt <- getIdeOptions
-        let getTargetFor modName nfp
-                | Just (TargetFile nfp') <- HM.lookupKey (TargetFile nfp) targets = do
-                    -- reuse the existing NormalizedFilePath in order to maximize sharing
-                    itExists <- getFileExists nfp'
-                    return $ if itExists then Just nfp' else Nothing
-                | Just tt <- HM.lookup (TargetModule modName) targets = do
-                    -- reuse the existing NormalizedFilePath in order to maximize sharing
-                    let nfp' = fromMaybe nfp $ HashSet.lookupElement nfp tt
-                    itExists <- getFileExists nfp'
-                    return $ if itExists then Just nfp' else Nothing
-                | otherwise = do
-                    itExists <- getFileExists nfp
-                    return $ if itExists then Just nfp else Nothing
+
+        moduleMaps <- use_ GetModulesPaths file
+
 #if MIN_VERSION_ghc(9,13,0)
         (diags, imports') <- fmap unzip $ forM imports $ \(isSource, _lvl, mbPkgName, modName) -> do
 #else
         (diags, imports') <- fmap unzip $ forM imports $ \(isSource, (mbPkgName, modName)) -> do
 #endif
-            diagOrImp <- locateModule (hscSetFlags dflags env) import_dirs (optExtensions opt) getTargetFor modName mbPkgName isSource
+
+            diagOrImp <- locateModule moduleMaps (hscSetFlags dflags env) import_dirs (optExtensions opt) modName mbPkgName isSource
             case diagOrImp of
                 Left diags              -> pure (diags, Just (modName, Nothing))
                 Right (FileImport path) -> pure ([], Just (modName, Just path))
@@ -659,6 +662,121 @@ mkLevelEdges ms dep_node_keys = concatMap (\nk -> map (\lvl -> mkModuleEdge lvl 
       _ -> [NormalLevel]
 #endif
 
+getModulesPathsRule :: Recorder (WithPriority Log) -> Rules ()
+getModulesPathsRule recorder = defineEarlyCutoff (cmapWithPrio LogShake recorder) $ Rule $ \GetModulesPaths file -> do
+  env_eq <- use_ GhcSession file
+
+  -- I use a cache stored inside the shake extra logic
+  -- TODO: how does it behaves with respect to multiple session?
+  -- TODO: can we instead use the action cache and ensure it is only called
+  -- once for all files in the same session?
+  ShakeExtras{moduleToPathCache} <- getShakeExtras
+
+  cache <- liftIO (readTVarIO moduleToPathCache)
+  case Map.lookup (envUnique env_eq) cache of
+    Just res -> pure (mempty, ([], Just res))
+    Nothing -> do
+      let env = hscEnv env_eq
+      let import_dirs = map (second homeUnitEnv_dflags) $ hugElts $ hsc_HUG env
+      opt <- getIdeOptions
+      let exts = (optExtensions opt)
+      let acceptedExtensions = concatMap (\x -> ['.':x, '.':x <> "-boot"]) exts
+
+      res <- flip mapM import_dirs $ \(u, dyn) -> do
+        res <- flip mapM (importPaths dyn) $ \dir' -> do
+          let dir = normalise dir'
+          let predicate path = pure (equalFilePath path dir || case takeFileName path of
+               []    -> False
+               (x:_) -> isUpper x)
+          let dir_number_directories = length (splitDirectories dir)
+          let toModule file = mkModuleName (intercalate "." $ drop dir_number_directories (splitDirectories (dropExtension file)))
+
+          -- TODO: we are taking/dropping extension, this could be factorized to save a few cpu cycles ;)
+          -- TODO: do acceptedextensions needs to be a set ? or a vector?
+          -- If the directory is empty, we return an empty list of modules
+          -- using 'catch' instead of an exception which would kill the LSP
+          modules <- (fmap (\path -> (toModule path, toNormalizedFilePath' path)) . filter (\y -> takeExtension y `elem` acceptedExtensions) <$> liftIO (listFilesInside predicate dir))
+                 `catch` (\(_ :: IOException) -> pure [])
+          let isSourceModule (_, path) = "-boot" `isSuffixOf` fromNormalizedFilePath path
+          let (sourceModules, notSourceModules) = partition isSourceModule modules
+          pure $ ModuleToFilenames { moduleMapSource = fmap (u, ) (Map.fromList sourceModules), moduleMap = fmap (u,) (Map.fromList notSourceModules)}
+        pure (mconcat res)
+
+      -- Extend the current module map with all the known targets
+      resExtended <- extendModuleMapWithKnownTargets file (mconcat res)
+
+      liftIO $ atomically $ modifyTVar' moduleToPathCache (Map.insert (envUnique env_eq) resExtended)
+
+      pure (mempty, ([], Just resExtended))
+
+
+-- | Extend the map from module name to filepath (exiting on the drive) with
+-- the list of known targets provided by HLS, as well as the VFS.
+--
+-- These known targets are files which were recently created and not yet saved
+-- to the filesystem.
+--
+-- This code is not really efficient (O(import_dirs * known_targets)), which
+-- can quickly represents millions of tests. It could be improved, but
+-- everything only happen once everytime known_targets is updated (which only
+-- happen when the project start or when a file is added) and the code is not
+-- doing any IO.
+extendModuleMapWithKnownTargets
+    :: NormalizedFilePath -> ModuleToFilenames ->
+       Action ModuleToFilenames
+extendModuleMapWithKnownTargets file moduleMap = do
+
+  KnownTargets knownTargets <- useNoFile_ GetKnownTargets
+  vfs <- fmap _vfsMap . liftIO . readTVarIO . vfsVar =<< getShakeExtras
+
+  env_eq <- use_ GhcSession file
+  let env = hscEnv env_eq
+  let import_dirs = map (second homeUnitEnv_dflags) $ hugElts $ hsc_HUG env
+  opt <- getIdeOptions
+  let exts = (optExtensions opt)
+  let acceptedExtensions = concatMap (\x -> ['.':x, '.':x <> "-boot"]) exts
+
+  let pathsFromTargetsMap = HS.toList $ mconcat (HM.elems knownTargets)
+  let pathsFromVFS = catMaybes $ map uriToNormalizedFilePath (Map.keys vfs)
+
+  -- This is the list of all paths known by HLS right now.
+  -- It includes paths which are from the known targets and path which are stored in the VFS
+  -- TODO: this is really unclear, but it seems that pathsFromTargetsMap are
+  -- subject to race condition with boot files and pathsFromVFS may not contain all the files
+  let paths = pathsFromTargetsMap <> pathsFromVFS
+
+  let (unzip -> (catMaybes -> new_module_map, catMaybes -> new_module_map_source)) = do
+        (u, dyn) <- import_dirs
+        -- TODO: avoid using so much `FilePath` logic AND please please,
+        -- normalize earlier.
+        --
+        -- The normalise here is in order to remove the trailing `.` which
+        -- could break the comparison later.
+        (normalise -> dir') <- importPaths dyn
+        let dirComponents = splitDirectories dir'
+        let dir_number_directories = length dirComponents
+
+        path <- paths
+
+        let pathString = fromNormalizedFilePath path
+        let pathComponents = splitDirectories pathString
+
+        -- Ensure this file is in the directory
+        guard $ dirComponents `isPrefixOf` pathComponents
+
+        -- Ensure that this extension is accepted
+        guard $ takeExtension pathString `elem` acceptedExtensions
+        let modName = mkModuleName (intercalate "." $ drop dir_number_directories (splitDirectories (dropExtension pathString)))
+        let isSourceModule = "-boot" `isSuffixOf` pathString
+        if isSourceModule
+        then
+           pure (Nothing, Just (modName, (u, path)))
+        else
+           pure (Just (modName, (u, path)), Nothing)
+
+  pure $ ModuleToFilenames { moduleMap = Map.fromList new_module_map, moduleMapSource = Map.fromList new_module_map_source } <> moduleMap
+
+
 dependencyInfoForFiles :: [NormalizedFilePath] -> Action (BS.ByteString, DependencyInformation)
 dependencyInfoForFiles fs = do
   (rawDepInfo, bm) <- rawDependencyInformation fs
@@ -749,6 +867,7 @@ loadGhcSession recorder ghcSessionDepsConfig = do
         IdeGhcSession{loadSessionFun} <- useNoFile_ GhcSessionIO
         -- loading is always returning a absolute path now
         (val,deps) <- liftIO $ loadSessionFun $ fromNormalizedFilePath file
+        -- TODO: this is responsible for a LOT of allocations
 
         -- add the deps to the Shake graph
         let addDependency fp = do
@@ -1284,6 +1403,7 @@ mainRule recorder RulesConfig{..} = do
     getModIfaceRule recorder
     getModSummaryRule templateHaskellWarning recorder
     getModuleGraphRule recorder
+    getModulesPathsRule recorder
     getFileHashRule recorder
     knownFilesRule recorder
     getClientSettingsRule recorder
--- ghcide/src/Development/IDE/Import/FindImports.hs
+++ ghcide/src/Development/IDE/Import/FindImports.hs
@@ -1,22 +1,23 @@
 -- Copyright (c) 2019 The DAML Authors. All rights reserved.
 -- SPDX-License-Identifier: Apache-2.0
 
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE CPP            #-}
+{-# LANGUAGE DeriveAnyClass #-}
 
 module Development.IDE.Import.FindImports
   ( locateModule
-  , locateModuleFile
   , Import(..)
   , ArtifactsLocation(..)
   , modSummaryToArtifactsLocation
   , isBootLocation
-  , mkImportDirs
+  , ModuleToFilenames(..)
   ) where
 
 import           Control.DeepSeq
-import           Control.Monad.Extra
 import           Control.Monad.IO.Class
 import           Data.List                         (find, isSuffixOf)
+import           Data.Map.Strict                   (Map)
+import qualified Data.Map.Strict                   as Map
 import           Data.Maybe
 import qualified Data.Set                          as S
 import           Development.IDE.GHC.Compat        as Compat
@@ -24,9 +25,9 @@ import           Development.IDE.GHC.Error         as ErrUtils
 import           Development.IDE.GHC.Orphans       ()
 import           Development.IDE.Types.Diagnostics
 import           Development.IDE.Types.Location
+import           GHC.Generics
 import           GHC.Types.PkgQual
-import           GHC.Unit.State
-import           System.FilePath
+import           GHC.Unit
 
 
 #if MIN_VERSION_ghc(9,11,0)
@@ -66,85 +67,92 @@ modSummaryToArtifactsLocation nfp ms = ArtifactsLocation nfp (ms_location <$> ms
       Just modSum -> isSource (ms_hsc_src modSum)
     mbMod = ms_mod <$> ms
 
+-- | Represents a mapping from module name to the associated unit and filepath
+data ModuleToFilenames = ModuleToFilenames {
+  -- | "normal" files (e.g. @.hs@)
+  moduleMap       :: Map ModuleName (UnitId, NormalizedFilePath),
+  -- | "boot" files (e.g. @.hs-boot@)
+  moduleMapSource :: Map ModuleName (UnitId, NormalizedFilePath)
+}
+  deriving (Show, NFData, Generic)
+
+-- | The mapping of one module to a file takes precedence in "import path"
+-- order
+instance Semigroup ModuleToFilenames where
+  ModuleToFilenames a b <> ModuleToFilenames a' b' = ModuleToFilenames (a <> a') (b <> b')
+
+instance Monoid ModuleToFilenames where
+  mempty = ModuleToFilenames mempty mempty
+
 data LocateResult
   = LocateNotFound
   | LocateFoundReexport UnitId
   | LocateFoundFile UnitId NormalizedFilePath
 
 -- | locate a module in the file system. Where we go from *daml to Haskell
-locateModuleFile :: MonadIO m
-             => [(UnitId, [FilePath], S.Set ModuleName)]
-             -> [String]
-             -> (ModuleName -> NormalizedFilePath -> m (Maybe NormalizedFilePath))
-             -> Bool
-             -> ModuleName
-             -> m LocateResult
-locateModuleFile import_dirss exts targetFor isSource modName = do
-  let candidates import_dirs =
-        [ toNormalizedFilePath' (prefix </> moduleNameSlashes modName <.> maybeBoot ext)
-           | prefix <- import_dirs , ext <- exts]
-  mf <- firstJustM go (concat [map (uid,) (candidates dirs) | (uid, dirs, _) <- import_dirss])
-  case mf of
-    Nothing ->
-      case find (\(_ , _, reexports) -> S.member modName reexports) import_dirss of
-        Just (uid,_,_) -> pure $ LocateFoundReexport uid
-        Nothing        -> pure LocateNotFound
-    Just (uid,file) -> pure $ LocateFoundFile uid file
-  where
-    go (uid, candidate) = fmap ((uid,) <$>) $ targetFor modName candidate
-    maybeBoot ext
-      | isSource = ext ++ "-boot"
-      | otherwise = ext
+locateModuleFile :: ModuleToFilenames -> Bool -> ModuleName -> [(UnitId, S.Set ModuleName)] -> LocateResult
+locateModuleFile ModuleToFilenames{..} isSource modName uid_reexports = do
+  case Map.lookup modName (if isSource then moduleMapSource else moduleMap) of
+                 Nothing ->
+                   case find (\(_ , reexports) -> S.member modName reexports) uid_reexports of
+                           Just (uid,_) -> LocateFoundReexport uid
+                           Nothing      -> LocateNotFound
+                 Just (uid, file) -> LocateFoundFile uid file
 
--- | This function is used to map a package name to a set of import paths.
+-- | This function is used to map a package name to a set of reexports
+-- TODO: the rest of this comment seems outdated:
 -- It only returns Just for unit-ids which are possible to import into the
 -- current module. In particular, it will return Nothing for 'main' components
 -- as they can never be imported into another package.
-mkImportDirs :: HscEnv -> (UnitId, DynFlags) -> Maybe (UnitId, ([FilePath], S.Set ModuleName))
+mkReexports :: HscEnv -> (UnitId, DynFlags) -> Maybe (UnitId, (S.Set ModuleName))
 #if MIN_VERSION_ghc(9,11,0)
-mkImportDirs _env (i, flags) = Just (i, (importPaths flags, S.fromList $ map reexportTo $ reexportedModules flags))
+mkReexports _env (i, flags) = Just (i, (S.fromList $ map reexportTo $ reexportedModules flags))
 #else
-mkImportDirs _env (i, flags) = Just (i, (importPaths flags, reexportedModules flags))
+mkReexports _env (i, flags) = Just (i, (reexportedModules flags))
 #endif
 
 -- | locate a module in either the file system or the package database. Where we go from *daml to
 -- Haskell
 locateModule
     :: MonadIO m
-    => HscEnv
+    => ModuleToFilenames
+    -> HscEnv
     -> [(UnitId, DynFlags)] -- ^ Import directories
     -> [String]                        -- ^ File extensions
-    -> (ModuleName -> NormalizedFilePath -> m (Maybe NormalizedFilePath))  -- ^ does file exist predicate
     -> Located ModuleName              -- ^ Module name
     -> PkgQual                -- ^ Package name
     -> Bool                            -- ^ Is boot module
     -> m (Either [FileDiagnostic] Import)
-locateModule env comp_info exts targetFor modName mbPkgName isSource = do
+locateModule moduleMaps env comp_info exts modName mbPkgName isSource = do
   case mbPkgName of
     -- 'ThisPkg' just means some home module, not the current unit
     ThisPkg uid
-      | Just (dirs, reexports) <- lookup uid import_paths
-          -> lookupLocal uid dirs reexports
+      -- TODO: there are MANY lookup on import_paths, which is a problem considering that it can be large.
+      | Just reexports <- lookup uid import_paths
+          -> lookupLocal uid moduleMaps reexports
       | otherwise -> return $ Left $ notFoundErr env modName $ LookupNotFound []
     -- if a package name is given we only go look for a package
     OtherPkg uid
-      | Just (dirs, reexports) <- lookup uid import_paths
-          -> lookupLocal uid dirs reexports
+      | Just reexports <- lookup uid import_paths
+          -> lookupLocal uid moduleMaps reexports
       | otherwise -> lookupInPackageDB
     NoPkgQual -> do
 
       -- Reexports for current unit have to be empty because they only apply to other units depending on the
       -- current unit. If we set the reexports to be the actual reexports then we risk looping forever trying
       -- to find the module from the perspective of the current unit.
-      mbFile <- locateModuleFile ((homeUnitId_ dflags, importPaths dflags, S.empty) : other_imports) exts targetFor isSource $ unLoc modName
+      let reexports = other_imports
+      let mbFile = locateModuleFile moduleMaps isSource (unLoc modName) reexports
+
       case mbFile of
         LocateNotFound -> lookupInPackageDB
         -- Lookup again with the perspective of the unit reexporting the file
-        LocateFoundReexport uid -> locateModule (hscSetActiveUnitId uid env) comp_info exts targetFor modName noPkgQual isSource
+        LocateFoundReexport uid -> locateModule moduleMaps (hscSetActiveUnitId uid env) comp_info exts modName noPkgQual isSource
         LocateFoundFile uid file -> toModLocation uid file
   where
     dflags = hsc_dflags env
-    import_paths = mapMaybe (mkImportDirs env) comp_info
+    import_paths = mapMaybe (mkReexports env) comp_info
+
     other_imports =
       -- Instead of bringing all the units into scope, only bring into scope the units
       -- this one depends on.
@@ -155,9 +163,9 @@ locateModule env comp_info exts targetFor modName mbPkgName isSource = do
       -- Without multi-component support it is hard to recontruct the dependency environment so
       -- unit a will have both unit b and unit c in scope.
 #if MIN_VERSION_ghc(9,11,0)
-      map (\uid -> let this_df = homeUnitEnv_dflags (ue_findHomeUnitEnv uid ue) in (uid, importPaths this_df, S.fromList $ map reexportTo $ reexportedModules this_df)) hpt_deps
+      map (\uid -> let this_df = homeUnitEnv_dflags (ue_findHomeUnitEnv uid ue) in (uid, S.fromList $ map reexportTo $ reexportedModules this_df)) hpt_deps
 #else
-      map (\uid -> let this_df = homeUnitEnv_dflags (ue_findHomeUnitEnv uid ue) in (uid, importPaths this_df, reexportedModules this_df)) hpt_deps
+      map (\uid -> let this_df = homeUnitEnv_dflags (ue_findHomeUnitEnv uid ue) in (uid, reexportedModules this_df)) hpt_deps
 #endif
     ue = hsc_unit_env env
     units = homeUnitEnv_units $ ue_findHomeUnitEnv (homeUnitId_ dflags) ue
@@ -169,12 +177,12 @@ locateModule env comp_info exts targetFor modName mbPkgName isSource = do
         let genMod = mkModule (RealUnit $ Definite uid) (unLoc modName)  -- TODO support backpack holes
         return $ Right $ FileImport $ ArtifactsLocation file (Just loc) (not isSource) (Just genMod)
 
-    lookupLocal uid dirs reexports = do
-      mbFile <- locateModuleFile [(uid, dirs, reexports)] exts targetFor isSource $ unLoc modName
+    lookupLocal uid moduleMaps reexports = do
+      let mbFile = locateModuleFile moduleMaps isSource (unLoc modName) [(uid, reexports)]
       case mbFile of
         LocateNotFound -> return $ Left $ notFoundErr env modName $ LookupNotFound []
         -- Lookup again with the perspective of the unit reexporting the file
-        LocateFoundReexport uid' -> locateModule (hscSetActiveUnitId uid' env) comp_info exts targetFor modName noPkgQual isSource
+        LocateFoundReexport uid' -> locateModule moduleMaps (hscSetActiveUnitId uid' env) comp_info exts modName noPkgQual isSource
         LocateFoundFile uid' file -> toModLocation uid' file
 
     lookupInPackageDB = do
