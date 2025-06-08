984270229b6cfcdf9d38a4609322cd2862d2a0b3
Jan van Brügge <supermanitu@gmail.com>
2025-06-08 19:11:03 +0200

    hls-notes-plugin: Allow to see where a note is referenced from

--- plugins/hls-notes-plugin/test/NotesTest.hs
+++ plugins/hls-notes-plugin/test/NotesTest.hs
@@ -11,6 +11,7 @@ main :: IO ()
 main = defaultTestRunner $
   testGroup "Notes"
     [ gotoNoteTests
+    , noteReferenceTests
     ]
 
 runSessionWithServer' :: FilePath -> (FilePath -> Session a) -> IO a
@@ -21,6 +22,21 @@ runSessionWithServer' fp act =
         , testDirLocation = Left fp
         } act
 
+noteReferenceTests :: TestTree
+noteReferenceTests = testGroup "Note References"
+   [
+      testCase "multi_file" $ runSessionWithServer' testDataDir $ \dir -> do
+        doc <- openDoc "NoteDef.hs" "haskell"
+        waitForKickDone
+        refs <- getReferences doc (Position 21 15) False
+        let fp = dir </> "NoteDef.hs"
+        liftIO $ refs @?= [
+            Location (filePathToUri (dir </> "Other.hs")) (Range (Position 6 13) (Position 6 13)),
+            Location (filePathToUri fp) (Range (Position 9 9) (Position 9 9)),
+            Location (filePathToUri fp) (Range (Position 5 67) (Position 5 67))
+          ]
+    ]
+
 gotoNoteTests :: TestTree
 gotoNoteTests = testGroup "Goto Note Definition"
     [
@@ -29,13 +45,13 @@ gotoNoteTests = testGroup "Goto Note Definition"
         waitForKickDone
         defs <- getDefinitions doc (Position 3 41)
         let fp = dir </> "NoteDef.hs"
-        liftIO $ defs @?= InL (Definition (InR [Location (filePathToUri fp) (Range (Position 8 9) (Position 8 9))]))
+        liftIO $ defs @?= InL (Definition (InR [Location (filePathToUri fp) (Range (Position 11 9) (Position 11 9))]))
     , testCase "liberal_format" $ runSessionWithServer' testDataDir $ \dir -> do
         doc <- openDoc "NoteDef.hs" "haskell"
         waitForKickDone
         defs <- getDefinitions doc (Position 5 64)
         let fp = dir </> "NoteDef.hs"
-        liftIO $ defs @?= InL (Definition (InR [Location (filePathToUri fp) (Range (Position 18 11) (Position 18 11))]))
+        liftIO $ defs @?= InL (Definition (InR [Location (filePathToUri fp) (Range (Position 21 11) (Position 21 11))]))
 
     , testCase "invalid_note" $ runSessionWithServer' testDataDir $ const $ do
         doc <- openDoc "NoteDef.hs" "haskell"
@@ -54,7 +70,7 @@ gotoNoteTests = testGroup "Goto Note Definition"
         waitForKickDone
         defs <- getDefinitions doc (Position 5 20)
         let fp = dir </> "NoteDef.hs"
-        liftIO $ defs @?= InL (Definition (InR [Location (filePathToUri fp) (Range (Position 12 6) (Position 12 6))]))
+        liftIO $ defs @?= InL (Definition (InR [Location (filePathToUri fp) (Range (Position 15 6) (Position 15 6))]))
     ]
 
 testDataDir :: FilePath
