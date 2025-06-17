2164729df5fa79efe118ff78a8cadc42aff7aca2
rm41339 <rachelma41339@gmail.com>
2025-06-17 18:17:26 +0100

    remove some redundancies between cabal and cabal-project plugin

--- plugins/hls-cabal-project-plugin/src/Ide/Plugin/CabalProject/Orphans.hs
+++ plugins/hls-cabal-project-plugin/src/Ide/Plugin/CabalProject/Orphans.hs
@@ -13,25 +13,7 @@ import qualified Distribution.Solver.Types.ProjectConfigPath as PCPath
 import           GHC.Generics                                (Generic)
 
 import qualified Distribution.Client.ProjectConfig.Types     as PC
-
--- ----------------------------------------------------------------
--- Cabal-syntax orphan instances we need sometimes
--- ----------------------------------------------------------------
-
-instance NFData (Field Position) where
-    rnf (Field name fieldLines) = rnf name `seq` rnf fieldLines
-    rnf (Section name sectionArgs fields) =  rnf name `seq` rnf sectionArgs `seq` rnf fields
-
-instance NFData (Name Position) where
-    rnf (Name ann fName) = rnf ann `seq` rnf fName
-
-instance NFData (FieldLine Position) where
-    rnf (FieldLine ann bs) = rnf ann `seq` rnf bs
-
-instance NFData (SectionArg Position) where
-    rnf (SecArgName ann bs)  = rnf ann `seq` rnf bs
-    rnf (SecArgStr ann bs)   = rnf ann `seq` rnf bs
-    rnf (SecArgOther ann bs) = rnf ann `seq` rnf bs
+import           Ide.Plugin.Cabal.Orphans                    ()
 
 -- Project Config Orphans
 
