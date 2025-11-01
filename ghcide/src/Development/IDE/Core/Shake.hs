9c8a7fb22c1b39526c72d3ca2f0b064e725ae4c3
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2025-11-01 16:13:38 +0400

    fix: remove the unsafePerformIO and head usage
    The cache is now stored inside the `ShakeExtra`. I'm unsure that's the
    right location for it.
    
    This should fix hlint. Also, not having a cache stored as top level
    entry may makes it not survive session reinit, which seams sane (for
    example, during tests).
    
    Still 3 `-boot` related tests are failing. 2 are however flaky.

--- ghcide/src/Development/IDE/Core/Shake.hs
+++ ghcide/src/Development/IDE/Core/Shake.hs
@@ -142,7 +142,7 @@ import           Development.IDE.GHC.Compat             (NameCache,
 import           Development.IDE.GHC.Compat             (NameCache,
                                                          NameCacheUpdater,
                                                          initNameCache,
-                                                         knownKeyNames)
+                                                         knownKeyNames, ModuleName, UnitId)
 #endif
 import           Development.IDE.GHC.Orphans            ()
 import           Development.IDE.Graph                  hiding (ShakeValue,
@@ -186,6 +186,7 @@ import           System.FilePath                        hiding (makeRelative)
 import           System.IO.Unsafe                       (unsafePerformIO)
 import           System.Time.Extra
 import           UnliftIO                               (MonadUnliftIO (withRunInIO))
+import Data.Map.Strict (Map)
 
 
 data Log
@@ -318,6 +319,10 @@ data ShakeExtras = ShakeExtras
     ,ideNc :: NameCache
     -- | A mapping of module name to known target (or candidate targets, if missing)
     ,knownTargetsVar :: TVar (Hashed KnownTargets)
+    ,moduleToPathCache :: TVar (Map
+     Unique
+     (Map ModuleName (UnitId, NormalizedFilePath),
+      Map ModuleName (UnitId, NormalizedFilePath)))
     -- | A mapping of exported identifiers for local modules. Updated on kick
     ,exportsMap :: TVar ExportsMap
     -- | A work queue for actions added via 'runInShakeSession'
@@ -717,6 +722,8 @@ shakeOpen recorder lspEnv defaultConfig idePlugins debouncer
         dirtyKeys <- newTVarIO mempty
         -- Take one VFS snapshot at the start
         vfsVar <- newTVarIO =<< vfsSnapshot lspEnv
+
+        moduleToPathCache <- newTVarIO mempty
         pure ShakeExtras{shakeRecorder = recorder, ..}
     shakeDb  <-
         shakeNewDatabase
@@ -1494,3 +1501,5 @@ runWithSignal msgStart msgEnd files rule = do
   kickSignal testing lspEnv files msgStart
   void $ uses rule files
   kickSignal testing lspEnv files msgEnd
+
+
