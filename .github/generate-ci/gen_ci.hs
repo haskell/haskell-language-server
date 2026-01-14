2263a75990265b343b3e54414405b71ed5802f56
fendor <fendor@posteo.de>
2026-01-14 10:37:09 +0100

    Use macos-15 distro as macos-13 is deprecated in GHA

--- .github/generate-ci/gen_ci.hs
+++ .github/generate-ci/gen_ci.hs
@@ -190,7 +190,7 @@ envVars arch os = object $
 runner :: Arch -> Opsys -> [Value]
 runner Amd64 (Linux _)   = ["ubuntu-latest"]
 runner AArch64 (Linux _) = ["ubuntu-22.04-arm"]
-runner Amd64 Darwin      = ["macOS-13"]
+runner Amd64 Darwin      = ["macOS-15"]
 runner AArch64 Darwin    = ["self-hosted", "macOS", "ARM64"]
 runner Amd64 Windows     = ["windows-latest"]
 runner AArch64 Windows   = error "aarch64 windows not supported"
