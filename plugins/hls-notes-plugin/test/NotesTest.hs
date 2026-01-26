def18b469c0def399c8c7d429fa770aec40f2824
vidit-od <82703629+vidit-od@users.noreply.github.com>
2026-01-26 13:53:32 +0530

    Improving Notes Plugin (#4791)
    * hover Note References to get declaration info
    
    * Note Declaration snippet
    
    * Note References
    
    * update config and format code
    
    * Implement Reviews

--- plugins/hls-notes-plugin/test/NotesTest.hs
+++ plugins/hls-notes-plugin/test/NotesTest.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import           Data.List        (sort)
 import           Ide.Plugin.Notes (Log, descriptor)
 import           System.FilePath  ((</>))
 import           Test.Hls
@@ -12,6 +13,7 @@ main = defaultTestRunner $
   testGroup "Notes"
     [ gotoNoteTests
     , noteReferenceTests
+    , hoverNoteTests
     ]
 
 runSessionWithServer' :: FilePath -> (FilePath -> Session a) -> IO a
@@ -28,13 +30,15 @@ noteReferenceTests = testGroup "Note References"
       testCase "multi_file" $ runSessionWithServer' testDataDir $ \dir -> do
         doc <- openDoc "NoteDef.hs" "haskell"
         waitForKickDone
-        refs <- getReferences doc (Position 21 15) False
-        let fp = dir </> "NoteDef.hs"
-        liftIO $ refs @?= [
-            Location (filePathToUri (dir </> "Other.hs")) (Range (Position 6 13) (Position 6 13)),
-            Location (filePathToUri fp) (Range (Position 9 9) (Position 9 9)),
-            Location (filePathToUri fp) (Range (Position 5 67) (Position 5 67))
-          ]
+        refs0 <- getReferences doc (Position 21 15) False
+        let refs = sort refs0
+            fp = dir </> "NoteDef.hs"
+
+        liftIO $ sort refs @?= sort
+            [ Location (filePathToUri (dir </> "Other.hs")) (Range (Position 6 13) (Position 6 13))
+            , Location (filePathToUri fp) (Range (Position 9 9) (Position 9 9))
+            , Location (filePathToUri fp) (Range (Position 5 67) (Position 5 67))
+            ]
     ]
 
 gotoNoteTests :: TestTree
@@ -75,3 +79,69 @@ gotoNoteTests = testGroup "Goto Note Definition"
 
 testDataDir :: FilePath
 testDataDir = "plugins" </> "hls-notes-plugin" </> "test" </> "testdata"
+
+hoverNoteTests :: TestTree
+hoverNoteTests = testGroup "Hover Notes"
+  [ testCase "hover normal-notes" $
+      runSessionWithServer' testDataDir $ \_dir -> do
+        let file = "HoverNote.hs"
+            pos  = Position 24 10
+        doc <- openDoc file "haskell"
+        waitForKickDone
+        hover <- getHover doc pos
+
+        let expected =
+              Just $ Hover (InL $ MarkupContent MarkupKind_Markdown
+              "Normal-Notes\n\nContent of Normal Notes\n\n")
+              (Just (Range (Position 24 3) (Position 24 22)))
+
+        liftIO $ hover @?= expected
+
+
+  , testCase "hover multi-notes-one" $
+      runSessionWithServer' testDataDir $ \_dir -> do
+        let file = "HoverNote.hs"
+            pos  = Position 25 10
+        doc <- openDoc file "haskell"
+        waitForKickDone
+        hover <- getHover doc pos
+
+        let expected =
+              Just $ Hover (InL $ MarkupContent MarkupKind_Markdown
+              "Multi-Notes-one\n\nContent of Multi-Notes-one\n\n\n")
+              (Just (Range (Position 25 3) (Position 25 25)))
+
+        liftIO $ hover @?= expected
+
+
+  , testCase "hover multi-notes-two" $
+      runSessionWithServer' testDataDir $ \_dir -> do
+        let file = "HoverNote.hs"
+            pos  = Position 26 10
+        doc <- openDoc file "haskell"
+        waitForKickDone
+        hover <- getHover doc pos
+
+        let expected =
+              Just $ Hover (InL $ MarkupContent MarkupKind_Markdown
+              "Multi-Notes-two\n\nContent of Multi-Notes-two\n\n")
+              (Just (Range (Position 26 3) (Position 26 25)))
+
+        liftIO $ hover @?= expected
+
+
+  , testCase "hover single-comment-declaration" $
+      runSessionWithServer' testDataDir $ \_dir -> do
+        let file = "HoverNote.hs"
+            pos  = Position 27 10
+        doc <- openDoc file "haskell"
+        waitForKickDone
+        hover <- getHover doc pos
+
+        let expected =
+              Just $ Hover (InL $ MarkupContent MarkupKind_Markdown
+              "Single Comment Declaration\n\nContent of Single Comment Declaration\n\n")
+              (Just (Range (Position 27 3) (Position 27 36)))
+
+        liftIO $ hover @?= expected
+  ]
