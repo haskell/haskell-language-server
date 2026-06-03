1d567613259a26f43e8cbef4447a0bc6255f41df
vidit <vidit894@gmail.com>
2026-06-03 01:00:50 +0000

    Fix notes plugin

--- plugins/hls-notes-plugin/src/Ide/Plugin/Notes.hs
+++ plugins/hls-notes-plugin/src/Ide/Plugin/Notes.hs
@@ -105,13 +105,16 @@ err :: MonadError PluginError m => Text -> Maybe a -> m a
 err s = maybe (throwError $ PluginInternalError s) pure
 
 getNote :: NormalizedFilePath -> IdeState -> Position -> ExceptT PluginError (HandlerM c) (Maybe Text)
-getNote nfp state (Position l c) = do
-    contents <-
-        err "Error getting file contents"
-        =<< liftIO (runAction "notes.getfileContents" state (getFileContents nfp))
-    line <- err "Line not found in file" (listToMaybe $ Rope.lines $ fst
-        (Rope.splitAtLine 1 $ snd $ Rope.splitAtLine (fromIntegral l) contents))
-    pure $ listToMaybe $ mapMaybe (atPos $ fromIntegral c) $ matchAllText noteRefRegex line
+getNote nfp state (Position l c) =
+    case getSourceFileOrigin nfp of
+        FromDependency -> pure Nothing
+        FromProject -> do
+            contents <-
+                err "Error getting file contents"
+                =<< liftIO (runAction "notes.getfileContents" state (getFileContents nfp))
+            line <- err "Line not found in file" (listToMaybe $ Rope.lines $ fst
+                (Rope.splitAtLine 1 $ snd $ Rope.splitAtLine (fromIntegral l) contents))
+            pure $ listToMaybe $ mapMaybe (atPos $ fromIntegral c) $ matchAllText noteRefRegex line
   where
     atPos c arr = case arr A.! 0 of
         -- We check if the line we are currently at contains a note
@@ -161,20 +164,23 @@ jumpToNote state _ param
 jumpToNote _ _ _ = throwError $ PluginInternalError "conversion to normalized file path failed"
 
 findNotesInFile :: NormalizedFilePath -> Recorder (WithPriority Log) -> Action (Maybe (HM.HashMap Text Position, HM.HashMap Text [Position]))
-findNotesInFile file recorder = do
-    -- GetFileContents only returns a value if the file is open in the editor of
-    -- the user. If not, we need to read it from disk.
-    contentOpt <- (snd =<<) <$> use GetFileContents file
-    content <- case contentOpt of
-        Just x  -> pure $ Rope.toText x
-        Nothing -> liftIO $ readFileUtf8 $ fromNormalizedFilePath file
-    let noteMatches = (A.! 1) <$> matchAllText noteRegex content
-        notes = toPositions noteMatches content
-    logWith recorder Debug $ LogNotesFound file (HM.toList notes)
-    let refMatches = (A.! 1) <$> matchAllText noteRefRegex content
-        refs = toPositions refMatches content
-    logWith recorder Debug $ LogNoteReferencesFound file (HM.toList refs)
-    pure $ Just (HM.mapMaybe (fmap fst . List.uncons) notes, refs)
+findNotesInFile file recorder =
+    case getSourceFileOrigin file of
+        FromDependency -> pure $ Just (HM.empty, HM.empty)
+        FromProject -> do
+            -- GetFileContents only returns a value if the file is open in the editor of
+            -- the user. If not, we need to read it from disk.
+            contentOpt <- (snd =<<) <$> use GetFileContents file
+            content <- case contentOpt of
+                Just x  -> pure $ Rope.toText x
+                Nothing -> liftIO $ readFileUtf8 $ fromNormalizedFilePath file
+            let noteMatches = (A.! 1) <$> matchAllText noteRegex content
+                notes = toPositions noteMatches content
+            logWith recorder Debug $ LogNotesFound file (HM.toList notes)
+            let refMatches = (A.! 1) <$> matchAllText noteRefRegex content
+                refs = toPositions refMatches content
+            logWith recorder Debug $ LogNoteReferencesFound file (HM.toList refs)
+            pure $ Just (HM.mapMaybe (fmap fst . List.uncons) notes, refs)
     where
         uint = fromIntegral . toInteger
         -- the regex library returns the character index of the match. However
@@ -323,62 +329,65 @@ autocomplete state _ params = do
       pos = params ^. L.position
       nuri = toNormalizedUri uri
 
-  contents <-
-    liftIO $
-      runAction "Notes.GetUriContents" state $
-        getUriContents nuri
+  case uriToNormalizedFilePath nuri of
+    Just nfp | getSourceFileOrigin nfp == FromDependency -> pure $ InL []
+    _ -> do
+      contents <-
+        liftIO $
+          runAction "Notes.GetUriContents" state $
+            getUriContents nuri
 
-  fmap InL $
-    case contents of
-      Nothing -> pure []
+      fmap InL $
+        case contents of
+          Nothing -> pure []
 
-      Just rope -> do
-        let linePrefix =  T.toLower $  T.stripEnd $  getLinePrefix rope pos
+          Just rope -> do
+            let linePrefix =  T.toLower $  T.stripEnd $  getLinePrefix rope pos
 
-        -- Suggest NOTE DECLARATION snippit if "note" prefix detected
-        if T.strip linePrefix == "note"
-        then
-          pure [CompletionItem "Note" Nothing (Just CompletionItemKind_Keyword) Nothing
-              (Just "Note Declaration") Nothing Nothing Nothing Nothing
-              Nothing (Just noteSnippet) (Just InsertTextFormat_Snippet) Nothing
-              Nothing Nothing Nothing Nothing Nothing Nothing
-          ]
+            -- Suggest NOTE DECLARATION snippit if "note" prefix detected
+            if T.strip linePrefix == "note"
+            then
+              pure [CompletionItem "Note" Nothing (Just CompletionItemKind_Keyword) Nothing
+                  (Just "Note Declaration") Nothing Nothing Nothing Nothing
+                  Nothing (Just noteSnippet) (Just InsertTextFormat_Snippet) Nothing
+                  Nothing Nothing Nothing Nothing Nothing Nothing
+              ]
 
-        -- Suggest list of all NOTE DECLARATION if "note [" infix detected
-        else if "note[" `T.isInfixOf` linePrefix || "note [" `T.isInfixOf` linePrefix
-        then
-          case uriToNormalizedFilePath nuri of
-            Nothing -> pure []
+            -- Suggest list of all NOTE DECLARATION if "note [" infix detected
+            else if "note[" `T.isInfixOf` linePrefix || "note [" `T.isInfixOf` linePrefix
+            then
+              case uriToNormalizedFilePath nuri of
+                Nothing -> pure []
 
-            Just nfp -> do
-              let typed =
-                   case T.breakOnEnd "[" linePrefix of
-                    (_, "")   -> ""
-                    (_, rest)-> T.strip rest
+                Just nfp -> do
+                  let typed =
+                       case T.breakOnEnd "[" linePrefix of
+                        (_, "")   -> ""
+                        (_, rest)-> T.strip rest
 
-              notesMap <-
-                runActionE "notes.completion.notes" state $
-                  useE MkGetNotes nfp
+                  notesMap <-
+                    runActionE "notes.completion.notes" state $
+                      useE MkGetNotes nfp
 
-              let allNotes = HM.keys notesMap
-                  matches =
-                    filter
-                      (\n -> T.toLower typed `T.isPrefixOf` T.toLower n)
-                      allNotes
+                  let allNotes = HM.keys notesMap
+                      matches =
+                        filter
+                          (\n -> T.toLower typed `T.isPrefixOf` T.toLower n)
+                          allNotes
 
-                  finalNotes =
-                    if null matches then allNotes else matches
-              pure $
-                map
-                  (\n ->
-                     CompletionItem n Nothing (Just CompletionItemKind_Reference) Nothing (Just "Note reference")
-                       Nothing Nothing (Just True) (Just "0") (Just n)
-                       Nothing Nothing Nothing Nothing Nothing
-                       Nothing Nothing Nothing Nothing
-                  )
-                  finalNotes
-        else
-          pure []
+                      finalNotes =
+                        if null matches then allNotes else matches
+                  pure $
+                    map
+                      (\n ->
+                         CompletionItem n Nothing (Just CompletionItemKind_Reference) Nothing (Just "Note reference")
+                           Nothing Nothing (Just True) (Just "0") (Just n)
+                           Nothing Nothing Nothing Nothing Nothing
+                           Nothing Nothing Nothing Nothing
+                      )
+                      finalNotes
+            else
+              pure []
 
 noteSnippet :: Text
 noteSnippet =
