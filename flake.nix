1fecee83c4e3230b25da6ecb102df11a20244aab
Curtis Chin Jen Sem <csochinjensem@gmail.com>
2026-06-10 08:48:42 +0200

    Remove LD_LIBRARY_PATH override in nix devShell (#4966)

--- flake.nix
+++ flake.nix
@@ -87,7 +87,6 @@
 
           shellHook = ''
             # @guibou: I'm not sure theses lines are needed
-            export LD_LIBRARY_PATH=${gmp}/lib:${zlib}/lib:${ncurses}/lib:${capstone}/lib
             export DYLD_LIBRARY_PATH=${gmp}/lib:${zlib}/lib:${ncurses}/lib:${capstone}/lib
             export PATH=$PATH:$HOME/.local/bin
 
